29fa742555286909e8c5a2bed2c8bc959f867319
joshuasbrown <joshua.s.brown@lanl.gov>
2020-03-10 08:27:25 -0600

    Switched from endl to \n, fixed outflow error

--- tst/integration/test_integration_mesh.cpp
+++ tst/integration/test_integration_mesh.cpp
@@ -51,35 +51,35 @@ TEST_CASE("Testing Mesh Constructor"){
   MPI_Comm_rank( MPI_COMM_WORLD, &rank);
 
   std::stringstream str_stream;
-  str_stream << "<time>" << std::endl;
-  str_stream << "nlim        = -1        # cycle limit" << std::endl;
-  str_stream << "tlim        = 0.25      # time limit" << std::endl;
-  str_stream << "ncycle_out  = 25        # interval for stdout summary info" << std::endl;
-  str_stream << "" << std::endl;
-  str_stream << "<mesh>" << std::endl;
-  str_stream << "nx1         = 16       # Number of zones in X1-direction" << std::endl;
-  str_stream << "x1min       = -0.5      # minimum value of X1" << std::endl;
-  str_stream << "x1max       = 0.5       # maximum value of X1" << std::endl;
-  str_stream << "ix1_bc      = outstr_streamow   # Inner-X1 boundary condition str_streamag" << std::endl;
-  str_stream << "ox1_bc      = outstr_streamow   # Outer-X1 boundary condition str_streamag" << std::endl;
-  str_stream << "" << std::endl;
-  str_stream << "nx2         = 24         # Number of zones in X2-direction" << std::endl;
-  str_stream << "x2min       = -0.6      # minimum value of X2" << std::endl;
-  str_stream << "x2max       = 0.6       # maximum value of X2" << std::endl;
-  str_stream << "ix2_bc      = periodic  # Inner-X2 boundary condition str_streamag" << std::endl;
-  str_stream << "ox2_bc      = periodic  # Outer-X2 boundary condition str_streamag" << std::endl;
-  str_stream << "" << std::endl;
-  str_stream << "nx3         = 12         # Number of zones in X3-direction" << std::endl;
-  str_stream << "x3min       = -0.7      # minimum value of X3" << std::endl;
-  str_stream << "x3max       = 0.7       # maximum value of X3" << std::endl;
-  str_stream << "ix3_bc      = periodic  # Inner-X3 boundary condition str_streamag" << std::endl;
-  str_stream << "ox3_bc      = periodic  # Outer-X3 boundary condition str_streamfgag" << std::endl;
-  str_stream << "" << std::endl;
-  str_stream << "num_threads = 2         # maximum number of OMP threads" << std::endl;
-  str_stream << "" << std::endl;
-  str_stream << "<meshblock>" << std::endl;
-  str_stream << "nx1 = 8" << std::endl;
-  str_stream << "nx2 = 4" << std::endl;
+  str_stream << "<time>\n";
+  str_stream << "nlim        = -1        # cycle limit\n";
+  str_stream << "tlim        = 0.25      # time limit\n";
+  str_stream << "ncycle_out  = 25        # interval for stdout summary info\n";
+  str_stream << "\n";
+  str_stream << "<mesh>\n";
+  str_stream << "nx1         = 16        # Number of zones in X1-direction\n";
+  str_stream << "x1min       = -0.5      # minimum value of X1\n";
+  str_stream << "x1max       = 0.5       # maximum value of X1\n";
+  str_stream << "ix1_bc      = outflow   # Inner-X1 boundary condition str_streamag\n";
+  str_stream << "ox1_bc      = outflow   # Outer-X1 boundary condition str_streamag\n";
+  str_stream << "\n";
+  str_stream << "nx2         = 24        # Number of zones in X2-direction\n";
+  str_stream << "x2min       = -0.6      # minimum value of X2\n";
+  str_stream << "x2max       = 0.6       # maximum value of X2\n";
+  str_stream << "ix2_bc      = periodic  # Inner-X2 boundary condition str_streamag\n";
+  str_stream << "ox2_bc      = periodic  # Outer-X2 boundary condition str_streamag\n";
+  str_stream << "\n";
+  str_stream << "nx3         = 12        # Number of zones in X3-direction\n";
+  str_stream << "x3min       = -0.7      # minimum value of X3\n";
+  str_stream << "x3max       = 0.7       # maximum value of X3\n";
+  str_stream << "ix3_bc      = periodic  # Inner-X3 boundary condition str_streamag\n";
+  str_stream << "ox3_bc      = periodic  # Outer-X3 boundary condition str_streamfgag\n";
+  str_stream << "\n";
+  str_stream << "num_threads = 2         # maximum number of OMP threads\n";
+  str_stream << "\n";
+  str_stream << "<meshblock>\n";
+  str_stream << "nx1 = 8\n";
+  str_stream << "nx2 = 4\n";
   str_stream << "nx3 = 6" << std::endl;
 
   ParameterInput pin;
@@ -89,7 +89,7 @@ TEST_CASE("Testing Mesh Constructor"){
   std::map<std::string, std::shared_ptr<StateDescriptor>> physics;
   PreFillDerivedFunc pre_fill_derived;
   int test_flag=0;
-
+  
   Mesh mesh(
       &pin,
       materials,
