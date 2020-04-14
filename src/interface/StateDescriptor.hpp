aeee9320ddaae73b5f8be3da5652f717b54d8f70
Josh Dolence <jdolence@lanl.gov>
2020-04-14 12:07:21 -0700

    fix broken merge

--- src/interface/StateDescriptor.hpp
+++ src/interface/StateDescriptor.hpp
@@ -104,8 +104,8 @@ class StateDescriptor {
     return names;
   }
 
-  const std::map<std::string, Metadata> &AllFields() { return _metadataMap; }
-  const std::map<std::string, std::vector<Metadata>> &AllSparseFields() {
+  std::map<std::string, Metadata> &AllFields() { return _metadataMap; }
+  std::map<std::string, std::vector<Metadata>> &AllSparseFields() {
     return _sparseMetadataMap;
   }
 
