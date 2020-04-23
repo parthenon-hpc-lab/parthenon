cb621b5447ad7970f4ef9a8e1826767c98c1476c
Sriram Swaminarayan <sriram@lanl.gov>
2020-04-23 13:19:06 -0600

    fixed formatting to make clang-format-8 happy

--- src/interface/container_iterator.hpp
+++ src/interface/container_iterator.hpp
@@ -40,9 +40,7 @@ class ContainerIterator {
   /// initializes the iterator with a container and a flag to match
   /// @param c the container on which you want the iterator
   /// @param flags: a vector of Metadata::flags that you want to match
-  ContainerIterator<T>(const Container<T> &c,
-                       const std::vector<MetadataFlag> &flags) {
-    // c.print();
+  ContainerIterator<T>(const Container<T> &c, const std::vector<MetadataFlag> &flags) {
     allVars_ = c.GetCellVariableVector();
     for (auto &svar : c.GetSparseVector()) {
       CellVariableVector<T> &svec = svar->GetVector();
@@ -56,9 +54,7 @@ class ContainerIterator {
   /// initializes the iterator with a container and a flag to match
   /// @param c the container on which you want the iterator
   /// @param names: a vector of std::string with names you want to match
-  ContainerIterator<T>(const Container<T> &c,
-                       const std::vector<std::string> &names) {
-    // c.print();
+  ContainerIterator<T>(const Container<T> &c, const std::vector<std::string> &names) {
     allVars_ = c.GetCellVariableVector();
     for (auto &svar : c.GetSparseVector()) {
       CellVariableVector<T> &svec = svar->GetVector();
@@ -71,12 +67,12 @@ class ContainerIterator {
 
   /// Changes the mask for the iterator and resets the iterator
   /// @param names: a vector of MetadataFlag that you want to match
-  void resetVars( const std::vector<std::string> &names ) {
+  void resetVars(const std::vector<std::string> &names) {
     // 1: clear out variables stored so far
     emptyVars_();
 
     // 2: fill in the subset of variables that match at least one entry in names
-    for ( auto pv : allVars_ ) {
+    for (auto pv : allVars_) {
       if (std::find(names.begin(), names.end(), pv->label()) != names.end())
         vars.push_back(pv);
     }
@@ -84,12 +80,12 @@ class ContainerIterator {
 
   /// Changes the mask for the iterator and resets the iterator
   /// @param flags: a vector of MetadataFlag that you want to match
-  void resetVars( const std::vector<MetadataFlag> &flags ) {
+  void resetVars(const std::vector<MetadataFlag> &flags) {
     // 1: clear out variables stored so far
     emptyVars_();
 
     // 2: fill in the subset of variables that match mask
-    for ( auto pv : allVars_ ) {
+    for (auto pv : allVars_) {
       if (pv->metadata().AnyFlagsSet(flags)) {
         vars.push_back(pv);
       }
