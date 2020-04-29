c2c3230fe312416418675c9dc355ea333763e157
Philipp Grete <grete@pa.msu.edu>
2020-04-29 08:16:06 -0400

    Kokkosize cartesian coord ctor

--- src/coordinates/cartesian.cpp
+++ src/coordinates/cartesian.cpp
@@ -31,82 +31,111 @@ namespace parthenon {
 
 Cartesian::Cartesian(MeshBlock *pmb, ParameterInput *pin, bool flag)
     : Coordinates(pmb, pin, flag) {
+
+  // get host mirrors to initialize on host
+  auto x1v_h = x1v.GetHostMirror();
+  auto dx1v_h = dx1v.GetHostMirror();
+  auto x1f_h = x1f.GetHostMirrorAndCopy();   // initialized in the Coordinates
+  auto dx1f_h = dx1f.GetHostMirrorAndCopy(); // initialized in the Coordinates
+  auto x2v_h = x2v.GetHostMirror();
+  auto dx2v_h = dx2v.GetHostMirror();
+  auto x2f_h = x2f.GetHostMirrorAndCopy();   // initialized in the Coordinates
+  auto dx2f_h = dx2f.GetHostMirrorAndCopy(); // initialized in the Coordinates
+  auto x3v_h = x3v.GetHostMirror();
+  auto dx3v_h = dx3v.GetHostMirror();
+  auto x3f_h = x3f.GetHostMirrorAndCopy();   // initialized in the Coordinates
+  auto dx3f_h = dx3f.GetHostMirrorAndCopy(); // initialized in the Coordinates
+
   // initialize volume-averaged coordinates and spacing
   // x1-direction: x1v = dx/2
   for (int i = il - ng; i <= iu + ng; ++i) {
-    x1v(i) = 0.5 * (x1f(i + 1) + x1f(i));
+    x1v_h(i) = 0.5 * (x1f_h(i + 1) + x1f_h(i));
   }
   for (int i = il - ng; i <= iu + ng - 1; ++i) {
     if (pmb->block_size.x1rat != 1.0) {
-      dx1v(i) = x1v(i + 1) - x1v(i);
+      dx1v_h(i) = x1v_h(i + 1) - x1v_h(i);
     } else {
-      // dx1v = dx1f constant for uniform mesh; may disagree with x1v(i+1) - x1v(i)
-      dx1v(i) = dx1f(i);
+      // dx1v = dx1f constant for uniform mesh; may disagree with x1v_h(i+1) - x1v_h(i)
+      dx1v_h(i) = dx1f_h(i);
     }
   }
 
   // x2-direction: x2v = dy/2
   if (pmb->block_size.nx2 == 1) {
-    x2v(jl) = 0.5 * (x2f(jl + 1) + x2f(jl));
-    dx2v(jl) = dx2f(jl);
+    x2v_h(jl) = 0.5 * (x2f_h(jl + 1) + x2f_h(jl));
+    dx2v_h(jl) = dx2f_h(jl);
   } else {
     for (int j = jl - ng; j <= ju + ng; ++j) {
-      x2v(j) = 0.5 * (x2f(j + 1) + x2f(j));
+      x2v_h(j) = 0.5 * (x2f_h(j + 1) + x2f_h(j));
     }
     for (int j = jl - ng; j <= ju + ng - 1; ++j) {
       if (pmb->block_size.x2rat != 1.0) {
-        dx2v(j) = x2v(j + 1) - x2v(j);
+        dx2v_h(j) = x2v_h(j + 1) - x2v_h(j);
       } else {
-        // dx2v = dx2f constant for uniform mesh; may disagree with x2v(j+1) - x2v(j)
-        dx2v(j) = dx2f(j);
+        // dx2v = dx2f constant for uniform mesh; may disagree with x2v_h(j+1) - x2v_h(j)
+        dx2v_h(j) = dx2f_h(j);
       }
     }
   }
 
   // x3-direction: x3v = dz/2
   if (pmb->block_size.nx3 == 1) {
-    x3v(kl) = 0.5 * (x3f(kl + 1) + x3f(kl));
-    dx3v(kl) = dx3f(kl);
+    x3v_h(kl) = 0.5 * (x3f_h(kl + 1) + x3f_h(kl));
+    dx3v_h(kl) = dx3f_h(kl);
   } else {
     for (int k = kl - ng; k <= ku + ng; ++k) {
-      x3v(k) = 0.5 * (x3f(k + 1) + x3f(k));
+      x3v_h(k) = 0.5 * (x3f_h(k + 1) + x3f_h(k));
     }
     for (int k = kl - ng; k <= ku + ng - 1; ++k) {
       if (pmb->block_size.x3rat != 1.0) {
-        dx3v(k) = x3v(k + 1) - x3v(k);
+        dx3v_h(k) = x3v_h(k + 1) - x3v_h(k);
       } else {
-        // dxkv = dx3f constant for uniform mesh; may disagree with x3v(k+1) - x3v(k)
-        dx3v(k) = dx3f(k);
+        // dxkv = dx3f constant for uniform mesh; may disagree with x3v_h(k+1) - x3v_h(k)
+        dx3v_h(k) = dx3f_h(k);
       }
     }
   }
+
+  // helper function to init on host
+  auto InitOnHost = [](ParArrayND<Real> dev_arr, int low, int up, Real init_val) {
+    auto host_arr = dev_arr.GetHostMirror();
+    for (int i = low; i <= up; ++i) {
+      host_arr(i) = init_val;
+    }
+    dev_arr.DeepCopy(host_arr);
+  };
+
   // initialize geometry coefficients
   // x1-direction
-  for (int i = il - ng; i <= iu + ng; ++i) {
-    h2v(i) = 1.0;
-    h2f(i) = 1.0;
-    h31v(i) = 1.0;
-    h31f(i) = 1.0;
-    dh2vd1(i) = 0.0;
-    dh2fd1(i) = 0.0;
-    dh31vd1(i) = 0.0;
-    dh31fd1(i) = 0.0;
-  }
+  InitOnHost(h2v, il - ng, iu + ng, 1.0);
+  InitOnHost(h2f, il - ng, iu + ng, 1.0);
+  InitOnHost(h31v, il - ng, iu + ng, 1.0);
+  InitOnHost(h31f, il - ng, iu + ng, 1.0);
+  InitOnHost(dh2vd1, il - ng, iu + ng, 0.0);
+  InitOnHost(dh2fd1, il - ng, iu + ng, 0.0);
+  InitOnHost(dh31vd1, il - ng, iu + ng, 0.0);
+  InitOnHost(dh31fd1, il - ng, iu + ng, 0.0);
 
   // x2-direction
   if (pmb->block_size.nx2 == 1) {
-    h32v(jl) = 1.0;
-    h32f(jl) = 1.0;
-    dh32vd2(jl) = 0.0;
-    dh32fd2(jl) = 0.0;
+    InitOnHost(h32v, jl, jl, 1.0);
+    InitOnHost(h32f, jl, jl, 1.0);
+    InitOnHost(dh32vd2, jl, jl, 0.0);
+    InitOnHost(dh32fd2, jl, jl, 0.0);
   } else {
-    for (int j = jl - ng; j <= ju + ng; ++j) {
-      h32v(j) = 1.0;
-      h32f(j) = 1.0;
-      dh32vd2(j) = 0.0;
-      dh32fd2(j) = 0.0;
-    }
+    InitOnHost(h32v, jl - ng, ju + ng, 1.0);
+    InitOnHost(h32f, jl - ng, ju + ng, 1.0);
+    InitOnHost(dh32vd2, jl - ng, ju + ng, 0.0);
+    InitOnHost(dh32fd2, jl - ng, ju + ng, 0.0);
   }
+
+  // copy data back to device
+  x1v.DeepCopy(x1v_h);
+  x2v.DeepCopy(x2v_h);
+  x3v.DeepCopy(x3v_h);
+  dx1v.DeepCopy(dx1v_h);
+  dx2v.DeepCopy(dx2v_h);
+  dx3v.DeepCopy(dx3v_h);
 }
 
 } // namespace parthenon
