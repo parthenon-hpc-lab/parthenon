08aeee01926446ab244c0b2cdac30f9ca3787784
Philipp Grete <grete@pa.msu.edu>
2020-05-06 12:55:11 -0400

    Add nested par for wrapper to MeshBlock

--- src/mesh/mesh.hpp
+++ src/mesh/mesh.hpp
@@ -171,6 +171,25 @@ class MeshBlock {
     parthenon::par_for(name, exec_space, nl, nu, kl, ku, jl, ju, il, iu, function);
   }
 
+  // 2D Outer default loop pattern
+  template <typename Function>
+  inline void par_for_outer(const std::string &name, const size_t &scratch_size_in_bytes,
+                            const int &scratch_level, const int &kl, const int &ku,
+                            const int &jl, const int &ju, const Function &function) {
+    parthenon::par_for_outer(name, exec_space, scratch_size_in_bytes, scratch_level, kl,
+                             ku, jl, ju, function);
+  }
+
+  // 3D Outer default loop pattern
+  template <typename Function>
+  inline void par_for(const std::string &name, size_t &scratch_size_in_bytes,
+                      const int &scratch_level, const int &nl, const int &nu,
+                      const int &kl, const int &ku, const int &jl, const int &ju,
+                      const Function &function) {
+    parthenon::par_for_outer(name, exec_space, scratch_size_in_bytes, scratch_level, nl,
+                             nu, kl, ku, jl, ju, function);
+  }
+
   std::size_t GetBlockSizeInBytes();
   int GetNumberOfMeshBlockCells() {
     return block_size.nx1 * block_size.nx2 * block_size.nx3;
