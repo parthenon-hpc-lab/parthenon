125a31ef85827ce218cf74589ccb10dbc49d096b
Philipp Grete <grete@pa.msu.edu>
2020-06-08 15:29:29 -0400

    Ensure buffers are first copied once block work is done

--- src/bvals/bvals_var.cpp
+++ src/bvals/bvals_var.cpp
@@ -111,7 +111,11 @@ void BoundaryVariable::CopyVariableBufferSameProcess(NeighborBlock &nb, int ssiz
   MeshBlock *ptarget_block = pmy_mesh_->FindMeshBlock(nb.snb.gid);
   // 2) which element in vector of BoundaryVariable *?
   BoundaryData<> *ptarget_bdata = &(ptarget_block->pbval->bvars[bvar_index]->bd_var_);
-  ptarget_block->deep_copy(ptarget_bdata->recv[nb.targetid], bd_var_.send[nb.bufid]);
+  // TODO(pgrete) ensure/double check that the data on target has already been used before
+  // overwriting it
+  // using source block for deep copy to ensure physics kernels are done
+  MeshBlock *psource_block = pmy_block_;
+  psource_block->deep_copy(ptarget_bdata->recv[nb.targetid], bd_var_.send[nb.bufid]);
   // finally, set the BoundaryStatus flag on the destination buffer
   ptarget_bdata->flag[nb.targetid] = BoundaryStatus::arrived;
   return;
@@ -126,7 +130,9 @@ void BoundaryVariable::CopyFluxCorrectionBufferSameProcess(NeighborBlock &nb, in
   // 2) which element in vector of BoundaryVariable *?
   BoundaryData<> *ptarget_bdata =
       &(ptarget_block->pbval->bvars[bvar_index]->bd_var_flcor_);
-  ptarget_block->deep_copy(ptarget_bdata->recv[nb.targetid],
+  // using source block for deep copy to ensure physics kernels are done
+  MeshBlock *psource_block = pmy_block_;
+  psource_block->deep_copy(ptarget_bdata->recv[nb.targetid],
                            bd_var_flcor_.send[nb.bufid]);
   ptarget_bdata->flag[nb.targetid] = BoundaryStatus::arrived;
   return;
