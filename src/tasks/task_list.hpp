6cb20e20b4ef9d35fda612176219a7787ee33083
Josh Dolence <jdolence@lanl.gov>
2020-06-24 23:17:07 -0600

    back to the lambda

--- src/tasks/task_list.hpp
+++ src/tasks/task_list.hpp
@@ -87,8 +87,7 @@ class TaskList {
   template <class F, class... Args>
   TaskID AddTask(F &&func, TaskID &dep, Args &&... args) {
     TaskID id(tasks_added_ + 1);
-    task_list_.push_back(
-        Task(id, dep, std::bind(std::forward<F>(func), std::forward<Args>(args)...)));
+    task_list_.push_back(Task(id, dep, [=]() mutable { return func(args...); }));
     tasks_added_++;
     return id;
   }
