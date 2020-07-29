e354229d90e42f02ca2aab468994d2c9abc55f16
Philipp Grete <grete@pa.msu.edu>
2020-07-29 13:22:07 -0400

    Add threads and streams to perf regresion test

--- tst/regression/test_suites/advection_performance/advection_performance.py
+++ tst/regression/test_suites/advection_performance/advection_performance.py
@@ -28,43 +28,78 @@ import utils.test_case
 """ To prevent littering up imported folders with .pyc files or __pycache_ folder"""
 sys.dont_write_bytecode = True
 
-mb_sizes = [256, 128, 64, 32, 16] # meshblock sizes
+n_threads_arr = [1, 4, 8]
+n_streams_arr = [1, 4, 8, 16]
+mb_sizes = [256, 128, 64, 32] # meshblock sizes
+
+def get_test_matrix():
+    test_configs = []
+    for n_threads in n_threads_arr:
+        for n_streams in n_streams_arr:
+            if n_streams < n_threads:
+                continue
+            if n_streams > 2*n_threads:
+                continue
+            for mb in mb_sizes:
+                test_configs.append((n_threads, n_streams, mb))
+    return test_configs
 
 class TestCase(utils.test_case.TestCaseAbs):
     def Prepare(self, parameters, step):
 
+        test_configs = get_test_matrix()
+        num_threads, num_streams, mb = test_configs[step - 1]
+
         parameters.driver_cmd_line_args = [
             'parthenon/mesh/nx1=256',
-            'parthenon/meshblock/nx1=%d' % mb_sizes[step - 1],
+            'parthenon/meshblock/nx1=%d' % mb,
             'parthenon/mesh/nx2=256',
-            'parthenon/meshblock/nx2=%d' % mb_sizes[step - 1],
+            'parthenon/meshblock/nx2=%d' % mb,
             'parthenon/mesh/nx3=256',
-            'parthenon/meshblock/nx3=%d' % mb_sizes[step - 1],
+            'parthenon/meshblock/nx3=%d' % mb,
+            'parthenon/mesh/num_threads=%d' % num_threads,
+            'parthenon/mesh/num_streams=%d' % num_streams,
         ]
-
         return parameters
 
     def Analyse(self, parameters):
 
         perfs = []
         for output in parameters.stdouts:
+            this_perf = []
             for line in output.decode("utf-8").split('\n'):
                 print(line)
-                if 'zone-cycles/omp_wsecond' in line:
-                    perfs.append(float(line.split(' ')[2]))
+                if 'cycle=5' in line or 'cycle=6' in line or 'cycle=7' in line or 'cycle=8' in line or 'cycle=9' in line:
+                    this_perf.append(float(line.split(' ')[5]))
+            perfs.append(this_perf)
 
         perfs = np.array(perfs)
+        test_configs = get_test_matrix()
+
+        if len(perfs) != len(test_configs):
+            print("ERROR: mismatch between expected and reported performance counters")
+            return False
 
         # Plot results
-
         fig, p = plt.subplots(2, 1, figsize = (4,8), sharex=True)
 
-        p[0].loglog(mb_sizes, perfs, label = "$256^3$ Mesh")
-        p[1].loglog(mb_sizes, perfs[0]/perfs)
+        num_envs = len(test_configs) // len(mb_sizes)
+
+        for i in range(num_envs):
+            num_threads, num_streams, unused = test_configs[i*len(mb_sizes)]
+            label = "$256^3$ Mesh; $N_T$ = %d; $N_S$ = %d" % (num_threads, num_streams)
+            perf = perfs[i*len(mb_sizes):(i+1)*len(mb_sizes)]
+            p[0].errorbar(mb_sizes, np.median(perf,axis=1),
+                yerr=(np.max(perf,axis=1) - np.median(perf,axis=1),
+                      np.median(perf,axis=1) - np.min(perf,axis=1)), label=label)
+
+            p[1].loglog(mb_sizes, np.median(perf,axis=1)[0]/np.median(perf,axis=1))
 
         for i in range(2):
             p[i].grid()
-        p[0].legend()
+            p[i].set_xscale('log')
+            p[i].set_yscale('log')
+        p[0].legend(fontsize=8)
         p[0].set_ylabel("zone-cycles/s")
         p[1].set_ylabel("normalized overhead")
         p[1].set_xlabel("Meshblock size")
