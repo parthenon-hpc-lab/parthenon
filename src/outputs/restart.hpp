4b878423aee50dfa6493301274ef58bb5de08474
Sriram Swaminarayan <sriram@lanl.gov>
2020-08-21 11:26:20 -0600

    moved more templated functions to header file because CI fails to find them

--- src/outputs/restart.hpp
+++ src/outputs/restart.hpp
@@ -113,7 +113,48 @@ class RestartReader {
   // Reads an array dataset from file as a 1D vector.
   // Returns number of items read in count if provided
   template <typename T>
-  std::vector<T> ReadDataset(const char *name, size_t *count = nullptr);
+  std::vector<T> ReadDataset(const char *name, size_t *count = nullptr) {
+    // Returns entire 1D array.
+    // status, never checked.  We should...
+#ifdef HDF5OUTPUT
+    herr_t status;
+
+    T *typepointer = nullptr;
+    hid_t theHdfType = getHdfType(typepointer);
+
+    hid_t dataset = H5Dopen2(fh_, name, H5P_DEFAULT);
+    hid_t dataspace = H5Dget_space(dataset);
+
+    // Allocate array of correct size
+    hid_t filespace = H5Dget_space(dataset);
+    int rank = H5Sget_simple_extent_ndims(filespace);
+    std::vector<hsize_t> dims(rank);
+    status = H5Sget_simple_extent_dims(filespace, dims.data(), NULL);
+    hsize_t isize = 1;
+    for (int idir = 0; idir < rank; idir++) {
+      isize = isize * dims[idir];
+    }
+    if (count != nullptr) {
+      *count = isize;
+    }
+
+    std::vector<T> data(isize);
+    /** Define memory dataspace **/
+    hid_t memspace = H5Screate_simple(rank, dims.data(), NULL);
+
+    // Read data from file
+    status = H5Dread(dataset, theHdfType, memspace, dataspace, H5P_DEFAULT,
+                     static_cast<void *>(data.data()));
+
+    // CLose the dataspace and data set.
+    H5Sclose(filespace);
+    H5Sclose(dataspace);
+    H5Dclose(dataset);
+#else
+    std::vector<T> data;
+#endif
+    return data;
+  }
 
   // Reads a string attribute.
   // Returns number of items read in count if provided
@@ -122,9 +163,13 @@ class RestartReader {
 
   // Type specific interface
   std::vector<Real> ReadAttr1DReal(const char *dataset, const char *name,
-                                   size_t *count = nullptr);
+                                   size_t *count = nullptr) {
+    return ReadAttrBytes_<Real>(dataset, name, count);
+  }
   std::vector<int32_t> ReadAttr1DI32(const char *dataset, const char *name,
-                                     size_t *count = nullptr);
+                                     size_t *count = nullptr) {
+    return ReadAttrBytes_<int32_t>(dataset, name, count);
+  }
 
   template <typename T>
   T GetAttr(const char *dataset, const char *name) {
@@ -139,9 +184,50 @@ class RestartReader {
   const std::string filename_;
   // // Reads an array attribute from file.
   // // Returns number of items read in count if provided
+  //! \fn std::vector<T> RestartReader::ReadAttr1D(const char *dataset,
+  //! const char *name, size_t *count = nullptr)
+  //  \brief Reads a 1D array attribute for given dataset
   template <typename T>
   std::vector<T> ReadAttrBytes_(const char *dataset, const char *name,
-                                size_t *count = nullptr);
+                                size_t *count = nullptr) {
+    // Returns entire 1D array.
+    // status, never checked.  We should...
+#ifdef HDF5OUTPUT
+    herr_t status;
+
+    T *typepointer = nullptr;
+    hid_t theHdfType = getHdfType(typepointer);
+
+    hid_t dset = H5Dopen2(fh_, dataset, H5P_DEFAULT);
+    hid_t attr = H5Aopen(dset, name, H5P_DEFAULT);
+    hid_t dataspace = H5Aget_space(attr);
+
+    // Allocate array of correct size
+    int rank = H5Sget_simple_extent_ndims(dataspace);
+    std::vector<hsize_t> dims(rank);
+    status = H5Sget_simple_extent_dims(dataspace, dims.data(), NULL);
+    hsize_t isize = 1;
+    for (int idir = 0; idir < rank; idir++) {
+      isize = isize * dims[idir];
+    }
+    if (count != nullptr) {
+      *count = isize;
+    }
+
+    std::vector<T> data(isize);
+
+    // Read data from file
+    status = H5Aread(attr, theHdfType, static_cast<void *>(data.data()));
+
+    // CLose the dataspace and data set.
+    H5Sclose(dataspace);
+    H5Aclose(attr);
+    H5Dclose(dset);
+#else
+    std::vector<T> data;
+#endif
+    return data;
+  }
 
 #ifdef HDF5OUTPUT
   // Currently all restarts are HDF5 files
@@ -150,22 +236,6 @@ class RestartReader {
   hsize_t nx1_, nx2_, nx3_;
 #endif
 };
-
-#ifndef HDF5OUTPUT
-template <typename T>
-std::vector<T> RestartReader::ReadDataset(const char *name, size_t *count) {
-  std::vector<T> dummy;
-  PARTHENON_FAIL("Restart reader required HDF to be compiled in");
-  return dummy;
-}
-template <typename T>
-std::vector<T> RestartReader::ReadAttrBytes_(const char *dataset, const char *name,
-                                             size_t *count) {
-  std::vector<T> dummy;
-  PARTHENON_FAIL("Restart reader required HDF to be compiled in");
-  return dummy;
-}
-#endif
 
 } // namespace parthenon
 #endif // OUTPUTS_RESTART_HPP_
