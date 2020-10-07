4d1f6453c754d17b9f150e6e3ef2f12fc63ed041
Ben R. Ryan <brryan@lanl.gov>
2020-10-07 09:02:49 -0600

    Formatting

--- src/interface/swarm.hpp
+++ src/interface/swarm.hpp
@@ -25,9 +25,9 @@
 #include <array>
 #include <cstdint>
 #include <list>
-#include <unordered_map>
 #include <memory>
 #include <string>
+#include <unordered_map>
 #include <utility>
 #include <vector>
 
