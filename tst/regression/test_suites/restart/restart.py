756296fe75731d452e328b3a5656186d9866365e
Andrew Gaspar <agaspar@lanl.gov>
2020-10-12 14:49:09 -0600

    Run Restart TestCase.Analyse in a subprocess
    Addresses an issue with hygiene when importing the
    h5py module. Importing h5py causes future MPI operations
    in the process or child process to fail (probably due to
    unpaired MPI_Init).
    
    Fixes #312

--- tst/regression/test_suites/restart/restart.py
+++ tst/regression/test_suites/restart/restart.py
@@ -16,11 +16,11 @@
 #========================================================================================
 
 # Modules
-import h5py
 import math
 import numpy as np
 import sys
 import os
+import subprocess
 import utils.test_case
 """ To prevent littering up imported folders with .pyc files or __pycache_ folder"""
 sys.dont_write_bytecode = True
@@ -44,19 +44,40 @@ class TestCase(utils.test_case.TestCaseAbs):
         return parameters
 
     def Analyse(self, parameters):
-        # spotcheck one variable
-        goldFile = 'gold.out0.00002.rhdf'
-        silverFile = 'silver.out0.00002.rhdf'
+        # HACK: On some systems, including LANL Darwin Power9, importing the
+        # h5py module causes future uses of `mpiexec` to fail - there appears to
+        # be some buried call to `MPI_Init` in the import of `h5py` that causes
+        # this. Therefore, we run the script in a child process for hygenic
+        # purposes.
+        #
+        # For more information, see:
+        # https://github.com/lanl/parthenon/issues/312
+        script = """
+import h5py
+import sys
 
-        gold = h5py.File(goldFile,'r')
-        silver = h5py.File(silverFile,'r')
+# spotcheck one variable
+goldFile = 'gold.out0.00002.rhdf'
+silverFile = 'silver.out0.00002.rhdf'
 
-        varName = "/advected"
-        goldData = gold[varName][:].flatten()
-        silverData = gold[varName][:].flatten()
+gold = h5py.File(goldFile,'r')
+silver = h5py.File(silverFile,'r')
 
-        # spot check on one variable
-        maxdiff = max(abs(goldData-silverData))
-        print('Variable: %s, diff=%g, N=%d'%(varName,maxdiff,len(goldData)))
+varName = "/advected"
+goldData = gold[varName][:].flatten()
+silverData = gold[varName][:].flatten()
 
-        return (maxdiff == 0.0)
+# spot check on one variable
+maxdiff = max(abs(goldData-silverData))
+print('Variable: %s, diff=%g, N=%d'%(varName,maxdiff,len(goldData)))
+
+if maxdiff == 0.0:
+    exit = 0
+else:
+    exit = 1
+
+sys.exit(exit)
+"""
+        proc = subprocess.run([sys.executable, "-c", script])
+
+        return (proc.returncode == 0)
