65bde7ea9f32c8e8d3dca4690b1906b9417ac18d
joshuasbrown <joshua.s.brown@lanl.gov>
2020-11-18 12:03:13 -0700

    More elegant solution

--- scripts/darwin/build_fast.sh
+++ scripts/darwin/build_fast.sh
@@ -5,26 +5,17 @@ source /etc/bashrc
 source /etc/profile
 source /projects/parthenon-int/parthenon-project/.bashrc
 
-function exit_on_error() {
-  local error_code=$?
-  if [[ $error_code != 0 ]]; then
-    exit 1
-  fi
-}
-
+# Exit on error
 set -e
 # Calculate number of available cores
 export J=$(( $(nproc --all) )) && echo Using ${J} cores during build
 
 cmake -S. -Bbuild
-#exit_on_error
 
 cmake --build build
-#exit_on_error
 
 cd build
 
 ctest 
-#exit_on_error
 
 exit 0
