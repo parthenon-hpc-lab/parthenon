7633039acca0456bb463069e5e4b51ff499748ce
joshuasbrown <joshua.s.brown@lanl.gov>
2020-11-18 12:39:09 -0700

    Fix submodules by calling update init

--- scripts/darwin/build_fast.sh
+++ scripts/darwin/build_fast.sh
@@ -8,6 +8,9 @@ source /projects/parthenon-int/parthenon-project/.bashrc
 # Calculate number of available cores
 export J=$(( $(nproc --all) )) && echo Using ${J} cores during build
 
+# Ensure the submodules are pulled in
+git submodule update --init
+
 cmake -S. -Bbuild
 
 cmake --build build
