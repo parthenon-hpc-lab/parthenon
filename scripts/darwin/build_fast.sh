3ef707ac1d4addf6ba6b15aff29e62f1dbfeacf1
joshuasbrown <joshua.s.brown@lanl.gov>
2020-11-18 11:47:42 -0700

    Fail hard on error

--- scripts/darwin/build_fast.sh
+++ scripts/darwin/build_fast.sh
@@ -5,15 +5,25 @@ source /etc/bashrc
 source /etc/profile
 source /projects/parthenon-int/parthenon-project/.bashrc
 
+function exit_on_error() {
+  local error_code=$?
+  echo "Error code is $error_code"
+  if [[ $error_code != 0 ]] then
+    exit 1
+  fi
+}
 # Calculate number of available cores
 export J=$(( $(nproc --all) )) && echo Using ${J} cores during build
 
 cmake -S. -Bbuild
+exit_on_error
 
 cmake --build build
+exit_on_error
 
 cd build
 
 ctest 
+exit_on_error
 
 exit 0
