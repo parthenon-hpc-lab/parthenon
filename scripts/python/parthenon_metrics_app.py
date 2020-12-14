267ba639ee37c2bf837abf928fb2df5e03da2ac2
joshuasbrown <joshua.s.brown@lanl.gov>
2020-12-14 10:47:13 -0700

    Added parthenon metrics application

--- scripts/python/parthenon_metrics_app.py
+++ scripts/python/parthenon_metrics_app.py
@@ -3,18 +3,43 @@ import jwt
 import pem
 import datetime
 import pycurl
-import subprocess
 import json
 import base64
-from urllib.parse import urlencode
 from io import BytesIO
-from shutil import copyfile
 
-from git import Repo
-from git import Head
+class Node:
+  def __init__(self, dir_name = "", rel_path = ""):
+    self.dir = dir_name
+    self.dirs = []
+    self.files = []
+    self.misc = []
+    self.rel_path = rel_path + dir_name
+
+  def insert(self, content, content_type):
+    if content_type == "dir":
+      self.dirs.append(Node(content, self.rel_path + "/"))
+    elif content_type == "file":
+      self.files.append(content)
+    else:
+      self.files.append(content)
+
+  def getNodes(self):
+    return self.dirs
+
+  def getPath(self):
+    return self.rel_path
+
+  def printTree(self):
+    print("Contents in folder: " + self.rel_path)
+    for fil in self.files:
+      print("File " + fil)
+    for mis in self.misc:
+      print("Misc " + mis)
+    for node in self.dirs:
+      node.printTree()
 
 
-# Place binary files in orphan branches
+ Place binary files in orphan branches
 # Place text files in wiki main branch
 class ParthenonApp:
   def __init__(self, pem_file):
@@ -28,16 +53,17 @@ class ParthenonApp:
     self.__branches = []
     self.__branch_current_commit_sha = {}
     self.__api_version = "application/vnd.github.v3+json"
+    self.__parth_root = Node()
 
   def __generateJWT(self,pem_file):
     certs = pem.parse_file(pem_file)
 
-    # 88204 is the app id
+    # iss is the app id
     # Ensuring that we request an access token that expires after a minute
     payload = { 
         'iat': datetime.datetime.utcnow(),
         'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60),
-        'iss': 88204
+        'iss': 92734
         }
     
     self.__jwt_token = jwt.encode(payload,str(certs[0]), algorithm='RS256').decode("utf-8")
@@ -91,16 +117,17 @@ class ParthenonApp:
 
     self.__access_token = js_obj['token']
 
-  def __getBranches(self):
-    header = [
+    self.__header = [
             'Authorization: token '+self.__access_token,
             'Accept: ' + self.__api_version,
             ]
+
+  def __getBranches(self):
     buffer_temp = BytesIO()
     c = pycurl.Curl()
     c.setopt(c.URL, self.__repo_url + "/branches")
     c.setopt(c.WRITEDATA, buffer_temp)
-    c.setopt(c.HTTPHEADER. header)
+    c.setopt(c.HTTPHEADER. self.__header)
     c.perform()
     c.close()
     js_obj_list = json.loads(buffer_temp.getvalue())
@@ -134,11 +161,6 @@ class ParthenonApp:
     if not branch_to_fork_from in branches:
       raise Exception("Cannot create new branch: " + branch + " from " + branch_to_fork_from " because " + branch_to_fork_from + " does not exist.")
 
-    header = [
-            'Authorization: token '+self.__access_token,
-            'Accept: ' + self.__api_version,
-            ]
-
     buffer_temp = BytesIO()
     custom_data = {"ref": "refs/heads/" + branch, "sha": self.__branch_current_commit_sha[branch_to_fork_from]}
     c = pycurl.Curl()
@@ -147,16 +169,11 @@ class ParthenonApp:
     buffer_temp2 = BytesIO(json.dumps(custom_data).encode('utf-8'))
     c2.setopt(c.READDATA, buffer_temp2)
     c.setopt(c.WRITEDATA, buffer_temp)
-    c.setopt(c.HTTPHEADER, header)
+    c.setopt(c.HTTPHEADER, self.__header)
     c.perform()
     c.close()
 
   def getContents(self,branch=self.__default_branch):
-    header = [
-            'Authorization: token '+self.__access_token,
-            'Accept: ' + self.__api_version,
-            ]
-
     buffer_temp = BytesIO()
     # 1. Check if file exists if so get SHA
     custom_data = {"branch":branch}
@@ -165,7 +182,7 @@ class ParthenonApp:
     buffer_temp2 = BytesIO(json.dumps(custom_data).encode('utf-8'))
     c.setopt(c.READDATA, buffer_temp2)
     c.setopt(c.WRITEDATA, buffer_temp)
-    c.setopt(c.HTTPHEADER, header)
+    c.setopt(c.HTTPHEADER, self.__header)
     c.perform()
     c.close()
 
@@ -190,11 +207,11 @@ class ParthenonApp:
 
 
 
-  def uploadFile(self,file_name, branch = self.__default_branch):
+  def upload(self,file_name, branch = self.__default_branch):
 
     if file_name.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
       if branch != self.__default_image_branch:
-        print("Note all images will be uploaded to an orphan branch named: " + self.__default_image_branch)
+        print("Note all images will be uploaded to a branch named: " + self.__default_image_branch)
         branch = self.__default_image_branch
 
     branches = getBranches()
@@ -202,11 +219,6 @@ class ParthenonApp:
     if not branch in branches:
       raise Execption("branch does not exist on repository")
     
-    header = [
-            'Authorization: token '+self.__access_token,
-            'Accept: ' + self.__api_version,
-            ]
-
     contents = getContents(branch)
 
     file_found = False
@@ -238,7 +250,7 @@ class ParthenonApp:
 
     https_url_to_file = self.__repo_url + "/contents/" + name_of_file
     c2 = pycurl.Curl()
-    c2.setopt(c2.HTTPHEADER, header)
+    c2.setopt(c2.HTTPHEADER, self.__header)
     c2.setopt(c2.URL, https_url_to_file)
     c2.setopt(c2.UPLOAD, 1)
     c2.setopt(c2.VERBOSE, True)
@@ -249,91 +261,56 @@ class ParthenonApp:
     c2.perform()
     c2.close()
 
-def get_wiki(token):
-    full_local_path = "/path/to/repo/"
-    username = "test_app"
-    password = token
-    remote = f"https://{username}:{password}@github.com/JoshuaSBrown/ConwaysGameOfLife.wiki.git"
-    if not os.path.isdir("ConwaysGameOfLife.wiki"):
-        print("No repo was found")
-        repo = Repo.clone_from(remote, "ConwaysGameOfLife.wiki")
-    else:
-        print("Dir was found")
-        repo = Repo("ConwaysGameOfLife.wiki")
-        repo.config_writer().set_value("user", "name", username).release()
+def __fillTree(current_node, branch):
+    nodes = current_node.getNodes()
+    for node in nodes:
+      buffer_temp = BytesIO()
+      custom_data = {"branch": branch}
+      buffer_temp2 = BytesIO(json.dumps(custom_data).encode('utf-8'))
+      c = pycurl.Curl()
+      c.setopt(c.URL, self.__repo_url + "/contents/" + node.getPath())
+      c.setopt(c.READDATA, buffer_temp2)
+      c.setopt(c.WRITEDATA, buffer_temp)
+      c.setopt(c.HTTPHEADER, self.__header)
+      c.perform()
+      c.close()
+      js_obj = json.loads(buffer_temp,getvalue())
 
-    os.environ["GIT_PASSWORD"] = password
-    return repo
-    #process = subprocess.Popen(["git", "clone https://github.com/JoshuaSBrown/ConwaysGameOfLife.wiki.git"], stdout=subprocess.PIPE)    
+      if isinstance(js_obj, list):
+        for ob in js_obj:
+          node.insert(ob['name'],ob['type'])
+      else:
+          node.insert(js_obj['name'],js_obj['type'])
 
-def add_file_to_wiki(repo, branch, name_of_file):
-    """ If the file is an image file it will be added to an orphan branch with the branch name equivalent to the file name"""
-    if name_of_file.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.bmp', '.gif')):
-        branch_name = name_of_file.replace("\.","_")
-        git = repo.git
-        hexshas = git.log('--pretty=%H','--follow','--',name_of_file).split('\n') 
-        print("***********")
-        print("Hex sha")
-        print(hexshas)
-        print("************************")
-        repo.git.revert(hexshas, no_edit=True)
-        copyfile(name_of_file, "ConwaysGameOfLife.wiki/"+ str(name_of_file))
-        #for h in hexshas:
-        #    print("Sha is %s" % h)
-        repo.index.add([str(name_of_file)])
-        repo.index.commit("Test commit message adding image file 2", parent_commits=None)
-#        print("Adding orphan branch")
-    else:
-        repo.index.add([str(name_of_file)])
-        repo.index.commit("Adding sample file")
+      self.__fillTree(node, branch)
 
-    repo.git.push("--set-upstream","origin", repo.head.reference)
+def getBranchTree(branch, access_token):
+    buffer_temp = BytesIO()
+    custom_data = {"branch": branch)
+    buffer_temp2 = BytesIO(json,dumps(custom_data).encode('utf-8'))
+    # 1. Check if file exists
+    c = pycurl.Curl()
+    c.setopt(c.URL, self.__repo_url + "/contents" )
+    c.setopt(c.READDATA, buffer_temp2)
+    c.setopt(c.WRITEDATA, buffer_temp)
+    c.setopt(c.HTTPHEADER, self.__header)
+    c.perform()
+    c.close()
 
+    js_obj = json.loads(buffer_temp.getvalue())
+    for obj in js_obj:
+      self.__parth_root.insert(ob['name'],ob['type'])
 
-#    os.chdir("ConwaysGameOfLife.wiki/")
-#    command = "checkout -b " + str(branch)
-#    process = subprocess.Popen(["git",command], stdout=subprocess.PIPE)    
-#    command = "git add " + str(name_of_file)
-#    process = subprocess.Popen(["git",command], stdout=subprocess.PIPE)    
-#    command = 'git commit -m "Adding file"'
-#    process = subprocess.Popen(["git",command], stdout=subprocess.PIPE)    
-#    command = "git push origin " + str(branch)
-#    process = subprocess.Popen(["git",command], stdout=subprocess.PIPE)    
-#
-     
+    self.__fillTree(self.__parth_root, branch)
 
-upload_to_wiki = True
-branch = "test"
+def main(**kwargs):
 
-pem_file = "./jbrown-test-app.2020-12-01.private-key.pem"
+  ParthenonApp app(kwargs.pop('permissions'))
 
-jwt_token = generateJSONWebToken(pem_file) #jwt.encode(payload,str(certs[0]), algorithm='RS256')
+  branch = kwargs.pop('branch')
+  if 'update' in kwargs:
+    app.upload(kwargs.pop('update'), branch)
 
-install_id = getInstallationId(jwt_token)
-
-access_token = createAccessToken(jwt_token, install_id)
-
-name_of_file = "purple_circ.png"
-
-repo = get_wiki(access_token)
-
-print("Trying to print tags")
-for tags in repo.tags:
-    print("Tag is")
-    print(tag)
-
-del os.environ['GIT_PASSWORD']
-add_file_to_wiki(repo, branch,name_of_file)
-#uploadFile(name_of_file, access_token)
-
-# To add a file to an orphan branch
-#        git = repo.git
-#        repo.head.reference = Head(repo, 'refs/heads/'+name_of_file)
-#        index = repo.index
-#        git.reset('--hard')
-#        index.add([str(name_of_file)])
-#        index.commit("Test commit message adding image file", parent_commits=None)
- 
 # Execute main function
 if __name__ == '__main__':
 
@@ -347,13 +324,26 @@ if __name__ == '__main__':
                         required=True,
                         help=desc)
 
+    desc = ('Path to file want to upload.')
+    parser.add_argument('--upload','-u',
+                        type=str,
+                        nargs=1,
+                        required=False,
+                        default = "develop",
+                        help=desc)
+
+
+    desc = ('Branch to use. Default is develop')
+    parser.add_argument('--branch','-b',
+                        type=str,
+                        nargs=1,
+                        required=False,
+                        help=desc)
+
     args = parser.parse_args()
     try:
         main(**vars(args))
     except Exception:
         raise
 
-def main(**kwargs):
-
-  ParthenonApp app(kwargs.pop('permissions'))
 
