cd8c1cd2d88ec0b694d4f3e7a2708d00e405feed
Andrew Gaspar <agaspar@lanl.gov>
2021-01-11 09:19:03 -0700

    Update Triad copyright

--- cmake/CheckCopyright.cmake
+++ cmake/CheckCopyright.cmake
@@ -1,5 +1,5 @@
 #=========================================================================================
-# (C) (or copyright) 2020. Triad National Security, LLC. All rights reserved.
+# (C) (or copyright) 2021. Triad National Security, LLC. All rights reserved.
 #
 # This program was produced under U.S. Government contract 89233218CNA000001 for Los
 # Alamos National Laboratory (LANL), which is operated by Triad National Security, LLC
@@ -29,7 +29,7 @@ set(COPYRIGHTABLE
     LICENSE)
 
 string(TIMESTAMP CURRENT_YEAR "%Y")
-set(LAST_UPDATED 2020)
+set(LAST_UPDATED 2021)
 
 if (NOT CURRENT_YEAR STREQUAL LAST_UPDATED)
     message(WARNING "Triad copyright is out of date. Consider updating.")
