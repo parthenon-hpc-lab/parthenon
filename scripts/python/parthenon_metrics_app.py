ebe81cd64ecb3c793ba5118820e5e4e9f56b5d08
joshuasbrown <joshua.s.brown@lanl.gov>
2021-01-20 14:27:54 -0700

    Fix typo

--- scripts/python/parthenon_metrics_app.py
+++ scripts/python/parthenon_metrics_app.py
@@ -173,7 +173,7 @@ class ParthenonApp(App):
     
     current_branch = os.getenv('CI_COMMIT_BRANCH')
     target_branch = super().getBranchMergingWith(current_branch)
-    wiki_file_name = branch + "_" + target_branch
+    wiki_file_name = current_branch + "_" + target_branch
     pr_wiki_page = os.path.join(str(self.__parthenon_wiki_dir), wiki_file_name + ".md" )
 
     all_dirs = os.listdir(regression_outputs)
