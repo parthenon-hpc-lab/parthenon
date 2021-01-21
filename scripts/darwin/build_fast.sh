bad6d6dbf8a15801c3841f112363ac0fe3bdf413
joshuasbrown <joshua.s.brown@lanl.gov>
2021-01-20 17:18:01 -0700

    Improve status repoting

--- scripts/darwin/build_fast.sh
+++ scripts/darwin/build_fast.sh
@@ -15,12 +15,13 @@ SOURCE=$(dirname "$0")
 GITHUB_APP_PEM="$1"
 export CI_COMMIT_SHA=$2
 export CI_COMMIT_BRANCH=$3
+CI_JOB_URL="$4"
 export GITHUB_APP_PEM="$GITHUB_APP_PEM"
 
 trap 'catch $? $LINENO' ERR
 catch() {
   echo "Error $1 occurred on $2"
-  ${SOURCE}/../python/parthenon_metrics_app.py --status "error"
+  ${SOURCE}/../python/parthenon_metrics_app.py --status "error" --status-url "${CI_JOB_URL}"
   exit $ERR
 }
 
@@ -28,7 +29,7 @@ module load gcc/9.2.0
 spack compiler find
 spack env activate darwin-ppc64le-gcc9-2021-01-20
 echo "build fast $GITHUB_APP_PEM"
-${SOURCE}/../python/parthenon_metrics_app.py -p "${GITHUB_APP_PEM}"  --status "pending"
+${SOURCE}/../python/parthenon_metrics_app.py -p "${GITHUB_APP_PEM}"  --status "pending" --status-context "Building parthenon" --status-url "${CI_JOB_URL}"
 
 # Calculate number of available cores
 export J=$(( $(nproc --all) )) && echo Using ${J} cores during build
@@ -40,6 +41,7 @@ cmake --build build
 
 cd build
 
+${SOURCE}/../python/parthenon_metrics_app.py -p "${GITHUB_APP_PEM}"  --status "pending" --status-context "Running tests" --status-url "${CI_JOB_URL}"
 ctest --output-on-failure -R performance
 
 exit 0
