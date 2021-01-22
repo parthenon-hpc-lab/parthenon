4b451deaec7b332238ca58dfd41ac474410b61ee
joshuasbrown <joshua.s.brown@lanl.gov>
2021-01-21 20:27:13 -0700

    Fix copyright

--- scripts/python/app.py
+++ scripts/python/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #=========================================================================================
-# (C) (or copyright) 2020. Triad National Security, LLC. All rights reserved.
+# (C) (or copyright) 2020-2021. Triad National Security, LLC. All rights reserved.
 #
 # This program was produced under U.S. Government contract 89233218CNA000001 for Los
 # Alamos National Laboratory (LANL), which is operated by Triad National Security, LLC
