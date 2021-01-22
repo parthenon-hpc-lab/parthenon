d744ce99418607bbb7f8ca49f8cf44101c61645c
joshuasbrown <joshua.s.brown@lanl.gov>
2021-01-21 22:25:24 -0700

    Fix typo

--- scripts/python/parthenon_metrics_app.py
+++ scripts/python/parthenon_metrics_app.py
@@ -149,9 +149,9 @@ class ParthenonApp(App):
       ind = 0
       for line in lines:
         line = line.split()
-        print("lines[2] %s" % lines[2])
-        mesh_blocks[ind] = float(lines[2])
-        zone_cycles[ind] = float(lines[0])
+        print("line[2] %s" % line[2])
+        mesh_blocks[ind] = float(line[2])
+        zone_cycles[ind] = float(line[0])
         ind = ind + 1
     return mesh_blocks, zone_cycles
 
