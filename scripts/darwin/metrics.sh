2aaa3722784aa12550f0483b1de20d9ad54d67bf
joshuasbrown <joshua.s.brown@lanl.gov>
2021-01-27 17:20:40 -0700

    Correct ci calls to account for daily scheduled runs

--- scripts/darwin/metrics.sh
+++ scripts/darwin/metrics.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 # Load system env only
-#source /etc/bashrc
-#source /etc/profile
-#source /projects/parthenon-int/parthenon-project/.bashrc
+source /etc/bashrc
+source /etc/profile
+source /projects/parthenon-int/parthenon-project/.bashrc
 source /projects/parthenon-int/parthenon-project/spack/share/spack/setup-env.sh
 
 # Exit on error
@@ -23,11 +23,16 @@ export CI_COMMIT_SHA="${3}"
 export CI_COMMIT_BRANCH="${4}"
 echo "Metrics PEM file $GITHUB_APP_PEM"
 
-data=$(${SOURCE}/../python/parthenon_metrics_app.py -p "${GITHUB_APP_PEM}" --get-target-branch --branch "${CI_COMMIT_BRANCH}")
+if [[ "develop" == "${CI_COMMIT_BRANCH}" ]]; then
+  # This is for the case where we are running on a schedule
+  target_branch="${CI_COMMIT_BRANCH}"
+else
+  data=$(${SOURCE}/../python/parthenon_metrics_app.py -p "${GITHUB_APP_PEM}" --get-target-branch --branch "${CI_COMMIT_BRANCH}")
 
-echo "Get target branch or pr"
-echo ${data}
+  echo "Get target branch or pr"
+  echo ${data}
 
-target_branch=$(echo "$data" | grep "Target branch is:" | awk '{print $4}')
+  target_branch=$(echo "$data" | grep "Target branch is:" | awk '{print $4}')
+fi
 
 ${SOURCE}/../python/parthenon_metrics_app.py -p "${GITHUB_APP_PEM}" --analyze "${BUILD_DIR}/tst/regression/outputs" --create --post-analyze-status --branch "${CI_COMMIT_BRANCH}" --target-branch "$target_branch" --generate-figures-on-analysis
