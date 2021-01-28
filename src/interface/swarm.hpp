4698997a8ce345a0d377320cb701a778443f8109
Ben R. Ryan <brryan@lanl.gov>
2021-01-28 08:54:06 -0700

    Style

--- src/interface/swarm.hpp
+++ src/interface/swarm.hpp
@@ -57,7 +57,6 @@ class SwarmDeviceContext {
   KOKKOS_INLINE_FUNCTION
   int GetNeighborBlockIndex(const int &n, const double &x, const double &y,
                             const double &z) const {
-
     int i = static_cast<int>(std::floor((x - x_min_) / ((x_max_ - x_min_) / 2.))) + 1;
     int j = static_cast<int>(std::floor((y - y_min_) / ((y_max_ - y_min_) / 2.))) + 1;
     int k = static_cast<int>(std::floor((z - z_min_) / ((z_max_ - z_min_) / 2.))) + 1;
