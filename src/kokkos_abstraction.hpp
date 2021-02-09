cda610ade1ccf75d04a65d03f8bfd778ff288ca1
Ben Ryan <brryan@lanl.gov>
2021-02-09 09:26:30 -0700

    Update src/kokkos_abstraction.hpp
    Co-authored-by: Andrew Gaspar <agaspar@lanl.gov>

--- src/kokkos_abstraction.hpp
+++ src/kokkos_abstraction.hpp
@@ -649,7 +649,7 @@ struct DeviceDeleter {
 
 template <typename T, typename ES = DevExecSpace, typename MS = DevMemSpace>
 std::unique_ptr<T, DeviceDeleter<MS>> DeviceAllocate() {
-  PARTHENON_REQUIRE(std::is_trivially_destructible<T>::value,
+  static_assert(std::is_trivially_destructible<T>::value,
                     "DeviceAllocate only supports trivially destructible classes!");
   auto up = std::unique_ptr<T, DeviceDeleter<MS>>(
       static_cast<T *>(Kokkos::kokkos_malloc<MS>(sizeof(T))));
