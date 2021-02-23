d59a41c40b2bc2b835423cdc4fcd717be4d8ca55
Jonas Lippuner <jlippuner@lanl.gov>
2021-02-23 11:04:53 -0600

    Revert "intentionally mess up linting and copyright"
    This reverts commit 265b7dc662b0ebdc4b8cfdf78d7bba91aabedbce.

--- src/mesh/mesh.hpp
+++ src/mesh/mesh.hpp
@@ -21,7 +21,6 @@
 //  The Mesh is the overall grid structure, and MeshBlocks are local patches of data
 //  (potentially on different levels) that tile the entire domain.
 
-#include <chrono>
 #include <cstdint>
 #include <functional>
 #include <map>
