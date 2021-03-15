a1a1d122b19209406129a6798db04895b64b93ac
joshuasbrown <joshua.s.brown@lanl.gov>
2021-03-15 14:39:23 -0600

    Fix import of submodule

--- scripts/python/parthenon_metrics_app.py
+++ scripts/python/parthenon_metrics_app.py
@@ -18,7 +18,7 @@ import os
 import datetime
 import json
 import numpy as np
-from githubapp import GitHubApp
+from parthenon.githubapp import GitHubApp
 import matplotlib.pyplot as plt
 
 
@@ -258,7 +258,7 @@ class PerformanceDataJsonParser():
                             cycles = data_grp.get('zone_cycles')
             return mesh_blocks, cycles
 
-class ParthenonApp(GitHubApp):
+class ParthenonApp(parthenon.githubapp.GitHubApp):
 
     """
     Parthenon App Class
