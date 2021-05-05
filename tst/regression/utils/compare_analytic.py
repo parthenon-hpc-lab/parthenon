5ac946a14e1a7755ce9da9d51c5cffc80de9ac94
Forrest Glines <forrestglines@gmail.com>
2021-05-05 11:59:41 -0400

    Fixed shape of file data in comparison

--- tst/regression/utils/compare_analytic.py
+++ tst/regression/utils/compare_analytic.py
@@ -3,9 +3,10 @@ import numpy as np
 ################################################################################
 #   Sample error functions
 #   
-#   Each accepts the gold and test data in the format data[grid_idx,k,j,i] and
-#   returns a error
+#   Each error function accepts the gold and test data in the format
+#   and returns a error metric
 ################################################################################
+
 def l1_err(gold,test):
     return np.sum(np.abs(gold-test))
 def l2_err(gold,test):
@@ -23,9 +24,34 @@ def linf_rel_err(gold,test):
     mask = (gold != 0)
     return np.max(np.abs((gold[mask]-test[mask])/gold[mask]))
 
+################################################################################
+#   Compare_analytic
+#   
+#   Compares data in filename to a dictionary of analytic functions for some
+#   components
+################################################################################
+
 def compare_analytic(filename,analytic_components,
         err_func=l2_err,tol=1e-12):
-    """
+    """Compares data in filename to analytic gold data in analytic_components.
+
+    Arguments:
+    filename: Filename of ".phdf" file to compare data
+    analytic_components: Dictionary keying component names to analytic functions.
+        Each analytic function in the dictionary takes the arguments:
+
+        def analytic_func(X,Y,Z,t)
+
+        where X,Y,Z comprise arrays of x,y,z coords to compute the analytic solution
+        for a component at time t
+
+    Keyword Arguments:
+    err_func=l2_err: Error function that accepts the analytic solution and data and
+                     returns an error metric
+    tol=1e-12: Tolerance of error metric. 
+
+    Returns True if the error of all components is under the tolerance, otherwise
+    returns False.
     """
 
     try:
@@ -45,7 +71,8 @@ def compare_analytic(filename,analytic_components,
     idx_component_name = 0
 
     #Read all data from the file
-    for var,num_components in zip(datafile.Info["DatasetNames"].astype(str),num_componentss):
+    for var,num_components in zip(datafile.Info["DatasetNames"].astype(str),
+                                  num_componentss):
         dataset = datafile.Get(var,flatten=False)
 
         if num_components != 1:
@@ -82,6 +109,11 @@ def compare_analytic(filename,analytic_components,
                 locations_x[grid_idx],
                 indexing="ij")
 
+    #Flatten the coordinate arrays, for simplicity
+    X = X.ravel()
+    Y = Y.ravel()
+    Z = Z.ravel()
+
     #Check all components for which an analytic version exists
     all_ok = True
     for component in analytic_components.keys():
@@ -90,7 +122,7 @@ def compare_analytic(filename,analytic_components,
         analytic_component = analytic_components[component](X,Y,Z,datafile.Time)
 
         #Compute the error between the file and analytic component
-        err = err_func(analytic_component,file_components[component])
+        err = err_func(analytic_component,file_components[component].ravel())
 
         if err > tol:
             print(f"Component {component} in {filename} error {err} exceeds tolerance {tol}")
