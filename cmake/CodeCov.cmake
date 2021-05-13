1405ba5df52500668cd5239b9a8012275704d476
joshuasbrown <joshua.s.brown@lanl.gov>
2021-05-13 15:21:12 -0600

    Add SHA256 checksum

--- cmake/CodeCov.cmake
+++ cmake/CodeCov.cmake
@@ -25,8 +25,8 @@
 # list(APPEND exclude_tests_with_these_labels "performance;CGS")
 #
 # add_coverage_label("${all_tests}" "${exclude_tests_with_these_labels}")
-# 
-# This will only add the coverage label to tests that do not contain the 
+#
+# This will only add the coverage label to tests that do not contain the
 # performance and CGS labels
 function(add_coverage_label tests )
   if( CODE_COVERAGE )
@@ -35,7 +35,7 @@ function(add_coverage_label tests )
       get_test_property(${CHECK_TEST} LABELS TEST_LABELS)
       foreach( exclude_if_contains_this_label ${ARGN})
         if( ${exclude_if_contains_this_label} IN_LIST TEST_LABELS)
-          set(exclude TRUE) 
+          set(exclude TRUE)
           continue()
         endif()
       endforeach()
@@ -49,7 +49,7 @@ endfunction()
 
 # This function creates the code coverage targets for parthenon:
 #
-# coverage - this target will build the coverage reports by running all tests 
+# coverage - this target will build the coverage reports by running all tests
 #            with the coverage label.
 #
 # make coverage
@@ -58,8 +58,8 @@ endfunction()
 #
 # make coverage-upload
 #
-# The 'create_parthenon_coverage_targets' should only be called after the parthenon 
-# library has been defined. 
+# The 'create_parthenon_coverage_targets' should only be called after the parthenon
+# library has been defined.
 function(create_pathenon_coverage_targets)
   if(CODE_COVERAGE)
 
@@ -68,18 +68,18 @@ function(create_pathenon_coverage_targets)
 
     if(NOT PATH_GCOV)
         message(FATAL_ERROR "Unable to build with code coverage gcov was not found.")
-    endif() 
+    endif()
 
     if(NOT PATH_LCOV)
         message(FATAL_ERROR "Unable to build with code coverage lcov was not found.")
-    endif() 
+    endif()
 
     if(NOT CMAKE_BUILD_TYPE STREQUAL "Debug")
         message(WARNING "Using code coverage with an optimized build is discouraged, as it may lead to misleading results.")
-    endif() 
+    endif()
 
     message(STATUS "Coverage reports will be placed in ${COVERAGE_PATH}/${COVERAGE_NAME}")
-   
+
     get_target_property(PARTHENON_SOURCES parthenon SOURCES)
     get_target_property(UNIT_TEST_SOURCES unit_tests SOURCES)
 
@@ -101,7 +101,7 @@ function(create_pathenon_coverage_targets)
       COMMAND ${PATH_LCOV} --gcov-tool ${PATH_GCOV} --directory ${PROJECT_BINARY_DIR} -b ${PROJECT_SOURCE_DIR} --capture --output-file ${COVERAGE_PATH}/${COVERAGE_NAME}/report.test.old
       # Remove Kokkos and tst info from code coverage
       COMMAND ${PATH_LCOV} --remove ${COVERAGE_PATH}/${COVERAGE_NAME}/report.test.old "*/Kokkos/*" "*/tst/*" -o ${COVERAGE_PATH}/${COVERAGE_NAME}/report.test
-      
+
       # Combining base line counters with counters from running tests
       COMMAND ${PATH_LCOV} --gcov-tool ${PATH_GCOV} -a ${COVERAGE_PATH}/${COVERAGE_NAME}/report.base -a ${COVERAGE_PATH}/${COVERAGE_NAME}/report.test --output-file ${COVERAGE_PATH}/${COVERAGE_NAME}/report.all
       # Remove unneeded reports
@@ -112,13 +112,12 @@ function(create_pathenon_coverage_targets)
       WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
       )
 
-    set(UPLOAD_COMMAND "bash <\(curl -s https://codecov.io/bash\) \|\| echo \"code coverage failed to upload\"")
-    add_custom_target(coverage-upload) 
+    add_custom_target(coverage-upload)
     add_custom_command(TARGET coverage-upload
       COMMAND echo "================ Uploading Code Coverage =================="
       # Upload coverage report
-      COMMAND ${PROJECT_SOURCE_DIR}/scripts/combine_coverage.sh ${PATH_LCOV} ${PATH_GCOV} ${COVERAGE_PATH}
-      COMMAND curl -s https://codecov.io/bash > ${COVERAGE_PATH}/CombinedCoverage/script.coverage
+      COMMAND ${parthenon_SOURCE_DIR}/scripts/combine_coverage.sh ${PATH_LCOV} ${PATH_GCOV} ${COVERAGE_PATH}
+      COMMAND ${CMAKE_COMMAND} -DCOVERAGE_PATH=${COVERAGE_PATH} -P "${parthenon_SOURCE_DIR}/cmake/CodeCovBashDownloadScript.cmake"
       COMMAND cat ${COVERAGE_PATH}/CombinedCoverage/script.coverage
       COMMAND cd ${COVERAGE_PATH}/CombinedCoverage && bash ${COVERAGE_PATH}/CombinedCoverage/script.coverage -p ${PROJECT_BINARY_DIR} -s ${COVERAGE_PATH}/CombinedCoverage
       WORKING_DIRECTORY ${COVERAGE_PATH}
---
+++ cmake/CodeCovBashDownloadScript.cmake
@@ -0,0 +1,1 @@
+file(DOWNLOAD https://codecov.io/bash ${COVERAGE_PATH}/CombinedCoverage/script.coverage EXPECTED_HASH SHA256=d6aa3207c4908d123bd8af62ec0538e3f2b9f257c3de62fad4e29cd3b59b41d9)
