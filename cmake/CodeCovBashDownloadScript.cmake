e6ccdf64b3963e69aaa91427980741ff71e79fba
Joshua S Brown <joshua.s.brown@lanl.gov>
2021-05-13 23:35:14 -0600

    Add missing copyright.

--- cmake/CodeCovBashDownloadScript.cmake
+++ cmake/CodeCovBashDownloadScript.cmake
@@ -1,1 +1,13 @@
+#=========================================================================================
+# (C) (or copyright) 2020-2021. Triad National Security, LLC. All rights reserved.
+#
+# This program was produced under U.S. Government contract 89233218CNA000001 for Los
+# Alamos National Laboratory (LANL), which is operated by Triad National Security, LLC
+# for the U.S. Department of Energy/National Nuclear Security Administration. All rights
+# in the program are reserved by Triad National Security, LLC, and the U.S. Department
+# of Energy/National Nuclear Security Administration. The Government is granted for
+# itself and others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide
+# license in this material to reproduce, prepare derivative works, distribute copies to
+# the public, perform publicly and display publicly, and to permit others to do so.
+#=========================================================================================
 file(DOWNLOAD https://codecov.io/bash ${COVERAGE_PATH}/CombinedCoverage/script.coverage EXPECTED_HASH SHA256=d6aa3207c4908d123bd8af62ec0538e3f2b9f257c3de62fad4e29cd3b59b41d9)
