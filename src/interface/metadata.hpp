1591089fe120810d227409bfaf57ef086c181913
Jonas Lippuner <jlippuner@lanl.gov>
2021-05-17 11:34:49 -0500

    small fix

--- src/interface/metadata.hpp
+++ src/interface/metadata.hpp
@@ -174,7 +174,7 @@ class Metadata {
   // 5 constructors, this is the general constructor called by all other constructors, so
   // we do some sanity checks here
   Metadata(const std::vector<MetadataFlag> &bits, const std::vector<int> &shape = {1},
-           int sparse_id = -1, const std::vector<std::string> component_labels = {},
+           int sparse_id = -1, const std::vector<std::string> &component_labels = {},
            const std::string &associated = "")
       : shape_(shape), sparse_id_(sparse_id), component_labels_(component_labels),
         associated_(associated) {
