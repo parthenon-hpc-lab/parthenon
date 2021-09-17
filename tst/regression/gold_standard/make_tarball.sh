90cd230692b9113e683fd30c632ee5e48f573363
Jonas Lippuner <jlippuner@lanl.gov>
2021-09-17 13:55:24 -0500

    add sparse_advection regression test

---
+++ tst/regression/gold_standard/make_tarball.sh
@@ -0,0 +1,26 @@
+# ========================================================================================
+# (C) (or copyright) 2021. Triad National Security, LLC. All rights reserved.
+#
+# This program was produced under U.S. Government contract 89233218CNA000001 for Los
+# Alamos National Laboratory (LANL), which is operated by Triad National Security, LLC
+# for the U.S. Department of Energy/National Nuclear Security Administration. All rights
+# in the program are reserved by Triad National Security, LLC, and the U.S. Department
+# of Energy/National Nuclear Security Administration. The Government is granted for
+# itself and others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide
+# license in this material to reproduce, prepare derivative works, distribute copies to
+# the public, perform publicly and display publicly, and to permit others to do so.
+# ========================================================================================
+
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+  echo "Usage: ${0}: VERSION"
+  exit 1
+fi
+
+outname="parthenon_regression_gold_v$1.tgz"
+
+tar czf $outname *.phdf *.phdf.xdmf current_version README.md
+
+echo "Created tarball $outname, SHA-256 hash:"
+sha256sum $outname
---
+++ tst/regression/test_suites/sparse_advection/sparse_advection.py
@@ -0,0 +1,54 @@
+# ========================================================================================
+# Parthenon performance portable AMR framework
+# Copyright(C) 2020 The Parthenon collaboration
+# Licensed under the 3-clause BSD License, see LICENSE file for details
+# ========================================================================================
+# (C) (or copyright) 2021. Triad National Security, LLC. All rights reserved.
+#
+# This program was produced under U.S. Government contract 89233218CNA000001 for Los
+# Alamos National Laboratory (LANL), which is operated by Triad National Security, LLC
+# for the U.S. Department of Energy/National Nuclear Security Administration. All rights
+# in the program are reserved by Triad National Security, LLC, and the U.S. Department
+# of Energy/National Nuclear Security Administration. The Government is granted for
+# itself and others acting on its behalf a nonexclusive, paid-up, irrevocable worldwide
+# license in this material to reproduce, prepare derivative works, distribute copies to
+# the public, perform publicly and display publicly, and to permit others to do so.
+# ========================================================================================
+
+# Modules
+import sys
+import utils.test_case
+
+""" To prevent littering up imported folders with .pyc files or __pycache_ folder"""
+sys.dont_write_bytecode = True
+
+
+class TestCase(utils.test_case.TestCaseAbs):
+    def Prepare(self, parameters, step):
+
+        parameters.coverage_status = "both"
+        return parameters
+
+    def Analyse(self, parameters):
+
+        sys.path.insert(
+            1,
+            parameters.parthenon_path
+            + "/scripts/python/packages/parthenon_tools/parthenon_tools",
+        )
+
+        try:
+            from phdf_diff import compare
+        except ModuleNotFoundError:
+            print("Couldn't find module to compare Parthenon hdf5 files.")
+            return False
+
+        delta = compare(
+            [
+                "sparse.out0.00002.phdf",
+                parameters.parthenon_path
+                + "/tst/regression/gold_standard/sparse.out0.00002.phdf",
+            ]
+        )
+
+        return delta == 0
