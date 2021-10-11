bba38aa41eac6d53388a5ba55c1e7461a0840b19
Philipp Grete <grete@pa.msu.edu>
2021-10-11 09:51:03 -0400

    Allow modification of SimTime in PreStepUserWorkInLoop

--- src/mesh/mesh.hpp
+++ src/mesh/mesh.hpp
@@ -147,7 +147,7 @@ class Mesh {
   static void UserWorkInLoopDefault(
       Mesh *, ParameterInput *,
       SimTime const &); // default behavior for pre- and post-step user work
-  std::function<void(Mesh *, ParameterInput *, SimTime const &)> PreStepUserWorkInLoop =
+  std::function<void(Mesh *, ParameterInput *, SimTime &)> PreStepUserWorkInLoop =
       &UserWorkInLoopDefault;
   std::function<void(Mesh *, ParameterInput *, SimTime const &)> PostStepUserWorkInLoop =
       &UserWorkInLoopDefault;
