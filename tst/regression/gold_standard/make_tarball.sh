6f544587cb2d93f71f3dda7d0d43cf537bea7901
Jonas Lippuner <jlippuner@lanl.gov>
2021-10-14 14:39:33 -0500

    update gold file hash, improve make_tarball.sh

--- tst/regression/gold_standard/make_tarball.sh
+++ tst/regression/gold_standard/make_tarball.sh
@@ -20,7 +20,47 @@ fi
 
 outname="parthenon_regression_gold_v$1.tgz"
 
+# check if output tarball already exists
+if [[ -f "$outname" ]]; then
+  read -p "WARNING: Output file ${outname} already exists. Overwrite? [yN] " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Aborting"
+    exit 1
+  fi
+fi
+
+# check if contents of current_version file matches the version we're creating
+curr_v=`cat current_version`
+if [[ "${curr_v}" != "${1}" ]]; then
+  read -p "WARNING: Contents of 'current_version' is '${curr_v}', but making tarball for version '${1}'. Update 'current_version' file? [yN] " -n 1 -r
+  echo    # (optional) move to a new line
+  if [[ ! $REPLY =~ ^[Yy]$ ]]; then
+    echo "Aborting"
+    exit 1
+  else
+    echo "Updating 'current_version' file"
+    echo
+    echo ${1} > current_version
+  fi
+fi
+
+# check if version is present in README.md
+if ! grep -q "${1}:" README.md; then
+  echo "ERROR: README.md does not have entry for version '${1}'. Please update README.md"
+  echo "Aborting"
+  exit 1
+fi
+
+echo
+echo "Adding the following files to tarball:"
+ls -lah *.phdf *.phdf.xdmf current_version README.md
+
+echo
+
 tar czf "$outname" *.phdf *.phdf.xdmf current_version README.md
 
 echo "Created tarball $outname, SHA-512 hash:"
+echo
+
 sha512sum "$outname"
