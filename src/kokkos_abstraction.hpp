653d4ac2562a1b5863ac63c904dc509ccfffbb33
Josh Dolence <jdolence@lanl.gov>
2021-11-24 16:54:15 -0700

    forwarding call to 1d TTR inner loop to TVR pattern

--- src/kokkos_abstraction.hpp
+++ src/kokkos_abstraction.hpp
@@ -657,6 +657,12 @@ KOKKOS_INLINE_FUNCTION void par_for_inner(InnerLoopPatternTTR, team_mbr_t team_m
     function(j, i);
   });
 }
+template <typename Function>
+KOKKOS_INLINE_FUNCTION void par_for_inner(InnerLoopPatternTTR, team_mbr_t team_member,
+                                          const int il, const int iu,
+                                          const Function &function) {
+  par_for_inner(inner_loop_pattern_tvr_tag, team_member, il, iu, function);
+}
 // Inner parallel loop using TeamVectorRange
 template <typename Function>
 KOKKOS_INLINE_FUNCTION void par_for_inner(InnerLoopPatternTVR, team_mbr_t team_member,
