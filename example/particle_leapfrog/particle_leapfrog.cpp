e7422595f6f23c02f711d7f123963a34e8bea844
Ben R. Ryan <brryan@lanl.gov>
2022-03-01 08:20:35 -0700

    copyright

--- example/particle_leapfrog/particle_leapfrog.cpp
+++ example/particle_leapfrog/particle_leapfrog.cpp
@@ -1,9 +1,9 @@
 //========================================================================================
 // Parthenon performance portable AMR framework
-// Copyright(C) 2021 The Parthenon collaboration
+// Copyright(C) 2021-2022 The Parthenon collaboration
 // Licensed under the 3-clause BSD License, see LICENSE file for details
 //========================================================================================
-// (C) (or copyright) 2020-2021. Triad National Security, LLC. All rights reserved.
+// (C) (or copyright) 2020-2022. Triad National Security, LLC. All rights reserved.
 //
 // This program was produced under U.S. Government contract 89233218CNA000001 for Los
 // Alamos National Laboratory (LANL), which is operated by Triad National Security, LLC
