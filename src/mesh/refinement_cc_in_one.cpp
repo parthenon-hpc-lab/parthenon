f3908c14363d7fb7b829d2e532eba908cd71fa08
Benjamin Ransom Ryan <brryan@lanl.gov>
2022-04-20 09:05:00 -0600

    Add comment about nbuffers = 1

--- src/mesh/refinement_cc_in_one.cpp
+++ src/mesh/refinement_cc_in_one.cpp
@@ -35,6 +35,8 @@ void Restrict(cell_centered_bvars::BufferCache_t &info, IndexShape &cellbounds,
   auto jb = cellbounds.GetBoundsJ(interior);
   auto ib = cellbounds.GetBoundsI(interior);
 
+  // TODO(BRR) nbuffers is currently always 1. In the future nbuffers could be > 1 to
+  // improve performance.
   const int nbuffers = info.extent_int(0);
   const int scratch_level = 1; // 0 is actual scratch (tiny); 1 is HBM
   size_t scratch_size_in_bytes = 1;
