f2ad5e57277aecaed2a9d44ef196c16b4687bf7e
Ben Ryan <brryan@lanl.gov>
2022-06-06 11:26:16 -0600

    Switch from char[STRLEN] to int type for key

--- tst/unit/test_unit_sort.cpp
+++ tst/unit/test_unit_sort.cpp
@@ -34,14 +34,10 @@ struct Key {
   KOKKOS_INLINE_FUNCTION
   Key() {}
   KOKKOS_INLINE_FUNCTION
-  Key(const int key, const char value[STRLEN]) : key_(key) {
-    for (int n = 0; n < STRLEN; n++) {
-      value_[n] = value[n];
-    }
-  }
+  Key(const int key, const int value) : key_(key), value_(value) {}
 
   int key_;
-  char value_[STRLEN];
+  int value_;
 };
 struct KeyComparator {
   KOKKOS_INLINE_FUNCTION
@@ -75,15 +71,15 @@ TEST_CASE("Sorting", "[sort]") {
         parthenon::loop_pattern_flatrange_tag, "initial data", parthenon::DevExecSpace(),
         0, 5 - 1, KOKKOS_LAMBDA(const int n) {
           if (n == 0) {
-            data(n) = Key(5, "test.");
+            data(n) = Key(5, 5);
           } else if (n == 1) {
-            data(n) = Key(4, "a");
+            data(n) = Key(4, 4);
           } else if (n == 3) {
-            data(n) = Key(3, "is");
+            data(n) = Key(3, 3);
           } else if (n == 2) {
-            data(n) = Key(2, "sentence");
+            data(n) = Key(2, 2);
           } else if (n == 4) {
-            data(n) = Key(1, "This");
+            data(n) = Key(1, 1);
           }
         });
 
@@ -92,11 +88,11 @@ TEST_CASE("Sorting", "[sort]") {
     auto data_h = Kokkos::create_mirror_view(data);
     Kokkos::deep_copy(data_h, data);
 
-    REQUIRE(data_h(0).value_ == std::string("This"));
-    REQUIRE(data_h(1).value_ == std::string("sentence"));
-    REQUIRE(data_h(2).value_ == std::string("is"));
-    REQUIRE(data_h(3).value_ == std::string("a"));
-    REQUIRE(data_h(4).value_ == std::string("test."));
+    REQUIRE(data_h(0).value_ == 1);
+    REQUIRE(data_h(1).value_ == 2);
+    REQUIRE(data_h(2).value_ == 3);
+    REQUIRE(data_h(3).value_ == 4);
+    REQUIRE(data_h(4).value_ == 5);
   }
 #endif // !defined(KOKKOS_ENABLE_HIP)
 }
