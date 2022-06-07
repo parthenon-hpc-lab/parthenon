ed08b8964e00b916227146c3cdaeeff2c021c43b
Luke Roberts <lfroberts@lanl.gov>
2022-06-07 13:07:03 -0600

    Refactor ParArrayNDGeneric to work for rank views

--- src/parthenon_arrays.hpp
+++ src/parthenon_arrays.hpp
@@ -31,305 +31,213 @@
 
 // Macro for automatically creating a useful name
 #define PARARRAY_TEMP                                                                    \
-  "ParArrayNDGeneric:" + std::string(__FILE__) + ":" + std::to_string(__LINE__)
+  "ParArrayND:" + std::string(__FILE__) + ":" + std::to_string(__LINE__)
 
 namespace parthenon {
 
+template<bool...> struct bool_pack;
+template<bool... bs> 
+using all_true = std::is_same<bool_pack<bs..., true>, bool_pack<true, bs...>>;
+
+template<class... Ts>
+using are_all_integral = all_true<std::is_integral<Ts>::value...>;
+
+template<class T> bool all_greater_than(T) {return true;}
+template<class T, class Arg, class... Args> 
+bool all_greater_than(T val, Arg v, Args... args) {
+  return (v > val) && all_greater_than(val, args...);
+}
+
 // API designed with Data = Kokkos::View<T******> in mind
 template <typename Data>
-class ParArrayNDGeneric {
+class ParArrayGeneric {
  public:
   using index_pair_t = std::pair<size_t, size_t>;
+  
+  ParArrayGeneric() = default;
+  __attribute__((nothrow)) ParArrayGeneric(const ParArrayGeneric<Data> &t) = default;
+  __attribute__((nothrow)) ~ParArrayGeneric() = default;
+  __attribute__((nothrow)) ParArrayGeneric<Data> &
+  operator=(const ParArrayGeneric<Data> &t) = default;
+  __attribute__((nothrow)) ParArrayGeneric(ParArrayGeneric<Data> &&t) = default;
+  __attribute__((nothrow)) ParArrayGeneric<Data> &
+  operator=(ParArrayGeneric<Data> &&t) = default;
 
-  ParArrayNDGeneric() = default;
-  ParArrayNDGeneric(const std::string &label, int nx6, int nx5, int nx4, int nx3, int nx2,
-                    int nx1) {
-    assert(nx6 > 0 && nx5 > 0 && nx4 > 0 && nx3 > 0 && nx2 > 0 && nx1 > 0);
-    d6d_ = Data(label, nx6, nx5, nx4, nx3, nx2, nx1);
-  }
-  ParArrayNDGeneric(const std::string &label, int nx5, int nx4, int nx3, int nx2,
-                    int nx1) {
-    assert(nx5 > 0 && nx4 > 0 && nx3 > 0 && nx2 > 0 && nx1 > 0);
-    d6d_ = Data(label, 1, nx5, nx4, nx3, nx2, nx1);
-  }
-  ParArrayNDGeneric(const std::string &label, int nx4, int nx3, int nx2, int nx1) {
-    assert(nx4 > 0 && nx3 > 0 && nx2 > 0 && nx1 > 0);
-    d6d_ = Data(label, 1, 1, nx4, nx3, nx2, nx1);
-  }
-  ParArrayNDGeneric(const std::string &label, int nx3, int nx2, int nx1) {
-    assert(nx3 > 0 && nx2 > 0 && nx1 > 0);
-    d6d_ = Data(label, 1, 1, 1, nx3, nx2, nx1);
-  }
-  ParArrayNDGeneric(const std::string &label, int nx2, int nx1) {
-    assert(nx2 > 0 && nx1 > 0);
-    d6d_ = Data(label, 1, 1, 1, 1, nx2, nx1);
-  }
-  ParArrayNDGeneric(const std::string &label, int nx1) {
-    assert(nx1 > 0);
-    d6d_ = Data(label, 1, 1, 1, 1, 1, nx1);
-  }
   KOKKOS_INLINE_FUNCTION
-  explicit ParArrayNDGeneric(const Data &v) : d6d_(v) {}
+  explicit ParArrayGeneric(const Data &v) : data_(v) {}
 
-  // legacy functions, as requested for backwards compatibility
-  // with athena++ patterns
-  void NewParArrayND(int nx1, const std::string &label = "ParArray1D") {
-    assert(nx1 > 0);
-    d6d_ = Data(label, 1, 1, 1, 1, 1, nx1);
-  }
-  void NewParArrayND(int nx2, int nx1, const std::string &label = "ParArray2D") {
-    assert(nx2 > 0 && nx1 > 0);
-    d6d_ = Data(label, 1, 1, 1, 1, nx2, nx1);
-  }
-  void NewParArrayND(int nx3, int nx2, int nx1, const std::string &label = "ParArray3D") {
-    assert(nx3 > 0 && nx2 > 0 && nx1 > 0);
-    d6d_ = Data(label, 1, 1, 1, nx3, nx2, nx1);
-  }
-  void NewParArrayND(int nx4, int nx3, int nx2, int nx1,
-                     const std::string &label = "ParArray4D") {
-    assert(nx4 > 0 && nx3 > 0 && nx2 > 0 && nx1 > 0);
-    d6d_ = Data(label, 1, 1, nx4, nx3, nx2, nx1);
-  }
-  void NewParArrayND(int nx5, int nx4, int nx3, int nx2, int nx1,
-                     const std::string &label = "ParArray5D") {
-    assert(nx5 > 0 && nx4 > 0 && nx3 > 0 && nx2 > 0 && nx1 > 0);
-    d6d_ = Data(label, 1, nx5, nx4, nx3, nx2, nx1);
-  }
-  void NewParArrayND(int nx6, int nx5, int nx4, int nx3, int nx2, int nx1,
-                     const std::string &label = "ParArray6D") {
-    assert(nx6 > 0 && nx5 > 0 && nx4 > 0 && nx3 > 0 && nx2 > 0 && nx1 > 0);
-    d6d_ = Data(label, nx6, nx5, nx4, nx3, nx2, nx1);
+  // Allow a ParArrayGeneric to be cast to its underlying Kokkos view 
+  KOKKOS_FORCEINLINE_FUNCTION
+  operator Data() { return data_; }
+  
+  template<class... Args, 
+           class = typename std::enable_if<are_all_integral<Args...>::value>::type>
+  ParArrayGeneric(const std::string &label, Args... args) : 
+      ParArrayGeneric(label, std::make_index_sequence<Data::rank - sizeof...(Args)>{}, args...)
+  {
+    assert(all_greater_than(0, args...));
+    static_assert(Data::rank - sizeof...(Args) >= 0);
   }
 
-  __attribute__((nothrow)) ParArrayNDGeneric(const ParArrayNDGeneric<Data> &t) = default;
-  __attribute__((nothrow)) ~ParArrayNDGeneric() = default;
-  __attribute__((nothrow)) ParArrayNDGeneric<Data> &
-  operator=(const ParArrayNDGeneric<Data> &t) = default;
-  __attribute__((nothrow)) ParArrayNDGeneric(ParArrayNDGeneric<Data> &&t) = default;
-  __attribute__((nothrow)) ParArrayNDGeneric<Data> &
-  operator=(ParArrayNDGeneric<Data> &&t) = default;
+  template<class... Args, 
+           class = typename std::enable_if<are_all_integral<Args...>::value>::type>
+  void NewParArrayND(Args... args, const std::string &label = "ParArrayND") {
+    assert(all_greater_than(0, args...));
+    static_assert(Data::rank - sizeof...(Args) >= 0);
+    NewParArrayND(std::make_index_sequence<Data::rank - sizeof...(Args)>{}, args..., label);
+  }
+
+  template<class... Args, 
+           class = typename std::enable_if<are_all_integral<Args...>::value>::type>
+  KOKKOS_FORCEINLINE_FUNCTION  
+  auto Get(Args... args) const {
+    static_assert(Data::rank - sizeof...(Args) >= 0);
+    return Get(std::make_index_sequence<Data::rank - sizeof...(Args)>{}, args...);
+  }
+  
+  template<class... Args, 
+           class = typename std::enable_if<are_all_integral<Args...>::value>::type> 
+  void Resize(Args... args) {
+    static_assert(Data::rank - sizeof...(Args) >= 0);
+    Resize(std::make_index_sequence<Data::rank - sizeof...(Args)>{}, args...);
+  }
+  
+  template<class... Args, 
+           class = typename std::enable_if<are_all_integral<Args...>::value>::type> 
+  KOKKOS_FORCEINLINE_FUNCTION
+  auto &operator() (Args... args) const {
+    static_assert(Data::rank - sizeof...(Args) >= 0);
+    return _operator_impl(std::make_index_sequence<Data::rank - sizeof...(Args)>{}, args...);
+  }
 
   // function to get the label
-  std::string label() const { return d6d_.label(); }
+  std::string label() const { return data_.label(); }
 
   // functions to get array dimensions
   KOKKOS_INLINE_FUNCTION int GetDim(const int i) const {
-    assert(0 < i && i <= 6 && "ParArrayNDGenerics are max 6D");
-    return d6d_.extent_int(6 - i);
+    assert(0 < i && i <= Data::rank);
+    return data_.extent_int(Data::rank - i);
   }
 
   // a function to get the total size of the array
   KOKKOS_INLINE_FUNCTION int GetSize() const {
-    return GetDim(1) * GetDim(2) * GetDim(3) * GetDim(4) * GetDim(5) * GetDim(6);
+    return data_.size(); 
+    // TODO(LFR) : Make sure there is no inconsistency here 
+    //return GetDim(1) * GetDim(2) * GetDim(3) * GetDim(4) * GetDim(5) * GetDim(6);
   }
 
   template <typename MemSpace>
   auto GetMirror(MemSpace const &memspace) {
-    auto mirror = Kokkos::create_mirror_view(memspace, d6d_);
-    return ParArrayNDGeneric<decltype(mirror)>(mirror);
+    auto mirror = Kokkos::create_mirror_view(memspace, data_);
+    return ParArrayGeneric<decltype(mirror)>(mirror);
   }
   auto GetHostMirror() { return GetMirror(Kokkos::HostSpace()); }
   auto GetDeviceMirror() { return GetMirror(Kokkos::DefaultExecutionSpace()); }
 
   template <typename Other>
   void DeepCopy(const Other &src) {
-    Kokkos::deep_copy(d6d_, src.Get());
+    Kokkos::deep_copy(data_, src.Get());
   }
 
   template <typename MemSpace>
   auto GetMirrorAndCopy(MemSpace const &memspace) {
-    auto mirror = Kokkos::create_mirror_view_and_copy(memspace, d6d_);
-    return ParArrayNDGeneric<decltype(mirror)>(mirror);
+    auto mirror = Kokkos::create_mirror_view_and_copy(memspace, data_);
+    return ParArrayGeneric<decltype(mirror)>(mirror);
   }
   auto GetHostMirrorAndCopy() { return GetMirrorAndCopy(Kokkos::HostSpace()); }
-
-  // JMM: DO NOT put noexcept here. It somehow interferes with inlining
-  // and the code slows down by a factor of 5.
-  KOKKOS_FORCEINLINE_FUNCTION
-  auto &operator()(const int n) const { return d6d_(0, 0, 0, 0, 0, n); }
-  KOKKOS_FORCEINLINE_FUNCTION
-  auto &operator()(const int n, const int i) const { return d6d_(0, 0, 0, 0, n, i); }
-  KOKKOS_FORCEINLINE_FUNCTION
-  auto &operator()(const int n, const int j, const int i) const {
-    return d6d_(0, 0, 0, n, j, i);
-  }
-  KOKKOS_FORCEINLINE_FUNCTION
-  auto &operator()(const int n, const int k, const int j, const int i) const {
-    return d6d_(0, 0, n, k, j, i);
-  }
-  KOKKOS_FORCEINLINE_FUNCTION
-  auto &operator()(const int m, const int n, const int k, const int j,
-                   const int i) const {
-    return d6d_(0, m, n, k, j, i);
-  }
-  KOKKOS_FORCEINLINE_FUNCTION
-  auto &operator()(const int p, const int m, const int n, const int k, const int j,
-                   const int i) const {
-    return d6d_(p, m, n, k, j, i);
-  }
+  
   template <typename... Args>
   KOKKOS_INLINE_FUNCTION auto Slice(Args... args) const {
-    auto v = Kokkos::subview(d6d_, std::forward<Args>(args)...);
-    return ParArrayNDGeneric<decltype(v)>(v);
+    auto v = Kokkos::subview(data_, std::forward<Args>(args)...);
+    return ParArrayGeneric<decltype(v)>(v);
   }
 
   // AthenaArray.InitWithShallowSlice(src,dim,indx,nvar)
-  // translates into auto dest = src.SliceD<dim>(std::make_pair(indx,indx+nvar))
-  // Call me as SliceD<N>(slc);
-  template <std::size_t N = 6>
-  auto
-  SliceD(index_pair_t slc,
-         std::integral_constant<int, N> ic = std::integral_constant<int, N>{}) const {
-    return SliceD(slc, ic);
+  // translates into auto dest = src.SliceD<dim>(std::make_pair(indx,indx+nvar)) 
+  template<std::size_t N = Data::rank> 
+  auto SliceD(index_pair_t slc) const {
+    static_assert(N <= Data::rank);
+    static_assert(N > 0);
+    return SliceD(std::make_index_sequence<Data::rank - N>{}, std::make_index_sequence<N - 1>{}, slc);  
   }
 
   // AthenaArray.InitWithShallowSlice(src,dim,indx,nvar)
   // translates into auto dest = src.SliceD<dim>(indx,nvar)
-  template <std::size_t N = 6>
-  auto SliceD(const int indx, const int nvar,
-              std::integral_constant<int, N> ic = std::integral_constant<int, N>{}) {
-    return SliceD(std::make_pair(indx, indx + nvar), ic);
+  template <std::size_t N = Data::rank>
+  auto SliceD(const int indx, const int nvar) {
+    return SliceD<N>(std::make_pair(indx, indx + nvar));
   }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(const int i) const {
-    assert(i >= 0);
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Kokkos::subview(d6d_, i, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL(),
-                           Kokkos::ALL(), Kokkos::ALL());
-  }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(const int j, const int i) const {
-    assert(j >= 0 && i >= 0);
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Kokkos::subview(d6d_, j, i, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL(),
-                           Kokkos::ALL());
-  }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(const int k, const int j, const int i) const {
-    assert(k >= 0 && j >= 0 && i >= 0);
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Kokkos::subview(d6d_, k, j, i, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL());
-  }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(const int n, const int k, const int j, const int i) const {
-    assert(n >= 0 && k >= 0 && j >= 0 && i >= 0);
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Kokkos::subview(d6d_, n, k, j, i, Kokkos::ALL(), Kokkos::ALL());
-  }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(const int m, const int n, const int k, const int j, const int i) const {
-    assert(m >= 0 && n >= 0 && k >= 0 && j >= 0 && i >= 0);
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Kokkos::subview(d6d_, m, n, k, j, i, Kokkos::ALL());
-  }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(const int l, const int m, const int n, const int k, const int j,
-           const int i) const {
-    assert(l >= 0 && m >= 0 && n >= 0 && k >= 0 && j >= 0 && i >= 0);
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Kokkos::subview(d6d_, l, m, n, k, j, i); // 0d view
-  }
-  KOKKOS_INLINE_FUNCTION
-  auto Get() const { return d6d_; }
-  // Resize View while preserving contents for shared extent
-  void Resize(const int nx1) { Kokkos::resize(d6d_, 1, 1, 1, 1, 1, nx1); }
-  void Resize(const int nx2, const int nx1) {
-    Kokkos::resize(d6d_, 1, 1, 1, 1, nx2, nx1);
-  }
-  void Resize(const int nx3, const int nx2, const int nx1) {
-    Kokkos::resize(d6d_, 1, 1, 1, nx3, nx2, nx1);
-  }
-  void Resize(const int nx4, const int nx3, const int nx2, const int nx1) {
-    Kokkos::resize(d6d_, 1, 1, nx4, nx3, nx2, nx1);
-  }
-  void Resize(const int nx5, const int nx4, const int nx3, const int nx2, const int nx1) {
-    Kokkos::resize(d6d_, 1, nx5, nx4, nx3, nx2, nx1);
-  }
-  void Resize(const int nx6, const int nx5, const int nx4, const int nx3, const int nx2,
-              const int nx1) {
-    Kokkos::resize(d6d_, nx6, nx5, nx4, nx3, nx2, nx1);
-  }
-
+  
   // Reset size to 0
   // Note: Copies of this array won't be affected
-  void Reset() { Kokkos::resize(d6d_, 0, 0, 0, 0, 0, 0); }
+  void Reset() { data_ = Data(); }
+
+  template <std::size_t... I>
+  KOKKOS_FORCEINLINE_FUNCTION auto
+  GetTemplate_impl(std::index_sequence<I...>) const {
+    return Get(((void) I, 0)...);
+  } 
 
   // call me as Get<D>();
-  template <std::size_t N = 6>
+  template <std::size_t N>
   KOKKOS_INLINE_FUNCTION auto
-  Get(const std::integral_constant<int, N> &ic = std::integral_constant<int, N>{}) const {
-    return Get(ic);
+  Get() const {
+    return GetTemplate_impl(std::make_index_sequence<Data::rank - N>{}); 
   }
-
+  
  private:
-  // These functions exist to get around the fact that partial template
-  // specializations are forbidden for functions within a namespace.
-  // The trick then is to use tag dispatch with std::integral_constant
-  // and template on std::integral_constant.
-  // This trick is thanks to Daniel Holladay.
-  KOKKOS_INLINE_FUNCTION
-  auto Get(std::integral_constant<int, 6>) const { return Get(); }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(std::integral_constant<int, 5>) const { return Get(0); }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(std::integral_constant<int, 4>) const { return Get(0, 0); }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(std::integral_constant<int, 3>) const { return Get(0, 0, 0); }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(std::integral_constant<int, 2>) const { return Get(0, 0, 0, 0); }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(std::integral_constant<int, 1>) const { return Get(0, 0, 0, 0, 0); }
-  KOKKOS_INLINE_FUNCTION
-  auto Get(std::integral_constant<int, 0>) const { return Get(0, 0, 0, 0, 0, 0); }
+  // The stupid void casts below are to suppress compiler warnings about 
+  // an unused value. Found this trick buried deep in the gcc documentation 
+  template<class... Args, std::size_t... I>
+  ParArrayGeneric(const std::string &label, std::index_sequence<I...>, Args... args) 
+      : data_(label, ((void) I, 1)..., args...) {}
+  
+  template<class... Args, std::size_t... I>
+  void NewParArrayND(std::index_sequence<I...>, Args... args, const std::string& label) {
+    data_ = Data(label, ((void) I, 1)..., args...); 
+  }
 
-#define SLC0 std::make_pair(0, 1)
-  KOKKOS_INLINE_FUNCTION
-  auto SliceD(index_pair_t slc, std::integral_constant<int, 6>) const {
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Slice(slc, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL(),
-                 Kokkos::ALL());
+  template<class... Args, std::size_t... I>
+  KOKKOS_FORCEINLINE_FUNCTION  
+  auto Get(std::index_sequence<I...>, Args... args) const { 
+    return Kokkos::subview(data_, args..., ((void) I, Kokkos::ALL())...);
   }
-  KOKKOS_INLINE_FUNCTION
-  auto SliceD(index_pair_t slc, std::integral_constant<int, 5>) const {
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Slice(SLC0, slc, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL());
-  }
-  KOKKOS_INLINE_FUNCTION
-  auto SliceD(index_pair_t slc, std::integral_constant<int, 4>) const {
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Slice(SLC0, SLC0, slc, Kokkos::ALL(), Kokkos::ALL(), Kokkos::ALL());
-  }
-  KOKKOS_INLINE_FUNCTION
-  auto SliceD(index_pair_t slc, std::integral_constant<int, 3>) const {
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Slice(SLC0, SLC0, SLC0, slc, Kokkos::ALL(), Kokkos::ALL());
-  }
-  KOKKOS_INLINE_FUNCTION
-  auto SliceD(index_pair_t slc, std::integral_constant<int, 2>) const {
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Slice(SLC0, SLC0, SLC0, SLC0, slc, Kokkos::ALL());
-  }
-  KOKKOS_INLINE_FUNCTION
-  auto SliceD(index_pair_t slc, std::integral_constant<int, 1>) const {
-    assert(GetSize() > 0 && "Can't take the subview of an unitialized array");
-    return Slice(SLC0, SLC0, SLC0, SLC0, SLC0, slc);
-  }
-#undef SLC0
 
-  Data d6d_;
+  template<class... Args, std::size_t... I>
+  void Resize(std::index_sequence<I...>, Args... args) { 
+    Kokkos::resize(data_, ((void) I, 1)..., args...);
+  }
+  
+  template<class... Args, std::size_t... I>
+  KOKKOS_FORCEINLINE_FUNCTION
+  auto &_operator_impl(std::index_sequence<I...>, Args... args) const {
+    return data_(((void) I, 0)..., args...);
+  }
+  
+  template<std::size_t... I, std::size_t... J>
+  auto SliceD(std::index_sequence<I...>, std::index_sequence<J...>, index_pair_t slc) const {
+    return Slice(((void) I, std::make_pair(0, 1))..., slc, ((void) J, Kokkos::ALL())...); 
+  }
+
+  Data data_;
 };
 
 template <typename T, typename Layout = LayoutWrapper>
 using device_view_t = Kokkos::View<T ******, Layout, DevMemSpace>;
+template <typename T, typename Layout = LayoutWrapper>
+using device_view4_t = Kokkos::View<T ****, Layout, DevMemSpace>;
 
 template <typename T, typename Layout = LayoutWrapper>
 using host_view_t = typename device_view_t<T, Layout>::HostMirror;
 
 template <typename T, typename Layout = LayoutWrapper>
-using ParArrayND = ParArrayNDGeneric<device_view_t<T, Layout>>;
+using ParArrayND = ParArrayGeneric<device_view_t<T, Layout>>;
 
 template <typename T, typename Layout = LayoutWrapper>
-using ParArrayHost = ParArrayNDGeneric<host_view_t<T, Layout>>;
+using ParArray4ND = ParArrayGeneric<device_view4_t<T, Layout>>;
+
+template <typename T, typename Layout = LayoutWrapper>
+using ParArrayHost = ParArrayGeneric<host_view_t<T, Layout>>;
 
 template <typename T>
 struct FaceArray {
