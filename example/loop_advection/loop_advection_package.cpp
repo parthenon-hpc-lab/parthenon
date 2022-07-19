055a6f16d80faf96e481e1ba117913a4b0f02ae7
Forrest Glines <fglines@sandia.gov>
2022-07-19 18:53:45 -0400

    Switched mockup to use sparse packs

--- example/loop_advection/loop_advection_package.cpp
+++ example/loop_advection/loop_advection_package.cpp
@@ -91,6 +91,14 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
   const auto &v1 = pkg->Param<Real>("v1");
   const auto &v2 = pkg->Param<Real>("v2");
 
+
+  const auto &tup =  SparsePack<>::Make(pmd->meshblock_data.Get(),{"E_edge","B_face"});
+  const auto &v = std::get<0>(tup);
+  const auto &pack_map = std::get<1>(tup);
+
+  const auto &A_vidx = pack_map["E_edge"];
+  const auto &B_face_vidx = pack_map["B_face"];
+
   //Initialize vector potential at cell edges
   const auto &A = md->PackVars(std::vector<std::string>{"E_edge"});
   for( int edge_dir=1; edge_dir <= 3; edge_dir++){
@@ -115,7 +123,7 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
         const Real ay = 0;
         const Real az = max( amp*(R - r),0);
 
-        A( edge_dir, b, k, j, i) = (edge_dir == 1 ) ? ax :
+        v( b, edge_dir, A_vidx, k, j, i) = (edge_dir == 1 ) ? ax :
                                    (edge_dir == 2 ) ? ay : az;
       });
   }
@@ -123,7 +131,6 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
 
   //Initialize magnetic field from curl of vector potential
   //Computing each direction individually (not necessarily the fastest method)
-  const auto &B = md->PackVars(std::vector<std::string>{"B_face"});
   for( int face_dir=1; face_dir <= 3; face_dir++){
     CartDir face_dir_enum = static_cast<CartDir>(face_dir);
 
@@ -148,18 +155,18 @@ void ProblemGenerator(MeshBlock *pmb, ParameterInput *pin) {
                                            coords.dx3(k);
 
         //First term: dEzdy, dExdz, or dEydx
-        const Real dEp2dp1 =(E(cart_dir_p2, b,  k, j, i)
-                            -E(cart_dir_p2, b,  k - (cart_dir_p1 == 3),
+        const Real dEp2dp1 =(v.cmpt( b, cart_dir_p2, A_vidx,  k, j, i)
+                            -v.cmpt( b, cart_dir_p2, A_vidx,  k - (cart_dir_p1 == 3),
                                                 j - (cart_dir_p1 == 2),
                                                 i - (cart_dir_p1 == 1)))/dp1;
 
         //Second term: dEydz, dEzdx, or dExdy
-        const Real dEp1dp2 =(E( cart_dir_p1, b, k, j, i)
-                            -E( cart_dir_p1, b, k - (cart_dir_p2 == 3),
+        const Real dEp1dp2 =(v.cmpt( b, cart_dir_p1, A_vidx, k, j, i)
+                            -v.cmpt( b, cart_dir_p1, A_vidx, k - (cart_dir_p2 == 3),
                                                 j - (cart_dir_p2 == 2),
                                                 i - (cart_dir_p2 == 1)))/dp2;
 
-        dBdt( cart_dir, b, k, j, i) = -(dEp2dp1 - dEp1dp2);
+        v.cmpt( b, cart_dir, B_face_vidx, b, k, j, i) = -(dEp2dp1 - dEp1dp2);
       });
   }
 
@@ -174,17 +181,15 @@ parthenon::TaskStatus calc_dBdt(parthenon::MeshBlock *pmb){
   const auto &v2 = pkg->Param<Real>("v2");
   const Real v3 = 0;
 
-  //FIXME(forrestglines): Currently each of these variables are in separate packs - can they be combined?
-  //Get variable packs for face centered B, cell centered B,  and edge centered E
-  const auto B_face & = pmd->meshblock_data.Get()->PackVars(
-      std::vector<std::string>({"B_face"}));
-  const auto B_cell & = pmd->meshblock_data.Get()->PackVars(
-      std::vector<std::string>({"B_cell"}));
-  const auto E_cell & = pmd->meshblock_data.Get()->PackVars(
-      std::vector<std::string>({"E_cell"}));
-  const auto E_edge & = pmd->meshblock_data.Get()->PackVars(
-      std::vector<std::string>({"E_edge"}));
-  const auto dBdt_face & = B_face;
+  const auto &tup =  SparsePack<>::Make(pmd->meshblock_data.Get(),{"B_face","B_cell","E_cell","E_edge"});
+  const auto &v = std::get<0>(tup);
+  const auto &pack_map = std::get<1>(tup);
+
+  const auto &B_face_vidx = pack_map["B_face"];
+  const auto &B_cell_vidx = pack_map["B_cell"];
+  const auto &E_cell_vidx = pack_map["E_cell"];
+  const auto &E_edge_vidx = pack_map["E_edge"];
+  const auto &dBdt_face_vidx = B_face_vidx;
 
   IndexRange ib = pmb->cellbounds.GetBoundsI(IndexDomain::entire);
   IndexRange jb = pmb->cellbounds.GetBoundsJ(IndexDomain::entire);
@@ -198,14 +203,14 @@ parthenon::TaskStatus calc_dBdt(parthenon::MeshBlock *pmb){
       KOKKOS_LAMBDA(const int k, const int j, const int i) {
 
         //Average B from the faces to the cell centers
-        B_cell(b, 0, k, j, i) = 0.5 * (B_face(1, b, k, j, i) + B_face(1, b, k, j, i+1));
-        B_cell(b, 1, k, j, i) = 0.5 * (B_face(2, b, k, j, i) + B_face(2, b, k, j+1, i));
-        B_cell(b, 2, k, j, i) = 0.5 * (B_face(3, b, k, j, i) + B_face(3, b, k+1, j, i));
+        v(b, B_cell_vidx+0, k, j, i) = 0.5 * (v.cmpt( b, 1, B_face_vidx, k, j, i) + v.cmpt(b, 1, B_face_vidx,  k, j, i+1));
+        v(b, B_cell_vidx+1, k, j, i) = 0.5 * (v.cmpt( b, 2, B_face_vidx, k, j, i) + v.cmpt(b, 2, B_face_vidx,  k, j+1, i));
+        v(b, B_cell_vidx+2, k, j, i) = 0.5 * (v.cmpt( b, 3, B_face_vidx, k, j, i) + v.cmpt(b, 3, B_face_vidx,  k+1, j, i));
 
         //Compute E_cell = v X B
-        E_cell(b, 0, k, j, i) = v2 * B3 - v3 * B2;
-        E_cell(b, 1, k, j, i) = v3 * B1 - v1 * B3;
-        E_cell(b, 2, k, j, i) = v1 * B2 - v2 * B1;
+        v(b, E_cell_vidx+0, k, j, i) = v2 * B3 - v3 * B2;
+        v(b, E_cell_vidx+1, k, j, i) = v3 * B1 - v1 * B3;
+        v(b, E_cell_vidx+2, k, j, i) = v1 * B2 - v2 * B1;
 
       });
   }
@@ -219,19 +224,19 @@ parthenon::TaskStatus calc_dBdt(parthenon::MeshBlock *pmb){
       KOKKOS_LAMBDA(const int k, const int j, const int i) {
         //Average E from the cell centers to cell edges
         if( k+1 < kb.e && j+1 < jb.e) 
-          E_edge(1, b, k, j, i) = E_face(b, 0,   k,   j,   i) + E_face(b, 0,   k, j+1,   i);
-                                + E_face(b, 0, k+1,   j,   i) + E_face(b, 0, k+1, j+1,   i);
+          v.cmpt(b, 1, E_edge_idx, k, j, i) = v.cmpt(b, 1, E_face_vidx,   k,   j,   i) + v.cmpt(b, 1, E_face_vidx,   k, j+1,   i);
+                                            + v.cmpt(b, 1, E_face_vidx, k+1,   j,   i) + v.cmpt(b, 1, E_face_vidx, k+1, j+1,   i);
         if( j+1 < jb.e && i+1 < ib.e) 
-          E_edge(2, b, k, j, i) = E_face(b, 0,   k,   j,   i) + E_face(b, 0,   k,   j, i+1);
-                                + E_face(b, 0, k+1,   j,   i) + E_face(b, 0, k+1,   j, i+1);
+          v.cmpt(b, 2, E_edge_idx, k, j, i) = v.cmpt(b, 2, E_face_vidx,   k,   j,   i) + v.cmpt(b, 2, E_face_vidx,   k,   j, i+1);
+                                            + v.cmpt(b, 2, E_face_vidx, k+1,   j,   i) + v.cmpt(b, 2, E_face_vidx, k+1,   j, i+1);
         if( j+1 < jb.e && k+1 < kb.e) 
-          E_edge(3, b, k, j, i) = E_face(b, 0,   k,   j,   i) + E_face(b, 0,   k,   j, i+1);
-                                + E_face(b, 0,   k, j+1,   i) + E_face(b, 0,   k, j+1, i+1);
+          v.cmpt(b, 3, E_edge_idx, k, j, i) = v.cmpt(b, 3, E_face_vidx,   k,   j,   i) + v.cmpt(b, 3, E_face_vidx,   k,   j, i+1);
+                                            + v.cmpt(b, 3, E_face_vidx,   k, j+1,   i) + v.cmpt(b, 3, E_face_vidx,   k, j+1, i+1);
 
 
 
-      });
-  }
+        });
+    }
 
   //After averaging from faces to edges, reduce bounds by 1
   //FIXME(forrestglines): Is this how to shrink bounds?
@@ -246,17 +251,17 @@ parthenon::TaskStatus calc_dBdt(parthenon::MeshBlock *pmb){
     pmb->par_for(
       "LoopAdvection::ProblemGenerator:curl_E_edge", 0, vf.dim(5)-1, kb.s, kb.e, jb.s, jb.e, ib.s, ib.e,
       KOKKOS_LAMBDA(const int k, const int j, const int i) {
-        const auto &coords = B.GetCoords(b);
+        const auto &coords = v.GetCoordinates(b);
 
         if( k+1 < kb.e && j+1 < jb.e) 
-          dBdt_face( 1, b, k, j, i) = (E_edge( 3, k, j+1, i) - E_edge( 3, k, j, i))/coords.dx2(j)
-                                    - (E_edge( 2, k+1, j, i) - E_edge( 2, k, j, i))/coords.dx3(k);
+          v.cmpt(b, 1, dBdt_face_vidx, k, j, i) = (v.cmpt(b, 3, E_edge_vidx k, j+1, i) - v.cmpt(b, 3, E_edge_vidx k, j, i))/coords.dx2(j)
+                                                - (v.cmpt(b, 2, E_edge_vidx k+1, j, i) - v.cmpt(b, 2, E_edge_vidx k, j, i))/coords.dx3(k);
         if( j+1 < jb.e && i+1 < ib.e) 
-          dBdt_face( 2, b, k, j, i) = (E_edge( 1, k+1, j, i) - E_edge( 1, k, j, i))/coords.dx3(k)
-                                    - (E_edge( 3, k, j, i+1) - E_edge( 3, k, j, i))/coords.dx1(i);
+          v.cmpt(b, 2, dBdt_face_vidx, k, j, i) = (v.cmpt(b, 1, E_edge_vidx k+1, j, i) - v.cmpt(b, 1, E_edge_vidx k, j, i))/coords.dx3(k)
+                                                - (v.cmpt(b, 3, E_edge_vidx k, j, i+1) - v.cmpt(b, 3, E_edge_vidx k, j, i))/coords.dx1(i);
         if( j+1 < jb.e && k+1 < kb.e) 
-          dBdt_face( 3, b, k, j, i) = (E_edge( 2, k, j, i+1) - E_edge( 2, k, j, i))/coords.dx3(k)
-                                    - (E_edge( 1, k, j+1, i) - E_edge( 1, k, j, i))/coords.dx2(j);
+          v.cmpt(b, 3, dBdt_face_vidx, k, j, i) = (v.cmpt(b, 2, E_edge_vidx k, j, i+1) - v.cmpt(b, 2, E_edge_vidx k, j, i))/coords.dx3(k)
+                                                - (v.cmpt(b, 1, E_edge_vidx k, j+1, i) - v.cmpt(b, 1, E_edge_vidx k, j, i))/coords.dx2(j);
       });
   }
 
@@ -276,20 +281,28 @@ void UserWorkAfterLoop(Mesh *mesh, ParameterInput *pin, SimTime &tm) {
     IndexRange jb = pmb->cellbounds.GetNodeBoundsJ(IndexDomain::interior);
     IndexRange kb = pmb->cellbounds.GetNodeBoundsK(IndexDomain::interior);
 
-    const auto &E_edge = pmd->meshblock_data.Get()->PackVars(
-        std::vector<std::string>({"E_edge"}));
-    const auto &div_E_node = pmd->meshblock_data.Get()->PackVars(
-        std::vector<std::string>({"div_E_node"}));
+    const auto &tup =  SparsePack<>::Make(pmd->meshblock_data.Get(),{"E_edge","div_E_node"});
+    const auto &v = std::get<0>(tup);
+    const auto &pack_map = std::get<1>(tup);
+
+    const auto &E_edge_vidx = pack_map["E_edge"];
+    const auto &div_E_node_vidx = pack_map["div_E_node"];
 
     // Compute the div of E at nodes(should be zero?)
     pmb->par_for(
       "LoopAdvection::calc_div_E", 0, vf.dim(5)-1, kb.s, kb.e, jb.s, jb.e, ib.s, ib.e,
       KOKKOS_LAMBDA(const int k, const int j, const int i) {
-        const auto &coords = E_edge.GetCoords(b);
+        const auto &coords = v.GetCoordinates(b);
 
-        div_E_node( b, k, j, i) = (E_edge( 1, b, k, j, i) - E_edge( 1, b,  k, j, i-1))/coords.dx1(i)
-                                 +(E_edge( 2, b, k, j, i) - E_edge( 2, b,  k, j-1, i))/coords.dx2(j)
-                                 +(E_edge( 3, b, k, j, i) - E_edge( 3, b,  k-1, j, i))/coords.dx3(k);
+        v(b, div_E_node_vidx, k, j, i) = (v.cmpt(b, 1, E_edge_vidx, k, j, i) -
+                                          v.cmpt(b, 1, E_edge_vidx, k, j, i - 1)) /
+                                             coords.dx1(i) +
+                                         (v.cmpt(b, 2, E_edge_vidx, k, j, i) -
+                                          v.cmpt(b, 2, E_edge_vidx, k, j - 1, i)) /
+                                             coords.dx2(j) +
+                                         (v.cmpt(b, 3, E_edge_vidx, k, j, i) -
+                                          v.cmpt(b, 3, E_edge_vidx, k - 1, j, i)) /
+                                             coords.dx3(k);
 
       });
   }
@@ -310,18 +323,28 @@ void UserWorkAfterLoop(Mesh *mesh, ParameterInput *pin, SimTime &tm) {
     const auto &div_Bc_cell = pmd->meshblock_data.Get()->PackVars(
         std::vector<std::string>({"div_Bc_cell"}));
 
+    const auto &tup = SparsePack<>::Make(
+        pmd->meshblock_data.Get(), {"B_face", "div_Bf_cell", "B_cell", "div_Bc_cell"});
+    const auto &v = std::get<0>(tup);
+    const auto &pack_map = std::get<1>(tup);
+
+    const auto &B_face_vidx = pack_map["B_face"];
+    const auto &div_Bf_cell_vidx = pack_map["div_Bf_cell"];
+    const auto &B_cell_vidx = pack_map["B_cell"];
+    const auto &div_Bc_cell_vidx = pack_map["div_Bc_cell"];
+
     // Compute the div of B at nodes(should be zero)
     pmb->par_for(
       "LoopAdvection::calc_div_B", 0, vf.dim(5)-1, kb.s, kb.e, jb.s, jb.e, ib.s, ib.e,
       KOKKOS_LAMBDA(const int k, const int j, const int i) {
-        const auto &coords = B_face.GetCoords(b);
+        const auto &coords = v.GetCoordinates(b);
 
-        div_Bf_cell( b, k, j, i) = (B_face( 1, b, k, j, i) - B_face( 1, b, k, j, i-1))/coords.dx1(i)
-                                  +(B_face( 2, b, k, j, i) - B_face( 2, b, k, j-1, i))/coords.dx2(j)
-                                  +(B_face( 3, b, k, j, i) - B_face( 3, b, k-1, j, i))/coords.dx3(k);
-        div_Bc_cell( b, k, j, i) = (B_cell( 1, b, k, j, i+1) - B_cell( 1, b, k, j, i-1))/(2*coords.dx1(i))
-                                  +(B_cell( 2, b, k, j+1, i) - B_cell( 2, b, k, j-1, i))/(2*coords.dx2(j))
-                                  +(B_cell( 3, b, k+1, j, i) - B_cell( 3, b, k-1, j, i))/(2*coords.dx3(k));
+        v( b, div_Bf_cell_vidx, k, j, i) = (v.cmpt( b, 1, B_face_vidx, k, j, i) - v.cmpt( b, 1, B_face_vidx, k, j, i-1))/coords.dx1(i)
+                                          +(v.cmpt( b, 2, B_face_vidx, k, j, i) - v.cmpt( b, 2, B_face_vidx, k, j-1, i))/coords.dx2(j)
+                                          +(v.cmpt( b, 3, B_face_vidx, k, j, i) - v.cmpt( b, 3, B_face_vidx, k-1, j, i))/coords.dx3(k);
+        v( b, div_Bc_cell_vidx, k, j, i) = (v( b, B_cell_vidx+0, k, j, i+1) - v( b, B_cell_vidx+0, k, j, i-1))/(2*coords.dx1(i))
+                                          +(v( b, B_cell_vidx+1, k, j+1, i) - v( b, B_cell_vidx+1, k, j-1, i))/(2*coords.dx2(j))
+                                          +(v( b, B_cell_vidx+1, k+1, j, i) - v( b, B_cell_vidx+2, k-1, j, i))/(2*coords.dx3(k));
 
       });
   }
