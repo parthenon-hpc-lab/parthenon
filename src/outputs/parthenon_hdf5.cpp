3f2cb8e296bf1792ac1f83ca8e0b20e165522bd4
jdolence <jcdolence@gmail.com>
2022-09-19 14:19:02 -0600

    go back to NaN output for unallocated sparse

--- src/outputs/parthenon_hdf5.cpp
+++ src/outputs/parthenon_hdf5.cpp
@@ -886,7 +886,7 @@ void PHDF5Output::WriteOutputFileImpl(Mesh *pm, ParameterInput *pin, SimTime *tm
         if (vinfo.is_sparse) {
           hsize_t N = varSize * vlen;
           for (int i = 0; i < N; ++i)
-            tmpData[index++] = 0.0;//std::numeric_limits<Real>::quiet_NaN();
+            tmpData[index++] = std::numeric_limits<Real>::quiet_NaN();
         } else {
           std::stringstream msg;
           msg << "### ERROR: Unable to find dense variable " << var_name << std::endl;
