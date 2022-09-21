0266860a005a8e0f5fee35661d1ed595b0a868b6
Luke Roberts <lfroberts@lanl.gov>
2022-09-21 14:05:46 -0600

    Update copyrights

--- src/parthenon/package.hpp
+++ src/parthenon/package.hpp
@@ -1,5 +1,5 @@
 //========================================================================================
-// (C) (or copyright) 2020-2021. Triad National Security, LLC. All rights reserved.
+// (C) (or copyright) 2020-2022. Triad National Security, LLC. All rights reserved.
 //
 // This program was produced under U.S. Government contract 89233218CNA000001 for Los
 // Alamos National Laboratory (LANL), which is operated by Triad National Security, LLC
