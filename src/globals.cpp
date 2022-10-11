c9180e6a4fa8762999790a88fb9011311b854a17
Jonah Miller <jonahm@lanl.gov>
2022-10-11 10:01:27 -0600

    fix comments referencing to BufferInfo_t

--- src/globals.cpp
+++ src/globals.cpp
@@ -44,7 +44,7 @@ Real receive_boundary_buffer_timeout;
 Real current_task_runtime_sec;
 
 namespace refinement {
-// Communication buffers are packed into a `BufferInfo_t` object.
+// Communication buffers are packed into a `BndInfo` object.
 // if the size of this object is greater than min_num_bufs,
 // hierarchical parallelism is used for prolongation/restriction.
 // otherwise one kernel per buffer is launched.
--- src/globals.hpp
+++ src/globals.hpp
@@ -43,7 +43,7 @@ extern Real receive_boundary_buffer_timeout;
 extern Real current_task_runtime_sec;
 
 namespace refinement {
-// Communication buffers are packed into a `BufferInfo_t` object.
+// Communication buffers are packed into a `BndInfo` object.
 // if the size of this object is greater than min_num_bufs,
 // hierarchical parallelism is used for prolongation/restriction.
 // otherwise one kernel per buffer is launched.
