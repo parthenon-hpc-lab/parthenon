10660f5c7f454e1b2cb44854fd6492ad5a33f072
Jonah Miller <jonahm@lanl.gov>
2022-11-29 04:17:49 -0700

    formatting

--- benchmarks/burgers/burgers_driver.cpp
+++ benchmarks/burgers/burgers_driver.cpp
@@ -130,8 +130,8 @@ TaskCollection BurgersDriver::MakeTaskCollection(BlockList_t &blocks, const int 
     auto fill_deriv = tl.AddTask(update, FillDerived<MeshData<Real>>, mc1.get());
 
     if (pmesh->multilevel) {
-      tl.AddTask(set | set_local,
-                 parthenon::cell_centered_bvars::RestrictGhostHalos, mc1, false);
+      tl.AddTask(set | set_local, parthenon::cell_centered_bvars::RestrictGhostHalos, mc1,
+                 false);
     }
     // estimate next time step
     if (stage == integrator->nstages) {
