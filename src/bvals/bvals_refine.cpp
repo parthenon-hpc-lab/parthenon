117d3e03cc079cd6495a666bbf165083ae33350a
Jonah Miller <jonahm@lanl.gov>
2022-12-03 10:20:31 -0700

    kill unused code

--- src/bvals/bvals_refine.cpp
+++ src/bvals/bvals_refine.cpp
@@ -49,49 +49,6 @@ namespace parthenon {
 // which are now called together as ProlongateBoundaries in
 // `bvals/bondary_conditions.hpp`. This allows us to loop over all variables in a
 // container.
-void BoundaryValues::FillRestrictionMetadata(cell_centered_bvars::BufferCacheHost_t &info,
-                                             int &idx,
-                                             std::shared_ptr<CellVariable<Real>> v) {
-  std::shared_ptr<MeshBlock> pmb = GetBlockPointer();
-  MeshRefinement *pmr = pmb->pmr.get();
-  int &mylevel = pmb->loc.level;
-  for (int l = 0; l < v->GetDim(6); ++l) {
-    for (int m = 0; m < v->GetDim(5); ++m) {
-      for (int n = 0; n < nneighbor; n++) {
-        NeighborBlock &nb = neighbor[n];
-        if (nb.snb.level >= mylevel) continue;
-
-        IndexRange bni, bnj, bnk;
-        ComputeRestrictionBounds_(nb, bni, bnj, bnk);
-
-        for (int nk = bnk.s; nk <= bnk.e; nk++) {
-          for (int nj = bnj.s; nj <= bnj.e; nj++) {
-            for (int ni = bni.s; ni <= bni.e; ni++) {
-              int ntype = std::abs(ni) + std::abs(nj) + std::abs(nk);
-              // skip myself or coarse levels; only the same level must be restricted
-              if (ntype == 0 || nblevel[nk + 1][nj + 1][ni + 1] != mylevel) continue;
-              ComputeRestrictionIndices_(nb, nk, nj, ni, info(idx).si, info(idx).ei,
-                                         info(idx).sj, info(idx).ej, info(idx).sk,
-                                         info(idx).ek);
-              info(idx).coords = pmb->coords;
-              info(idx).coarse_coords = pmb->pmr->coarse_coords;
-              info(idx).allocated = v->IsAllocated();
-              if (v->IsAllocated()) {
-                info(idx).fine = v->data.Get();
-                info(idx).coarse = v->coarse_s.Get();
-              }
-              info(idx).refinement_op = RefinementOp_t::Restriction;
-              info(idx).Nt = v->GetDim(6);
-              info(idx).Nu = v->GetDim(5);
-              info(idx).Nv = v->GetDim(4);
-              idx++;
-            }
-          }
-        }
-      }
-    }
-  }
-}
 
 void BoundaryValues::ProlongateBoundaries() {
   std::shared_ptr<MeshBlock> pmb = GetBlockPointer();
@@ -187,28 +144,6 @@ void BoundaryValues::ComputeRestrictionIndices_(const NeighborBlock &nb, int nk,
   CalcIndices(ris, rie, ni, nb.ni.ox1, cib);
   CalcIndices(rjs, rje, nj, nb.ni.ox2, cjb);
   CalcIndices(rks, rke, nk, nb.ni.ox3, ckb);
-}
-
-void BoundaryValues::ComputeRestrictionBounds_(const NeighborBlock &nb, IndexRange &ni,
-                                               IndexRange &nj, IndexRange &nk) {
-  auto getbounds = [](const int nbx, IndexRange &n) {
-    n.s = std::max(nbx - 1, -1); // can be -1 or 0
-    n.e = std::min(nbx + 1, 1);  // can be 0 or 1
-  };
-
-  std::shared_ptr<MeshBlock> pmb = GetBlockPointer();
-  getbounds(nb.ni.ox1, ni);
-  if (pmb->block_size.nx2 == 1) {
-    nj.s = nj.e = 0;
-  } else {
-    getbounds(nb.ni.ox2, nj);
-  }
-
-  if (pmb->block_size.nx3 == 1) {
-    nk.s = nk.e = 0;
-  } else {
-    getbounds(nb.ni.ox3, nk);
-  }
 }
 
 void BoundaryValues::ComputeProlongationBounds_(const NeighborBlock &nb, IndexRange &bi,
