5a2d14b155bd10d4533c7184d349612f2f947fd8
Ben Wibking <ben@wibking.com>
2023-01-10 21:08:13 +0000

    loop over MeshData

--- example/advection/render_ascent.cpp
+++ example/advection/render_ascent.cpp
@@ -1,4 +1,5 @@
 #include "render_ascent.hpp"
+#include "defs.hpp"
 
 using namespace parthenon::package::prelude;
 using namespace parthenon;
@@ -27,20 +28,43 @@ void render_ascent(Mesh *par_mesh, ParameterInput *pin, SimTime const &tm) {
   ascent_opts["mpi_comm"] = MPI_Comm_c2f(MPI_COMM_WORLD);
   a.open(ascent_opts);
 
-  // create a Conduit node to hold our mesh data
-  Node mesh;
+  // vector of all meshes
+  std::vector<Node> mesh_vec;
 
-  // TODO(ben): For *each* meshblock, do:
-  {
+  for (auto [name, thisMeshData] : par_mesh->mesh_data.Stages()) {
+    IndexRange ib = thisMeshData->GetBoundsI(IndexDomain::entire);
+    IndexRange jb = thisMeshData->GetBoundsJ(IndexDomain::entire);
+    IndexRange kb = thisMeshData->GetBoundsK(IndexDomain::entire);
+    int nx = ib.e - ib.s + 1;
+    int ny = jb.e - jb.s + 1;
+    int nz = kb.e - kb.s + 1;
+
+    std::vector<std::string> vars({"U"});
+    auto &v = thisMeshData->PackVariables(vars);
+    auto &coords = v.GetCoords(0);
+    Real dx1 = coords.CellWidth<X1DIR>(0, 0, 0);
+    Real dx2 = coords.CellWidth<X2DIR>(0, 0, 0);
+    Real dx3 = coords.CellWidth<X3DIR>(0, 0, 0);
+    std::array<Real, 3> corner = coords.GetXmin();
+
+    // create a Conduit node to hold our mesh data
+    // (we need to save this in a list for rendering)
+    Node mesh;
+
     // create the coordinate set
     mesh["coordsets/coords/type"] = "uniform";
-    mesh["coordsets/coords/dims/i"] = 3;
-    mesh["coordsets/coords/dims/j"] = 3;
+    mesh["coordsets/coords/dims/i"] = nx + 1;
+    mesh["coordsets/coords/dims/j"] = ny + 1;
+    mesh["coordsets/coords/dims/k"] = nz + 1;
+    uint64_t ncells = nx * ny * nz;
+
     // add origin and spacing to the coordset (optional)
-    mesh["coordsets/coords/origin/x"] = -10.0;
-    mesh["coordsets/coords/origin/y"] = -10.0;
-    mesh["coordsets/coords/spacing/dx"] = 10.0;
-    mesh["coordsets/coords/spacing/dy"] = 10.0;
+    mesh["coordsets/coords/origin/x"] = corner[0];
+    mesh["coordsets/coords/origin/y"] = corner[1];
+    mesh["coordsets/coords/origin/z"] = corner[2];
+    mesh["coordsets/coords/spacing/dx"] = dx1;
+    mesh["coordsets/coords/spacing/dy"] = dx2;
+    mesh["coordsets/coords/spacing/dz"] = dx3;
 
     // add the topology
     // this case is simple b/c it's implicitly derived from the coordinate set
@@ -54,13 +78,13 @@ void render_ascent(Mesh *par_mesh, ParameterInput *pin, SimTime const &tm) {
       mesh["fields/ele_example/association"] = "element";
       // reference the topology this field is defined on by name
       mesh["fields/ele_example/topology"] = "topo";
-      // set the field values, for this case we have 4 elements
-      auto data_values = DataType::float64(4);
+      // set the field values
+      auto data_values = DataType::float64(ncells);
       mesh["fields/ele_example/values"].set(data_values);
 
       float64 *ele_vals_ptr = mesh["fields/ele_example/values"].value();
 
-      for (int i = 0; i < 4; i++) {
+      for (int i = 0; i < ncells; i++) {
         ele_vals_ptr[i] = float64(i);
       }
     }
@@ -78,6 +102,10 @@ void render_ascent(Mesh *par_mesh, ParameterInput *pin, SimTime const &tm) {
     conduit::relay::io::blueprint::save_mesh(mesh, "complete_uniform_mesh_example",
                                              "json");
 
+    mesh_vec.emplace_back(mesh);
+  }
+
+  for (auto &mesh : mesh_vec) {
     // publish mesh to ascent
     a.publish(mesh);
   }
