67dd77f21bc50c41b10ef09bb15a77b3ea582d45
Luke Roberts <lfroberts@lanl.gov>
2023-03-09 16:57:04 -0700

    lint

--- src/interface/state_descriptor.cpp
+++ src/interface/state_descriptor.cpp
@@ -273,9 +273,9 @@ bool StateDescriptor::AddFieldImpl(const VarID &vid, const Metadata &m_in,
     metadataMap_.insert({vid, m});
     refinementFuncMaps_.Register(m);
     allocControllerReverseMap_.insert({vid, control_vid});
-    // Add this variable to the set of unique IDs at the 
-    // earliest possible time 
-    CellVariable<Real>::GetUniqueID(vid.label()); 
+    // Add this variable to the set of unique IDs at the
+    // earliest possible time
+    CellVariable<Real>::GetUniqueID(vid.label());
   }
 
   return true;
