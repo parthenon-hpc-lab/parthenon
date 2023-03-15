598f0da37f34fbe6e36d4ba55a7cfce94a475f27
Philipp Grete <pgrete@hs.uni-hamburg.de>
2023-03-15 14:42:32 +0100

    revert to use host array for ascent_ghosts field

--- src/outputs/outputs.hpp
+++ src/outputs/outputs.hpp
@@ -205,7 +205,7 @@ class AscentOutput : public OutputType {
                        const SignalHandler::OutputSignal signal) override;
 
  private:
-  ParArray1D<std::int32_t> ghost_mask;
+  HostArray1D<std::int32_t> ghost_mask;
 };
 
 #ifdef ENABLE_HDF5
