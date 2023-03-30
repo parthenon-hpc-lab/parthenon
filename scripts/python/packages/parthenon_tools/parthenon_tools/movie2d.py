228e97f0764dd369f30517b03409f66d2677f059
Gurkirat Singh <tbhaxor@gmail.com>
2023-03-30 22:16:50 +0530

    improve the performance of movies2d.py

--- scripts/python/packages/parthenon_tools/parthenon_tools/movie2d.py
+++ scripts/python/packages/parthenon_tools/parthenon_tools/movie2d.py
@@ -13,62 +13,49 @@
 
 from __future__ import print_function
 
-from argparse import ArgumentParser
-
-# import os
+import os
+import logging
 import numpy as np
+from phdf import phdf
+
+from argparse import ArgumentParser
+from pathlib import Path
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
+
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 
-parser = ArgumentParser(
-    prog="movie2d",
-    description="Plot snapshots of 2d parthenon output",
-)
+logging.basicConfig(level=logging.CRITICAL,
+                    format='%(asctime)s [%(levelname)s]\t%(message)s')
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+
+parser = ArgumentParser(prog="movie2d",description="Plot snapshots of 2d parthenon output")
+
+parser.add_argument("--vector-component", dest="vc", type=float, default=None,
+                    help="Vector component of field to plot. Mutually exclusive with --tensor-component.")
+parser.add_argument("--tensor-component", dest="tc", type=float, nargs=2, default=None, 
+                    help="Tensor components of field to plot. Mutally exclusive with --vector-component.")
+parser.add_argument("--workers", "-w", help="Number of parallel workers to use (default: 10)", 
+                    type=int, metavar="COUNT", default=10)
+parser.add_argument("--worker-type", help="Type of worker to use (default: process)", 
+                    choices=["process", "thread"], default="process", metavar="TYPE")
+parser.add_argument("--time-step", help="Value of dt parameter from parthenon/output* block", 
+                    metavar="DT", default=1.0, type=float)
+parser.add_argument("--output-directory", "-d", help=f"Output directory to save the images (default: {os.getcwd()})", 
+                    type=Path, default=os.getcwd(), metavar="DIR")
+parser.add_argument("--debug", help="Enable graph debug mode. This will run in the single thread. (default: false)", 
+                    action="store_true", default=False)
+
 parser.add_argument("field", type=str, help="field to plot")
-parser.add_argument(
-    "--vector-component",
-    dest="vc",
-    type=float,
-    default=None,
-    help=(
-        "Vector component of field to plot. "
-        + "Mutually exclusive with --tensor-component."
-    ),
-)
-parser.add_argument(
-    "--tensor-component",
-    dest="tc",
-    type=float,
-    nargs=2,
-    default=None,
-    help=(
-        "Tensor components of field to plot "
-        + "Mutally exclusive with --vector-component."
-    ),
-)
 parser.add_argument("files", type=str, nargs="+", help="files to plot")
-
-
-# def addPath():
-#     """add the vis/python directory to the pythonpath variable"""
-#     myPath = os.path.realpath(os.path.dirname(__file__))
-#     # sys.path.insert(0,myPath+'/../vis/python')
-#     # sys.path.insert(0,myPath+'/vis/python')
-
-
-def read(filename, nGhost=0):
-    """Read the parthenon hdf file"""
-    from phdf import phdf
-
-    f = phdf(filename)
-    return f
-
 
 def plot_dump(
     xf,
     yf,
     q,
-    name,
+    time_title,
+    output_file: Path,
     with_mesh=False,
     block_ids=[],
     xi=None,
@@ -96,12 +83,15 @@ def plot_dump(
 
     fig = plt.figure()
     p = fig.add_subplot(111, aspect=1)
+    p.set_title(f"t = {time_title} seconds")
+
     qm = np.ma.masked_where(np.isnan(q), q)
     qmin = qm.min()
     qmax = qm.max()
-    NumBlocks = q.shape[0]
-    for i in range(NumBlocks):
-        # Plot the actual data, should work if parthenon/output*/ghost_zones = true or false
+
+    n_blocks = q.shape[0]
+    for i in range(n_blocks):
+        # Plot the actual data, should work if parthenon/output*/ghost_zones = true/false
         # but obviously no ghost data will be shown if ghost_zones = false
         p.pcolormesh(xf[i, :], yf[i, :], q[i, :, :], vmin=qmin, vmax=qmax)
 
@@ -141,45 +131,59 @@ def plot_dump(
                 )
                 p.add_patch(rect)
 
-    plt.savefig(name, dpi=300)
-    plt.close()
+    fig.savefig(output_file, dpi=300)
+    plt.close(fig=fig)
+    logger.debug(f"Saved {time_title}s time-step to {output_file}")
+
 
 
 if __name__ == "__main__":
     # addPath()
     args = parser.parse_args()
-    field = args.field
-    files = args.files
-    components = [0, 0]
-    if (args.tc is not None) and (args.vc is not None):
+    logger.setLevel(logging.DEBUG if args.debug else logging.INFO)
+
+    if args.tc and args.vc:
         raise ValueError(
             "Only one of --tensor-component and --vector-component should be set."
         )
+
+    if args.workers > 1:
+        logger.warning("Matplotlib is not multi-thread friendly. Read this for more details https://matplotlib.org/stable/users/faq/howto_faq.html#work-with-threads")
+        logger.warning("Try decreasing threads if you encounter any undefined behaviour")
+    # Create output director if does't exists
+    args.output_directory.mkdir(0o755, True, True)
+    logger.info(f"Total files to process: {len(args.files)}")
+
+    components = [0, 0]
     if args.tc is not None:
         components = args.tc
     if args.vc is not None:
         components = [0, args.vc]
-    dump_id = 0
-    debug_plot = False
-    for f in files:
-        data = read(f)
-        print(data)
-        q = data.Get(field, False, not debug_plot)
-        name = str(dump_id).rjust(4, "0") + ".png"
-        if debug_plot:
-            plot_dump(
-                data.xg,
-                data.yg,
-                q,
-                name,
-                True,
-                data.gid,
-                data.xig,
-                data.yig,
-                data.xeg,
-                data.yeg,
-                components,
-            )
-        else:
-            plot_dump(data.xng, data.yng, q, name, True)
-        dump_id += 1
+    
+    _x = ProcessPoolExecutor if args.worker_type == "process" else ThreadPoolExecutor
+    current_time = 0.0
+    with _x(max_workers=args.workers) as pool:
+        for dump_id, file_name in enumerate(args.files):
+            data = phdf(file_name)
+            if args.field not in data.Variables:
+                logging.error(f"No such field \"{args.field}\" in {file_name}. \
+This will lead to stop further processing")
+                break
+            
+            logger.debug(f"Submitting {file_name}")
+            q = data.Get(args.field, False, not args.debug)
+            name = args.output_directory / Path(str(dump_id).rjust(4, "0") + ".png")
+            if args.debug:
+                pool.submit(plot_dump, data.xg, data.yg, q, current_time, name, True,
+                            data.gid, data.xig, data.yig, data.xeg, data.yeg,
+                            components)
+            else:
+                pool.submit(plot_dump, data.xng, data.yng, 
+                            q, current_time, name, True)
+            current_time += args.time_step
+            current_time = round(current_time, ndigits=2)
+
+
+    logger.info("All files are sent to the processor")
+
+logger.info("All done")
