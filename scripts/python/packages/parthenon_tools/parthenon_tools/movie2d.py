01677e008433ad39434d3d2be7b07fffe7a507d0
Gurkirat Singh <tbhaxor@gmail.com>
2023-04-06 04:48:52 +0530

    Update scripts/python/packages/parthenon_tools/parthenon_tools/movie2d.py
    Co-authored-by: Luke Roberts <lfroberts@lanl.gov>

--- scripts/python/packages/parthenon_tools/parthenon_tools/movie2d.py
+++ scripts/python/packages/parthenon_tools/parthenon_tools/movie2d.py
@@ -239,7 +239,7 @@ if __name__ == "__main__":
             data = phdf(file_name)
             if args.field not in data.Variables:
                 logger.error(
-                    f'No such field "{args.field}" in {file_name}. This will lead to stop further processing'
+                    f'No such field "{args.field}" in {file_name}. Further processing stopped.'
                 )
                 logger.info(f"Available fields: {data.Variables}")
                 ERROR_FLAG = True
