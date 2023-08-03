ea3219794603aa88d4a984c11b8678b7ddc0f128
Luke Roberts <lfroberts@lanl.gov>
2023-08-02 19:00:33 -0600

    Use tophat rhs

--- example/poisson_gmg/parthenon_app_inputs.cpp
+++ example/poisson_gmg/parthenon_app_inputs.cpp
@@ -38,19 +38,27 @@ void ProblemGenerator(Mesh *pm, ParameterInput *pin, MeshData<Real> *md) {
   Real z0 = pin->GetOrAddReal("poisson", "z0", 0.0);
   Real radius = pin->GetOrAddReal("poisson", "radius", 0.1);
 
-  auto desc = parthenon::MakePackDescriptor<poisson_package::res_err>(md);
+  auto desc = parthenon::MakePackDescriptor<poisson_package::res_err, poisson_package::rhs_base>(md);
   auto pack = desc.GetPack(md);
+  
+  constexpr auto te = poisson_package::te; 
 
   auto &cellbounds = pmb->cellbounds;
-  auto ib = cellbounds.GetBoundsI(IndexDomain::entire, TopologicalElement::NN);
-  auto jb = cellbounds.GetBoundsJ(IndexDomain::entire, TopologicalElement::NN);
-  auto kb = cellbounds.GetBoundsK(IndexDomain::entire, TopologicalElement::NN);
+  auto ib = cellbounds.GetBoundsI(IndexDomain::entire, te);
+  auto jb = cellbounds.GetBoundsJ(IndexDomain::entire, te);
+  auto kb = cellbounds.GetBoundsK(IndexDomain::entire, te);
   pmb->par_for(
       "Poisson::ProblemGenerator", 0, pack.GetNBlocks() - 1, kb.s, kb.e, jb.s, jb.e, ib.s,
       ib.e, KOKKOS_LAMBDA(const int b, const int k, const int j, const int i) {
         const auto &coords = pack.GetCoordinates(b);
-        pack(b, TopologicalElement::NN, poisson_package::res_err(), k, j, i) =
-            coords.X<1, TopologicalElement::NN>(i);
+        Real x = coords.X<1, te>(i);
+
+        Real val = 0.0;
+        if (x > 0.25 && x < 0.75) {
+          val = 100.0;
+        }
+        pack(b, te, poisson_package::res_err(), k, j, i) = val;
+        pack(b, te, poisson_package::rhs_base(), k, j, i) = val;
       });
 }
 
