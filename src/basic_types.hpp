e5fc879f33a4c07c3354ede854422a18f599153e
Luke Roberts <lfroberts@lanl.gov>
2023-08-15 17:06:54 -0600

    Move enums to basic_types

--- src/basic_types.hpp
+++ src/basic_types.hpp
@@ -36,6 +36,15 @@ using Real = double;
 #endif
 #endif
 
+// needed for arrays dimensioned over grid directions
+// enumerator type only used in Mesh::EnrollUserMeshGenerator()
+// X0DIR time-like direction
+// X1DIR x, r, etc...
+// X2DIR y, theta, etc...
+// X3DIR z, phi, etc...
+enum CoordinateDirection { NODIR = -1, X0DIR = 0, X1DIR = 1, X2DIR = 2, X3DIR = 3 };
+enum class BlockLocation {Left = 0, Center = 1, Right = 2};
+
 enum class TaskStatus { fail, complete, incomplete, iterate, skip };
 enum class AmrTag : int { derefine = -1, same = 0, refine = 1 };
 enum class RefinementOp_t { Prolongation, Restriction, None };
