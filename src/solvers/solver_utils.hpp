9746ad4eedee7f253781a2147ec327418bb4c49b
Luke Roberts <lfroberts@lanl.gov>
2023-10-03 15:28:30 -0600

    Move Jacobi

--- src/solvers/solver_utils.hpp
+++ src/solvers/solver_utils.hpp
@@ -292,54 +292,6 @@ TaskID DotProduct(TaskID dependency_in, TaskRegion &region, TL_t &tl, int partit
   return finish_global_adotb;
 }
 
-
-enum class GSType { all, red, black };
-template <class rhs_t, class Axold_t, class D_t, class xold_t, class xnew_t, bool only_md_level = false>
-TaskStatus Jacobi(std::shared_ptr<MeshData<Real>> &md, double weight,
-                      GSType gs_type = GSType::all) {
-  using namespace parthenon;
-  const int ndim = md->GetMeshPointer()->ndim;
-  using TE = parthenon::TopologicalElement;
-  TE te = TE::CC;
-  IndexRange ib = md->GetBoundsI(IndexDomain::interior, te);
-  IndexRange jb = md->GetBoundsJ(IndexDomain::interior, te);
-  IndexRange kb = md->GetBoundsK(IndexDomain::interior, te);
-
-  auto pkg = md->GetMeshPointer()->packages.Get("poisson_package");
-  const auto alpha = pkg->Param<Real>("diagonal_alpha");
-
-  int nblocks = md->NumBlocks();
-  std::vector<bool> include_block(nblocks, true);
-
-  if (only_md_level) {
-    for (int b = 0; b < nblocks; ++b)
-      include_block[b] =
-          (md->grid.logical_level == md->GetBlockData(b)->GetBlockPointer()->loc.level());
-  }
-
-  auto desc = parthenon::MakePackDescriptor<xold_t, xnew_t, Axold_t, rhs_t, D_t>(md.get());
-  auto pack = desc.GetPack(md.get(), include_block);
-  parthenon::par_for(
-      DEFAULT_LOOP_PATTERN, "CaclulateFluxes", DevExecSpace(), 0, pack.GetNBlocks() - 1,
-      kb.s, kb.e, jb.s, jb.e, ib.s, ib.e,
-      KOKKOS_LAMBDA(const int b, const int k, const int j, const int i) {
-        const auto &coords = pack.GetCoordinates(b);
-        if ((i + j + k) % 2 == 1 && gs_type == GSType::red) return;
-        if ((i + j + k) % 2 == 0 && gs_type == GSType::black) return;
-        
-        Real diag_elem = pack(b, te, D_t(), k, j, i);
-
-        // Get the off-diagonal contribution to Ax = (D + L + U)x = y
-        Real off_diag =
-            pack(b, te, Axold_t(), k, j, i) - diag_elem * pack(b, te, xold_t(), k, j, i);
-
-        Real val = pack(b, te, rhs_t(), k, j, i) - off_diag;
-        pack(b, te, xnew_t(), k, j, i) =
-            weight * val / diag_elem + (1.0 - weight) * pack(b, te, xold_t(), k, j, i);
-      });
-  return TaskStatus::complete;
-}
-
 struct flux_poisson {
 template <class var_t, bool only_md_level = false>
 static TaskStatus CalculateFluxes(std::shared_ptr<MeshData<Real>> &md) {
