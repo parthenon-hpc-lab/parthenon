db8c02afbdfbb1fcbfdb335d838a594af3d394d3
Ben Wibking <ben@wibking.com>
2024-02-02 14:22:21 -0500

    enable parabolic, cubic function tests

--- tst/unit/test_interpolation.cpp
+++ tst/unit/test_interpolation.cpp
@@ -42,7 +42,6 @@ TEST_CASE("interpolation of lines", "[interpolate_line]") {
   REQUIRE(interpolate_line(2.5) == 2.5);
 }
 
-#if 0
 TEST_CASE("interpolation of parabolas", "[interpolate_parabola]") {
   REQUIRE(interpolate_parabola(1.0) == 1.0);
   REQUIRE(interpolate_parabola(2.5) == 2.5*2.5);
@@ -54,7 +53,6 @@ TEST_CASE("interpolation of cubic functions", "[interpolate_cubic]") {
   REQUIRE(interpolate_cubic(2.5) == 2.5*2.5*2.5);
   REQUIRE(interpolate_cubic(4.0) == 64.0);
 }
-#endif
 
 double interpolate_broken_line(double x) {
   // f(x) = x
