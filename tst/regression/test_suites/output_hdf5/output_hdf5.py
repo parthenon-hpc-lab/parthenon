f57ce85c3ac7e452211d0c9112704fb0a172a96e
Ben Ryan <brryan@lanl.gov>
2024-03-15 13:42:16 -0600

    Update history output testing

--- tst/regression/test_suites/output_hdf5/output_hdf5.py
+++ tst/regression/test_suites/output_hdf5/output_hdf5.py
@@ -119,12 +119,15 @@ class TestCase(utils.test_case.TestCaseAbs):
         if ret_2d != 0 or ret_3d != 0:
             analyze_status = False
 
-        hst_2d = np.genfromtxt("advection_2d.hst")
-        hst_3d = np.genfromtxt("advection_3d.hst")
+        hst_2d = np.genfromtxt("advection_2d.out1.hst")
+        hst_3d = np.genfromtxt("advection_3d.out1.hst")
         ref_results = [
             ["time", 1.0, 1.0],
             ["dt", 1.75781e-03, 3.12500e-03],
             ["total", 7.06177e-02, 1.39160e-02],
+            ["power0", 7.06177e-02, 1.39160e-02],
+            ["power1", 3.88112e-02, 2.59597e-03],
+            ["power2", 2.65948e-02, 7.19427e-04],
             ["max", 9.43685e-01, 4.80914e-01],
             [
                 "min",
