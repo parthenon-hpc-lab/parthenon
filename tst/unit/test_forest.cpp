162842cf6ad107a1e0fcf7303ebd848a993f13f1
Luke Roberts <lfroberts@lanl.gov>
2024-05-06 16:28:32 -0600

    Add non-trivial forest tests

--- tst/unit/test_forest.cpp
+++ tst/unit/test_forest.cpp
@@ -24,7 +24,7 @@
 #include "mesh/forest/forest.hpp"
 
 using namespace parthenon::forest;
-
+namespace {
 void DerefineAllPossibleLocations(Forest &forest) {
   auto locs = forest.GetMeshBlockListAndResolveGids();
   std::vector<parthenon::LogicalLocation> deref_locs;
@@ -44,7 +44,50 @@ void DerefineAllPossibleLocations(Forest &forest) {
   }
 }
 
-TEST_CASE("Forest construction", "[forest]") {
+// Create an n-tree forest with one internal point of valence n and refine one tree
+// twice at a location next to the n-valence point. This should result in a forest with 
+// 10 + 7 * (n - 1) blocks after properly nested refinement. The logical coordinate 
+// transformations between the produced trees are nontrivial. If nblocks_min != nblocks_max, 
+// this creates a forest that is the combination of disconnected n-tree forests with 
+// n between nblocks_min and nblocks_max 
+Forest n_blocks(int nblocks_min, int nblocks_max) {
+  std::unordered_map<uint64_t, std::shared_ptr<Node>> nodes;
+  ForestDefinition forest_def;
+  int nc = 0;
+  int fc = 0;
+  parthenon::Real xoffset = 0.0;
+  for (int nblocks = nblocks_min; nblocks <= nblocks_max; ++nblocks) {
+    for (int point = 0; point < 2 * nblocks; ++point) {
+      nodes[nc + point] =
+          Node::create(nc + point, {std::sin(point * M_PI / nblocks) + xoffset,
+                                    std::cos(point * M_PI / nblocks)});
+    }
+    nodes[nc + 2 * nblocks] = Node::create(nc + 2 * nblocks, {0.0 + xoffset, 0.0});
+    auto &n = nodes;
+    for (int t = 0; t < nblocks; ++t)
+      forest_def.AddFace(fc + t, {n[nc + 2 * t + 1], n[nc + 2 * t],
+                                  n[nc + (2 * t + 2) % (2 * nblocks)],
+                                  n[nc + 2 * nblocks]});
+    nc += 2 * nblocks + 1;
+    fc += nblocks;
+    xoffset += 2.2;
+  }
+  auto forest = Forest::Make2D(forest_def);
+
+  // Do some refinements that should propagate into all trees
+  fc = 0;
+  for (int nblocks = nblocks_min; nblocks <= nblocks_max; ++nblocks) {
+    forest.Refine(parthenon::LogicalLocation(fc + 2, 0, 0, 0, 0));
+    forest.Refine(parthenon::LogicalLocation(fc + 2, 1, 1, 1, 0));
+    forest.Refine(parthenon::LogicalLocation(fc + 2, 2, 3, 3, 0));
+    fc += nblocks;
+  }
+
+  return forest;
+}
+}
+
+TEST_CASE("Simple forest construction", "[forest]") {
   // Create two trees in two dimensions that both have a single block
   auto tree1 = Tree::create(1, 2, 0);
   auto tree2 = Tree::create(2, 2, 0);
@@ -104,5 +147,28 @@ TEST_CASE("Forest construction", "[forest]") {
     DerefineAllPossibleLocations(forest);
     locs = forest.GetMeshBlockListAndResolveGids();
     REQUIRE(locs.size() == 2);
+  }
+}
+
+TEST_CASE("Singular forest construction", "[forest]") {
+  GIVEN("A forest with three trees and three-valent point") {
+    auto forest = n_blocks(3, 3); 
+    auto locs = forest.GetMeshBlockListAndResolveGids();
+    REQUIRE(locs.size() == 24);
+  }
+  GIVEN("A forest with four trees and four-valent point") {
+    auto forest = n_blocks(4, 4); 
+    auto locs = forest.GetMeshBlockListAndResolveGids();
+    REQUIRE(locs.size() == 31);
+  }
+  GIVEN("A forest with five trees and five-valent point") {
+    auto forest = n_blocks(5, 5); 
+    auto locs = forest.GetMeshBlockListAndResolveGids();
+    REQUIRE(locs.size() == 38);
+  }
+  GIVEN("A forest with three-, four-, and five-valent points") {
+    auto forest = n_blocks(3, 5); 
+    auto locs = forest.GetMeshBlockListAndResolveGids();
+    REQUIRE(locs.size() == 93);
   }
 }
