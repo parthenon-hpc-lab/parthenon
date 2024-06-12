ca69688b19ba261e84ec4014a21c77a89a87d243
Luke Roberts <lfroberts@lanl.gov>
2024-06-12 16:30:06 -0600

    Make reading solver parameters easier to deal with

--- example/poisson_gmg/poisson_package.cpp
+++ example/poisson_gmg/poisson_package.cpp
@@ -79,54 +79,17 @@ std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin) {
   int max_poisson_iterations = pin->GetOrAddInteger("poisson", "max_iterations", 10000);
   pkg->AddParam<>("max_iterations", max_poisson_iterations);
 
-  int pre_smooth_iterations = pin->GetOrAddInteger("poisson", "pre_smooth_iterations", 2);
-  pkg->AddParam<>("pre_smooth_iterations", pre_smooth_iterations);
-
-  int post_smooth_iterations =
-      pin->GetOrAddInteger("poisson", "post_smooth_iterations", 2);
-  pkg->AddParam<>("post_smooth_iterations", post_smooth_iterations);
-
   Real diagonal_alpha = pin->GetOrAddReal("poisson", "diagonal_alpha", 0.0);
   pkg->AddParam<>("diagonal_alpha", diagonal_alpha);
-
-  Real jacobi_damping = pin->GetOrAddReal("poisson", "jacobi_damping", 0.5);
-  pkg->AddParam<>("jacobi_damping", jacobi_damping);
-
-  std::string smoother_method = pin->GetOrAddString("poisson", "smoother", "SRJ2");
-  pkg->AddParam<>("smoother", smoother_method);
-
-  bool do_FAS = pin->GetOrAddBoolean("poisson", "do_FAS", false);
-  pkg->AddParam<>("do_FAS", do_FAS);
 
   std::string solver = pin->GetOrAddString("poisson", "solver", "MG");
   pkg->AddParam<>("solver", solver);
 
-  bool precondition = pin->GetOrAddBoolean("poisson", "precondition", true);
-  pkg->AddParam<>("precondition", precondition);
-
-  int precondition_vcycles = pin->GetOrAddInteger("poisson", "precondition_vcycles", 1);
-  pkg->AddParam<>("precondition_vcycles", precondition_vcycles);
-
   bool flux_correct = pin->GetOrAddBoolean("poisson", "flux_correct", false);
   pkg->AddParam<>("flux_correct", flux_correct);
 
-  Real restart_threshold = pin->GetOrAddReal("poisson", "restart_threshold", 0.0);
-  pkg->AddParam<>("restart_threshold", restart_threshold);
-
-  int check_interval = pin->GetOrAddInteger("poisson", "check_interval", 100);
-  pkg->AddParam<>("check_interval", check_interval);
-
   Real err_tol = pin->GetOrAddReal("poisson", "error_tolerance", 1.e-8);
   pkg->AddParam<>("error_tolerance", err_tol);
-
-  Real res_tol = pin->GetOrAddReal("poisson", "residual_tolerance", 1.e-8);
-  pkg->AddParam<>("residual_tolerance", res_tol);
-
-  bool fail_flag = pin->GetOrAddBoolean("poisson", "fail_without_convergence", false);
-  pkg->AddParam<>("fail_without_convergence", fail_flag);
-
-  bool warn_flag = pin->GetOrAddBoolean("poisson", "warn_without_convergence", true);
-  pkg->AddParam<>("warn_without_convergence", warn_flag);
 
   bool use_exact_rhs = pin->GetOrAddBoolean("poisson", "use_exact_rhs", false);
   pkg->AddParam<>("use_exact_rhs", use_exact_rhs);
@@ -134,20 +97,12 @@ std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin) {
   PoissonEquation eq;
   eq.do_flux_cor = flux_correct;
 
-  parthenon::solvers::MGParams mg_params;
-  mg_params.max_iters = max_poisson_iterations;
-  mg_params.residual_tolerance = res_tol;
-  mg_params.do_FAS = do_FAS;
-  mg_params.smoother = smoother_method;
+  parthenon::solvers::MGParams mg_params(pin, "poisson/solver_params");
   parthenon::solvers::MGSolver<u, rhs, PoissonEquation> mg_solver(pkg.get(), mg_params,
                                                                   eq);
   pkg->AddParam<>("MGsolver", mg_solver, parthenon::Params::Mutability::Mutable);
 
-  parthenon::solvers::BiCGSTABParams bicgstab_params;
-  bicgstab_params.max_iters = max_poisson_iterations;
-  bicgstab_params.residual_tolerance = res_tol;
-  bicgstab_params.precondition = precondition;
-  bicgstab_params.print_per_step = true;
+  parthenon::solvers::BiCGSTABParams bicgstab_params(pin, "poisson/solver_params");
   parthenon::solvers::BiCGSTABSolver<u, rhs, PoissonEquation> bicg_solver(
       pkg.get(), bicgstab_params, eq);
   pkg->AddParam<>("MGBiCGSTABsolver", bicg_solver,
