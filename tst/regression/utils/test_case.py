e8c268e5ebae64f18682048f1333968da9ca7df7
Philipp Grete <pgrete@hs.uni-hamburg.de>
2024-07-02 16:34:36 +0200

    Fix formatting

--- tst/regression/utils/test_case.py
+++ tst/regression/utils/test_case.py
@@ -89,7 +89,7 @@ class TestManager:
         try:
             parthenon_path = os.path.realpath(__file__)
             idx = parthenon_path.rindex("/parthenon/")
-            self.parameters.parthenon_path = parthenon_path[:idx + 10]
+            self.parameters.parthenon_path = parthenon_path[: idx + 10]
         except ValueError:
             baseDir = os.path.dirname(__file__)
             self.parameters.parthenon_path = os.path.abspath(baseDir + "/../../../")
