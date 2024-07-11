af4b966a8025e0588dedda9b684c07ec2e88b2fc
Philipp Grete <pgrete@hs.uni-hamburg.de>
2024-07-11 10:14:57 +0200

    Fix formatting

--- src/outputs/parthenon_opmd.cpp
+++ src/outputs/parthenon_opmd.cpp
@@ -298,7 +298,7 @@ void OpenPMDOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin, SimTime *tm,
     }
 
     it.setAttribute("BoundaryConditions", boundary_condition_str);
-  }                                 // Info section
+  } // Info section
 
   Kokkos::Profiling::popRegion(); // write Attributes
 
@@ -505,10 +505,10 @@ void OpenPMDOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin, SimTime *tm,
             }
           }
         } // loop over components
-      }   // out_var->IsAllocated()
-    }     // loop over blocks
+      } // out_var->IsAllocated()
+    } // loop over blocks
     it.seriesFlush();
-  }                               // loop over vars
+  } // loop over vars
   Kokkos::Profiling::popRegion(); // write all variable data
 
   // The iteration can be closed in order to help free up resources.
