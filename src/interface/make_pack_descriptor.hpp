090a8d968f67aa9785bb6ecd9d64156d28206b5d
Luke Roberts <lfroberts@lanl.gov>
2024-07-30 21:47:12 -0600

    format and lint

--- src/interface/make_pack_descriptor.hpp
+++ src/interface/make_pack_descriptor.hpp
@@ -137,13 +137,13 @@ inline auto MakePackDescriptor(StateDescriptor *psd, const std::vector<Uid_t> &v
   return typename SparsePack<>::Descriptor(base_desc);
 }
 
-template <class... Types, class... Args> 
-inline auto MakePackDescriptorFromTypeList(TypeList<Types...>, Args&&... args) { 
+template <class... Types, class... Args>
+inline auto MakePackDescriptorFromTypeList(TypeList<Types...>, Args &&...args) {
   return MakePackDescriptor<Types...>(std::forward<Args>(args)...);
 }
 
-template <class TL, class... Args> 
-inline auto MakePackDescriptorFromTypeList(Args&&... args) { 
+template <class TL, class... Args>
+inline auto MakePackDescriptorFromTypeList(Args &&...args) {
   return MakePackDescriptorFromTypeList(TL(), std::forward<Args>(args)...);
 }
 
