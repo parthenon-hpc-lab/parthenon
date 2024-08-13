e0354f6675d53c6420f2ad4b11f76b6420d296bc
Luke Roberts <lfroberts@lanl.gov>
2024-08-13 14:31:46 -0600

    fix partitioning tests

--- src/utils/partition_stl_containers.hpp
+++ src/utils/partition_stl_containers.hpp
@@ -47,8 +47,6 @@ auto ToSizeN(Container_t<T, extra...> &container, const int N) {
   using std::to_string;
   using namespace partition_impl;
 
-  if (N == 0) return Partition_t<T>();
-
   PARTHENON_REQUIRE_THROWS(N > 0, "Your partition must be at least size 1");
 
   int nelements = container.size();
