4fa3932f846711fcd7961265968e011bcef762cd
adam reyes <adam.c.reyes@gmail.com>
2024-09-07 15:23:04 +0200

    workaround for rtd theme table box line breaks

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -49,6 +49,7 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
+html_css_files = ["css/custom.css"]
 
 # configuration for sphinx_multiversion
 smv_remote_whitelist = r"^(origin)$"
