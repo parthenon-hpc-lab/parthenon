85568d68a95f0d724a44280a598322faf5ae52be
adam reyes <adam.c.reyes@gmail.com>
2024-09-08 16:57:28 +0200

    Revert "workaround for rtd theme table box line breaks"
    This reverts commit 4fa3932f846711fcd7961265968e011bcef762cd.

--- doc/sphinx/conf.py
+++ doc/sphinx/conf.py
@@ -49,7 +49,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_css_files = ["css/custom.css"]
 
 # configuration for sphinx_multiversion
 smv_remote_whitelist = r"^(origin)$"
