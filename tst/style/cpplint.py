7fb2920f71ab846ad8df355bf2b845c8cbfc23e2
Jonah Miller <jonahm@lanl.gov>
2024-09-12 16:15:38 -0600

    sre_compile now deprecated

--- tst/style/cpplint.py
+++ tst/style/cpplint.py
@@ -837,7 +837,7 @@ def Match(pattern, s):
     # performance reasons; factoring it out into a separate function turns out
     # to be noticeably expensive.
     if pattern not in _regexp_compile_cache:
-        _regexp_compile_cache[pattern] = sre_compile.compile(pattern)
+        _regexp_compile_cache[pattern] = re.compile(pattern)
     return _regexp_compile_cache[pattern].match(s)
 
 
@@ -855,14 +855,14 @@ def ReplaceAll(pattern, rep, s):
       string with replacements made (or original string if no replacements)
     """
     if pattern not in _regexp_compile_cache:
-        _regexp_compile_cache[pattern] = sre_compile.compile(pattern)
+        _regexp_compile_cache[pattern] = re.compile(pattern)
     return _regexp_compile_cache[pattern].sub(rep, s)
 
 
 def Search(pattern, s):
     """Searches the string for the pattern, caching the compiled regexp."""
     if pattern not in _regexp_compile_cache:
-        _regexp_compile_cache[pattern] = sre_compile.compile(pattern)
+        _regexp_compile_cache[pattern] = re.compile(pattern)
     return _regexp_compile_cache[pattern].search(s)
 
 
