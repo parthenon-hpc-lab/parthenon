a9fb9ea0617f0c26a4b82e01b0fda237450ca59c
Ben Ryan <brryan@lanl.gov>
2024-11-07 16:12:43 -0700

    retest

--- tst/regression/test_suites/particle_leapfrog_outflow/particle_leapfrog_outflow.py
+++ tst/regression/test_suites/particle_leapfrog_outflow/particle_leapfrog_outflow.py
@@ -57,5 +57,7 @@ class TestCase(utils.test_case.TestCaseAbs):
         )
         if ref_data.shape != final_data.shape:
             print("TEST FAIL: Mismatch between actual and reference data shape.")
+            print(ref_data)
+            print(final_data)
             return False
         return (np.abs(final_data - ref_data) <= 1e-10).all()
