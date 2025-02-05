683ebf6d8497a1adb43ada486b59a1d140f259a8
jdolence <jdolence@lanl.gov>
2025-02-05 18:06:22 -0500

    little cleanup

--- src/coordinates/uniform_cylindrical.hpp
+++ src/coordinates/uniform_cylindrical.hpp
@@ -21,10 +21,6 @@ class UniformCylindrical : public UniformCoordinates<UniformCylindrical> {
   using base_t = UniformCoordinates<UniformCylindrical>;
 
  public:
-  using base_t::CellWidth;
-  using base_t::Dxc;
-  using base_t::Scale;
-  using base_t::Volume;
   using base_t::Xc;
   UniformCylindrical() = default;
   UniformCylindrical(const RegionSize &rs, ParameterInput *pin)
