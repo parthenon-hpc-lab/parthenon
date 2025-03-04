6c76498f86df884ba1c80401d6ba834e062d577b
Luke Roberts <lfroberts@lanl.gov>
2025-03-04 13:59:34 -0700

    format

--- example/poisson_gmg/poisson_package.cpp
+++ example/poisson_gmg/poisson_package.cpp
@@ -25,8 +25,8 @@
 #include <parthenon/package.hpp>
 #include <solvers/bicgstab_solver.hpp>
 #include <solvers/cg_solver.hpp>
-#include <solvers/tridiag_solver.hpp>
 #include <solvers/solver_utils.hpp>
+#include <solvers/tridiag_solver.hpp>
 
 #include "defs.hpp"
 #include "kokkos_abstraction.hpp"
@@ -107,9 +107,8 @@ std::shared_ptr<StateDescriptor> Initialize(ParameterInput *pin) {
         std::make_shared<parthenon::solvers::BiCGSTABSolver<PoissEq, preconditioner_t>>(
             "base", "u", "rhs", pin, "poisson/solver_params", PoissEq(pin, "poisson"));
   } else if (solver == "Tridiag") {
-    psolver =
-        std::make_shared<parthenon::solvers::TridiagSolver<PoissEq>>(
-            "base", "u", "rhs", pin, "poisson/solver_params", PoissEq(pin, "poisson"));
+    psolver = std::make_shared<parthenon::solvers::TridiagSolver<PoissEq>>(
+        "base", "u", "rhs", pin, "poisson/solver_params", PoissEq(pin, "poisson"));
   } else {
     PARTHENON_FAIL("Unknown solver type.");
   }
--- src/solvers/tridiag_solver.hpp
+++ src/solvers/tridiag_solver.hpp
@@ -56,22 +56,23 @@ class TridiagSolver : public SolverBase {
   // User defined container containing the rhs vector, only needs to contain sol_fields
   std::string container_rhs;
   // Internal containers for solver which create deep copies of sol_fields
-  std::string container_100, container_010, container_001,
-              container_100_out, container_010_out, container_001_out,
-              container_Aup, container_Adi, container_Alo, container_r;
+  std::string container_100, container_010, container_001, container_100_out,
+      container_010_out, container_001_out, container_Aup, container_Adi, container_Alo,
+      container_r;
 
   static inline std::size_t id{0};
 
  public:
   TridiagSolver(const std::string &container_base, const std::string &container_u,
-           const std::string &container_rhs, ParameterInput *pin,
-           const std::string &input_block, const equations &eq_in = equations())
+                const std::string &container_rhs, ParameterInput *pin,
+                const std::string &input_block, const equations &eq_in = equations())
       : container_base(container_base), container_u(container_u),
-        container_rhs(container_rhs), iter_counter(0),
-        eqs_(eq_in) {
+        container_rhs(container_rhs), iter_counter(0), eqs_(eq_in) {
     FieldTL::IterateTypes(
         [this](auto t) { this->sol_fields.push_back(decltype(t)::name()); });
-    PARTHENON_REQUIRE(sol_fields.size() == 1, "Tridiagonal solver only works for a single field on a single one-dimensional block.");
+    PARTHENON_REQUIRE(sol_fields.size() == 1,
+                      "Tridiagonal solver only works for a single field on a single "
+                      "one-dimensional block.");
     std::string solver_id = "tridiag" + std::to_string(id++);
     container_100 = solver_id + "_100";
     container_010 = solver_id + "_010";
@@ -91,14 +92,15 @@ class TridiagSolver : public SolverBase {
     return dependence;
   }
 
-  static TaskStatus SetMasks(const std::shared_ptr<MeshData<Real>> &md_100, 
-                      const std::shared_ptr<MeshData<Real>> &md_010,
-                      const std::shared_ptr<MeshData<Real>> &md_001) {
+  static TaskStatus SetMasks(const std::shared_ptr<MeshData<Real>> &md_100,
+                             const std::shared_ptr<MeshData<Real>> &md_010,
+                             const std::shared_ptr<MeshData<Real>> &md_001) {
     IndexRange ib = md_100->GetBoundsI(IndexDomain::interior);
     IndexRange jb = md_100->GetBoundsJ(IndexDomain::interior);
     IndexRange kb = md_100->GetBoundsK(IndexDomain::interior);
 
-    PARTHENON_REQUIRE(md_100->GetMeshPointer()->nbtotal == 1, "Tridiag only works for a single block.");
+    PARTHENON_REQUIRE(md_100->GetMeshPointer()->nbtotal == 1,
+                      "Tridiag only works for a single block.");
     PARTHENON_REQUIRE(jb.s == jb.e, "Must be one dimensional.");
     PARTHENON_REQUIRE(kb.s == kb.e, "Must be one dimensional.");
 
@@ -108,32 +110,34 @@ class TridiagSolver : public SolverBase {
     auto pack_001 = desc.GetPack(md_001.get());
 
     parthenon::par_for(
-      DEFAULT_LOOP_PATTERN, "SetMasks", DevExecSpace(), 0, pack_100.GetNBlocks() - 1, kb.s,
-      kb.e, jb.s, jb.e, ib.s, ib.e,
-      KOKKOS_LAMBDA(const int b, const int k, const int j, const int i) {
-        pack_100(b, 0, k, j, i) = ((i - ib.s + 3) % 3) == 0; 
-        pack_010(b, 0, k, j, i) = ((i - ib.s + 2) % 3) == 0; 
-        pack_001(b, 0, k, j, i) = ((i - ib.s + 1) % 3) == 0; 
-    });
+        DEFAULT_LOOP_PATTERN, "SetMasks", DevExecSpace(), 0, pack_100.GetNBlocks() - 1,
+        kb.s, kb.e, jb.s, jb.e, ib.s, ib.e,
+        KOKKOS_LAMBDA(const int b, const int k, const int j, const int i) {
+          pack_100(b, 0, k, j, i) = ((i - ib.s + 3) % 3) == 0;
+          pack_010(b, 0, k, j, i) = ((i - ib.s + 2) % 3) == 0;
+          pack_001(b, 0, k, j, i) = ((i - ib.s + 1) % 3) == 0;
+        });
 
     return TaskStatus::complete;
   }
 
-  static TaskStatus SetDiagonals(const std::shared_ptr<MeshData<Real>> &md_100_out, 
-                          const std::shared_ptr<MeshData<Real>> &md_010_out,
-                          const std::shared_ptr<MeshData<Real>> &md_001_out,
-                          const std::shared_ptr<MeshData<Real>> &md_Alo,
-                          const std::shared_ptr<MeshData<Real>> &md_Adi,
-                          const std::shared_ptr<MeshData<Real>> &md_Aup) {
+  static TaskStatus SetDiagonals(const std::shared_ptr<MeshData<Real>> &md_100_out,
+                                 const std::shared_ptr<MeshData<Real>> &md_010_out,
+                                 const std::shared_ptr<MeshData<Real>> &md_001_out,
+                                 const std::shared_ptr<MeshData<Real>> &md_Alo,
+                                 const std::shared_ptr<MeshData<Real>> &md_Adi,
+                                 const std::shared_ptr<MeshData<Real>> &md_Aup) {
     IndexRange ib = md_Alo->GetBoundsI(IndexDomain::interior);
     IndexRange jb = md_Alo->GetBoundsJ(IndexDomain::interior);
     IndexRange kb = md_Alo->GetBoundsK(IndexDomain::interior);
-    
-    PARTHENON_REQUIRE(md_100_out->GetMeshPointer()->nbtotal == 1, "Tridiag only works for a single block.");
+
+    PARTHENON_REQUIRE(md_100_out->GetMeshPointer()->nbtotal == 1,
+                      "Tridiag only works for a single block.");
     PARTHENON_REQUIRE(jb.s == jb.e, "Must be one dimensional.");
     PARTHENON_REQUIRE(kb.s == kb.e, "Must be one dimensional.");
-    
-    static auto desc = parthenon::MakePackDescriptorFromTypeList<FieldTL>(md_100_out.get());
+
+    static auto desc =
+        parthenon::MakePackDescriptorFromTypeList<FieldTL>(md_100_out.get());
     auto pack_100 = desc.GetPack(md_100_out.get());
     auto pack_010 = desc.GetPack(md_010_out.get());
     auto pack_001 = desc.GetPack(md_001_out.get());
@@ -142,25 +146,25 @@ class TridiagSolver : public SolverBase {
     auto pack_up = desc.GetPack(md_Aup.get());
 
     parthenon::par_for(
-      DEFAULT_LOOP_PATTERN, "SetDiagonalsBasedOnMasks", DevExecSpace(), 0, pack_100.GetNBlocks() - 1,
-      kb.s, kb.e, jb.s, jb.e, ib.s, ib.e,
-      KOKKOS_LAMBDA(const int b, const int k, const int j, const int i) {
-        if ((i - ib.s) % 3 == 0) {
-          pack_di(b, 0, k, j, i) = pack_100(b, 0, k, j, i); 
-          pack_up(b, 0, k, j, i) = pack_010(b, 0, k, j, i); 
-          pack_lo(b, 0, k, j, i) = pack_001(b, 0, k, j, i); 
-        }
-        else if ((i - ib.s) % 3 == 1) {
-          pack_lo(b, 0, k, j, i) = pack_100(b, 0, k, j, i);
-          pack_di(b, 0, k, j, i) = pack_010(b, 0, k, j, i); 
-          pack_up(b, 0, k, j, i) = pack_001(b, 0, k, j, i); 
-        } else {
-          pack_up(b, 0, k, j, i) = pack_100(b, 0, k, j, i);
-          pack_lo(b, 0, k, j, i) = pack_010(b, 0, k, j, i); 
-          pack_di(b, 0, k, j, i) = pack_001(b, 0, k, j, i); 
-        }
-        printf("row %i: (%e, %e, %e) \n", i, pack_lo(b, 0, k, j, i), pack_di(b, 0, k, j, i), pack_up(b, 0, k, j, i));
-    });
+        DEFAULT_LOOP_PATTERN, "SetDiagonalsBasedOnMasks", DevExecSpace(), 0,
+        pack_100.GetNBlocks() - 1, kb.s, kb.e, jb.s, jb.e, ib.s, ib.e,
+        KOKKOS_LAMBDA(const int b, const int k, const int j, const int i) {
+          if ((i - ib.s) % 3 == 0) {
+            pack_di(b, 0, k, j, i) = pack_100(b, 0, k, j, i);
+            pack_up(b, 0, k, j, i) = pack_010(b, 0, k, j, i);
+            pack_lo(b, 0, k, j, i) = pack_001(b, 0, k, j, i);
+          } else if ((i - ib.s) % 3 == 1) {
+            pack_lo(b, 0, k, j, i) = pack_100(b, 0, k, j, i);
+            pack_di(b, 0, k, j, i) = pack_010(b, 0, k, j, i);
+            pack_up(b, 0, k, j, i) = pack_001(b, 0, k, j, i);
+          } else {
+            pack_up(b, 0, k, j, i) = pack_100(b, 0, k, j, i);
+            pack_lo(b, 0, k, j, i) = pack_010(b, 0, k, j, i);
+            pack_di(b, 0, k, j, i) = pack_001(b, 0, k, j, i);
+          }
+          printf("row %i: (%e, %e, %e) \n", i, pack_lo(b, 0, k, j, i),
+                 pack_di(b, 0, k, j, i), pack_up(b, 0, k, j, i));
+        });
 
     return TaskStatus::complete;
   }
@@ -173,56 +177,67 @@ class TridiagSolver : public SolverBase {
     IndexRange ib = md_Alo->GetBoundsI(IndexDomain::interior);
     IndexRange jb = md_Alo->GetBoundsJ(IndexDomain::interior);
     IndexRange kb = md_Alo->GetBoundsK(IndexDomain::interior);
-    
-    PARTHENON_REQUIRE(md_Alo->GetMeshPointer()->nbtotal == 1, "Tridiag only works for a single block.");
+
+    PARTHENON_REQUIRE(md_Alo->GetMeshPointer()->nbtotal == 1,
+                      "Tridiag only works for a single block.");
     PARTHENON_REQUIRE(jb.s == jb.e, "Must be one dimensional.");
     PARTHENON_REQUIRE(kb.s == kb.e, "Must be one dimensional.");
-    
+
     static auto desc = parthenon::MakePackDescriptorFromTypeList<FieldTL>(md_Alo.get());
     auto pack_lo = desc.GetPack(md_Alo.get());
     auto pack_di = desc.GetPack(md_Adi.get());
     auto pack_up = desc.GetPack(md_Aup.get());
     auto pack_rhs = desc.GetPack(md_rhs.get());
     auto pack_out = desc.GetPack(md_out.get());
-    
-    const int b = 0; 
+
+    const int b = 0;
     const int k = kb.s;
     const int j = jb.s;
 
-    // Since this needs to be sequential, we launch an outer loop of size one. Obviously this would 
-    // be really inefficient on device 
+    // Since this needs to be sequential, we launch an outer loop of size one. Obviously
+    // this would be really inefficient on device
     parthenon::par_for(
-      DEFAULT_LOOP_PATTERN, "DotProduct", DevExecSpace(), 0, 0,
-      KOKKOS_LAMBDA(const int) {
-        printf("\n");
-        pack_di(b, 0, k, j, ib.s - 1) = 1.0;
-        for (int i = ib.s; i <= ib.e; ++i) {
-          pack_di(b, 0, k, j, i) -= pack_lo(b, 0, k, j, i) * pack_up(b, 0, k, j, i - 1) / pack_di(b, 0, k, j, i - 1);  
-          pack_rhs(b, 0, k, j, i) -= pack_lo(b, 0, k, j, i) * pack_rhs(b, 0, k, j, i - 1) / pack_di(b, 0, k, j, i - 1);  
-          printf("[%i] (%e %e %e) x = %e\n", i, pack_lo(b, 0, k, j, i), pack_di(b, 0, k, j, i), pack_up(b, 0, k, j, i), pack_rhs(b, 0, k, j, i));
-        }
-        printf("\n");
-        pack_out(b, 0, k, j, ib.e + 1) = 0.0; 
-        for (int i = ib.e; i >= ib.s; --i) { 
-          pack_out(b, 0, k, j, i) = (pack_rhs(b, 0, k, j, i)
-                                   - pack_up(b, 0, k, j, i) * pack_out(b, 0, k, j, i + 1)) / pack_di(b, 0, k, j, i);
-          printf("[%i] (%e %e %e) %e = %e\n", i, pack_lo(b, 0, k, j, i), pack_di(b, 0, k, j, i), pack_up(b, 0, k, j, i), pack_out(b, 0, k, j, i), pack_rhs(b, 0, k, j, i));
-        }
-        pack_out(b, 0, k, j, ib.s - 1) = 0.0;
-        printf("\n");
-    });
+        DEFAULT_LOOP_PATTERN, "DotProduct", DevExecSpace(), 0, 0,
+        KOKKOS_LAMBDA(const int) {
+          printf("\n");
+          pack_di(b, 0, k, j, ib.s - 1) = 1.0;
+          for (int i = ib.s; i <= ib.e; ++i) {
+            pack_di(b, 0, k, j, i) -= pack_lo(b, 0, k, j, i) *
+                                      pack_up(b, 0, k, j, i - 1) /
+                                      pack_di(b, 0, k, j, i - 1);
+            pack_rhs(b, 0, k, j, i) -= pack_lo(b, 0, k, j, i) *
+                                       pack_rhs(b, 0, k, j, i - 1) /
+                                       pack_di(b, 0, k, j, i - 1);
+            printf("[%i] (%e %e %e) x = %e\n", i, pack_lo(b, 0, k, j, i),
+                   pack_di(b, 0, k, j, i), pack_up(b, 0, k, j, i),
+                   pack_rhs(b, 0, k, j, i));
+          }
+          printf("\n");
+          pack_out(b, 0, k, j, ib.e + 1) = 0.0;
+          for (int i = ib.e; i >= ib.s; --i) {
+            pack_out(b, 0, k, j, i) =
+                (pack_rhs(b, 0, k, j, i) -
+                 pack_up(b, 0, k, j, i) * pack_out(b, 0, k, j, i + 1)) /
+                pack_di(b, 0, k, j, i);
+            printf("[%i] (%e %e %e) %e = %e\n", i, pack_lo(b, 0, k, j, i),
+                   pack_di(b, 0, k, j, i), pack_up(b, 0, k, j, i),
+                   pack_out(b, 0, k, j, i), pack_rhs(b, 0, k, j, i));
+          }
+          pack_out(b, 0, k, j, ib.s - 1) = 0.0;
+          printf("\n");
+        });
 
     return TaskStatus::complete;
   }
-
 
   static TaskStatus PrintSolution(const std::shared_ptr<MeshData<Real>> &md_r,
                                   const std::shared_ptr<MeshData<Real>> &md_rhs) {
     IndexRange ib = md_r->GetBoundsI(IndexDomain::interior);
     IndexRange jb = md_r->GetBoundsJ(IndexDomain::interior);
     IndexRange kb = md_r->GetBoundsK(IndexDomain::interior);
-    
-    PARTHENON_REQUIRE(md_r->GetMeshPointer()->nbtotal == 1, "Tridiag only works for a single block.");
+
+    PARTHENON_REQUIRE(md_r->GetMeshPointer()->nbtotal == 1,
+                      "Tridiag only works for a single block.");
     PARTHENON_REQUIRE(jb.s == jb.e, "Must be one dimensional.");
     PARTHENON_REQUIRE(kb.s == kb.e, "Must be one dimensional.");
 
@@ -230,13 +245,12 @@ class TridiagSolver : public SolverBase {
     auto pack_r = desc.GetPack(md_r.get());
     auto pack_rhs = desc.GetPack(md_rhs.get());
 
-
     parthenon::par_for(
-      DEFAULT_LOOP_PATTERN, "SetDiagonalsBasedOnMasks", DevExecSpace(), 0, 0,
-      kb.s, kb.e, jb.s, jb.e, ib.s, ib.e,
-      KOKKOS_LAMBDA(const int b, const int k, const int j, const int i) {
-        printf("row %i: %e  %e \n", i, pack_r(b, 0, k, j, i), pack_rhs(b, 0, k, j, i));
-    }); 
+        DEFAULT_LOOP_PATTERN, "SetDiagonalsBasedOnMasks", DevExecSpace(), 0, 0, kb.s,
+        kb.e, jb.s, jb.e, ib.s, ib.e,
+        KOKKOS_LAMBDA(const int b, const int k, const int j, const int i) {
+          printf("row %i: %e  %e \n", i, pack_r(b, 0, k, j, i), pack_rhs(b, 0, k, j, i));
+        });
     return TaskStatus::complete;
   }
 
@@ -257,10 +271,13 @@ class TridiagSolver : public SolverBase {
     auto &md_010 = pmesh->mesh_data.Add(container_010, partitions[partition], sol_fields);
     auto &md_001 = pmesh->mesh_data.Add(container_001, partitions[partition], sol_fields);
 
-    auto &md_100_out = pmesh->mesh_data.Add(container_100_out, partitions[partition], sol_fields);
-    auto &md_010_out = pmesh->mesh_data.Add(container_010_out, partitions[partition], sol_fields);
-    auto &md_001_out = pmesh->mesh_data.Add(container_001_out, partitions[partition], sol_fields);
-    
+    auto &md_100_out =
+        pmesh->mesh_data.Add(container_100_out, partitions[partition], sol_fields);
+    auto &md_010_out =
+        pmesh->mesh_data.Add(container_010_out, partitions[partition], sol_fields);
+    auto &md_001_out =
+        pmesh->mesh_data.Add(container_001_out, partitions[partition], sol_fields);
+
     auto &md_Alo = pmesh->mesh_data.Add(container_Alo, partitions[partition], sol_fields);
     auto &md_Adi = pmesh->mesh_data.Add(container_Adi, partitions[partition], sol_fields);
     auto &md_Aup = pmesh->mesh_data.Add(container_Aup, partitions[partition], sol_fields);
@@ -268,20 +285,19 @@ class TridiagSolver : public SolverBase {
 
     iter_counter = 0;
     bool multilevel = pmesh->multilevel;
-    
-    auto masks = tl.AddTask(dependence, SetMasks, md_100, md_010, md_001); 
-    auto Ax100 = eqs_.Ax(tl, masks, md_base, md_100, md_100_out); 
-    auto Ax010 = eqs_.Ax(tl, Ax100, md_base, md_010, md_010_out); 
-    auto Ax001 = eqs_.Ax(tl, Ax010, md_base, md_001, md_001_out); 
-    auto explicit_A = tl.AddTask(Ax001, SetDiagonals,
-                                 md_100_out, md_010_out, md_001_out,
-                                 md_Alo, md_Adi, md_Aup); 
-    
+
+    auto masks = tl.AddTask(dependence, SetMasks, md_100, md_010, md_001);
+    auto Ax100 = eqs_.Ax(tl, masks, md_base, md_100, md_100_out);
+    auto Ax010 = eqs_.Ax(tl, Ax100, md_base, md_010, md_010_out);
+    auto Ax001 = eqs_.Ax(tl, Ax010, md_base, md_001, md_001_out);
+    auto explicit_A = tl.AddTask(Ax001, SetDiagonals, md_100_out, md_010_out, md_001_out,
+                                 md_Alo, md_Adi, md_Aup);
+
     auto copy_rhs = tl.AddTask(explicit_A, utils::CopyData<FieldTL>, md_rhs, md_r);
     auto sol = tl.AddTask(copy_rhs, Solve, md_Alo, md_Adi, md_Aup, md_r, md_u);
-    
+
     sol = tl.AddTask(sol, utils::SetToZero<FieldTL>, md_r);
-    auto Ax_check = eqs_.Ax(tl, sol, md_base, md_u, md_r); 
+    auto Ax_check = eqs_.Ax(tl, sol, md_base, md_u, md_r);
     auto print = tl.AddTask(Ax_check, PrintSolution, md_r, md_rhs);
 
     return print;
