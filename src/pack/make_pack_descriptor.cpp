2f51d867a5ee241c6ef6ac20f0ab5d288d54daee
Luke Roberts <lfroberts@lanl.gov>
2025-04-09 19:12:02 -0600

    switch to a slightly different model

--- src/pack/make_pack_descriptor.cpp
+++ src/pack/make_pack_descriptor.cpp
@@ -11,9 +11,12 @@
 // the public, perform publicly and display publicly, and to permit others to do so.
 //========================================================================================
 
+#include <memory>
 #include <regex>
 #include <set>
 #include <string>
+#include <tuple>
+#include <unordered_map>
 #include <vector>
 
 #include "mesh/meshblock.hpp"
@@ -21,11 +24,34 @@
 
 namespace parthenon {
 namespace impl {
-PackDescriptor MakePackDescriptorBase(StateDescriptor *psd,
+class PackDescCache : public PackDescriptorCacheBase {
+ public:
+  using key_t = std::tuple<std::vector<std::string>, std::vector<bool>,
+                           std::vector<MetadataFlag>, std::set<PDOpt>>;
+  std::unordered_map<key_t, impl::PackDescriptor> map;
+};
+
+PackDescriptor MakePackDescriptorBase(StateDescriptor *psd, Mesh *pmesh,
                                       const std::vector<std::string> &vars,
                                       const std::vector<bool> &use_regex,
                                       const std::vector<MetadataFlag> &flags,
                                       const std::set<PDOpt> &options) {
+  const std::string cache_label{"normal"};
+  if (pmesh) {
+    if (!pmesh->pack_desc_cache_map.count(cache_label)) {
+      // Create a cache for PackDescriptors created with this particular selector
+      auto pcache = std::make_shared<PackDescCache>();
+      pmesh->pack_desc_cache_map.emplace(
+          cache_label, std::dynamic_pointer_cast<PackDescriptorCacheBase>(pcache));
+    } else {
+      // Check if a PackDescriptor already exists in the cache
+      auto pcache = std::dynamic_pointer_cast<PackDescCache>(
+          pmesh->pack_desc_cache_map[cache_label]);
+      auto key = std::make_tuple(vars, use_regex, flags, options);
+      if (pcache->map.count(key)) return pcache->map[key];
+    }
+  }
+
   PARTHENON_REQUIRE(vars.size() == use_regex.size(),
                     "Vargroup names and use_regex need to be the same size.");
   auto selector = [&](int vidx, const VarID &id, const Metadata &md) {
@@ -44,13 +70,42 @@ PackDescriptor MakePackDescriptorBase(StateDescriptor *psd,
     return false;
   };
 
-  return PackDescriptor(psd, vars, selector, options);
+  auto pd = PackDescriptor(psd, vars, selector, options);
+
+  if (pmesh) {
+    // Store the newly created PackDescriptor in the cache
+    auto pcache =
+        std::dynamic_pointer_cast<PackDescCache>(pmesh->pack_desc_cache_map[cache_label]);
+    auto key = std::make_tuple(vars, use_regex, flags, options);
+    pcache->map.emplace(key, pd);
+  }
+  return pd;
 }
 
-PackDescriptor MakePackDescriptorBase(StateDescriptor *psd,
+class PackDescUidCache : public PackDescriptorCacheBase {
+ public:
+  using key_t =
+      std::tuple<std::vector<Uid_t>, std::vector<MetadataFlag>, std::set<PDOpt>>;
+  std::unordered_map<key_t, impl::PackDescriptor> map;
+};
+
+PackDescriptor MakePackDescriptorBase(StateDescriptor *psd, Mesh *pmesh,
                                       const std::vector<Uid_t> &var_ids,
                                       const std::vector<MetadataFlag> &flags,
                                       const std::set<PDOpt> &options) {
+  const std::string cache_label{"uid"};
+  if (pmesh) {
+    if (!pmesh->pack_desc_cache_map.count(cache_label)) {
+      auto pcache = std::make_shared<PackDescUidCache>();
+      pmesh->pack_desc_cache_map.emplace(
+          cache_label, std::dynamic_pointer_cast<PackDescriptorCacheBase>(pcache));
+    }
+    auto pcache = std::dynamic_pointer_cast<PackDescUidCache>(
+        pmesh->pack_desc_cache_map[cache_label]);
+    auto key = std::make_tuple(var_ids, flags, options);
+    if (pcache->map.count(key)) return pcache->map[key];
+  }
+
   auto selector = [&](int vidx, const VarID &id, const Metadata &md) {
     if (flags.size() > 0) {
       for (const auto &flag : flags) {
@@ -60,6 +115,14 @@ PackDescriptor MakePackDescriptorBase(StateDescriptor *psd,
     if (Variable<Real>::GetUniqueID(id.label()) == var_ids[vidx]) return true;
     return false;
   };
+
+  auto pd = PackDescriptor(psd, var_ids, selector, options);
+  if (pmesh) {
+    auto pcache = std::dynamic_pointer_cast<PackDescUidCache>(
+        pmesh->pack_desc_cache_map[cache_label]);
+    auto key = std::make_tuple(var_ids, flags, options);
+    pcache->map.emplace(key, pd);
+  }
 
   return PackDescriptor(psd, var_ids, selector, options);
 }
@@ -74,7 +137,7 @@ void SetMeshAndStateDescriptor(MT *pmd, Mesh *&pmesh, StateDescriptor *&psd) {
     psd = pmesh->resolved_packages.get();
   } else if constexpr (std::is_same_v<MT, MeshBlock>) {
     pmesh = pmd->pmy_mesh;
-    psd = pmesh->resolved_packages.get(); 
+    psd = pmesh->resolved_packages.get();
   } else if constexpr (std::is_same_v<MT, Mesh>) {
     pmesh = pmd;
     psd = pmesh->resolved_packages.get();
