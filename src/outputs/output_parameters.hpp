9d36b5ed2a7c955eb2f68ebca5277da90417bf88
Jonah Miller <jonahm@lanl.gov>
2025-06-12 15:02:08 -0400

    more CC

--- src/outputs/output_parameters.hpp
+++ src/outputs/output_parameters.hpp
@@ -1,4 +1,8 @@
 //========================================================================================
+// Parthenon performance portable AMR framework
+// Copyright(C) 2020-2025 The Parthenon collaboration
+// Licensed under the 3-clause BSD License, see LICENSE file for details
+//========================================================================================
 // Athena++ astrophysical MHD code
 // Copyright(C) 2014 James M. Stone <jmstone@princeton.edu> and other code contributors
 // Licensed under the 3-clause BSD License, see LICENSE file for details
