0d5d4d3c51292e058c483356d8e8f0d993948fac
Philipp Grete <pgrete@hs.uni-hamburg.de>
2025-07-02 23:29:23 +0200

    Fix misplaced comma

--- src/parthenon_manager.cpp
+++ src/parthenon_manager.cpp
@@ -314,9 +314,8 @@ void ParthenonManager::RestartPackages(Mesh &rm, RestartReader &resfile) {
                                    " is marked as sparse in restart file");
     }
 
-    max_fillsize =
-        std::max(max_fillsize, static_cast<size_t>(v_info.FillSize(theDomain),
-                                                   resfile.BlockdataIsPadded()));
+    max_fillsize = std::max(max_fillsize, static_cast<size_t>(v_info.FillSize(
+                                              theDomain, resfile.BlockdataIsPadded())));
   }
 
   // make sure we have all sparse variables that are in the restart file
