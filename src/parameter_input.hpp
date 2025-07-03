4646e9cfc18ef33aea6a8bf28df1521b77d28751
Jonah Miller <jonahm@lanl.gov>
2025-07-03 11:56:11 -0400

    use is_integral and include string in templated get or add

--- src/parameter_input.hpp
+++ src/parameter_input.hpp
@@ -123,28 +123,28 @@ class ParameterInput {
   template <typename T, typename... Args>
   T GetOrAdd(const std::string &block, const std::string &name, const T &value,
              Args &&...args) {
-    // JMM: This is slightly dangerous but helps with the pain point
-    // Adam mentioned. Will be resolved with a more flexible parser
-    // such as TOML.
-    if constexpr (std::is_same_v<T, int> || std::is_same_v<T, std::size_t> ||
-                  std::is_same_v<T, std::uint64_t>) {
-      return GetOrAddInteger(block, name, value, std::forward<Args>(args)...);
+    if constexpr (std::is_same_v<T, bool>) {
+      return GetOrAddBoolean(block, name, value, std::forward<Args>(args)...);
     } else if constexpr (std::is_same_v<T, Real>) {
       return GetOrAddReal(block, name, value, std::forward<Args>(args)...);
-    } else if constexpr (std::is_same_v<T, bool>) {
-      return GetOrAddBoolean(block, name, value, std::forward<Args>(args)...);
+    } else if constexpr (std::is_same_v<T, std::string>) {
+      return GetOrAddString(block, name, value, std::forward<Args>(args)...);
+    } else if (std::is_integral_v<T>) {
+      return GetOrAddInteger(block, name, value, std::forward<Args>(args)...);
     } else {
       PARTHENON_THROW("Unknown type\n");
     }
   }
   template <typename T, typename... Args>
   T Get(const std::string &block, const std::string &name, Args &&...args) {
-    if constexpr (std::is_same_v<T, int>) {
-      return GetInteger(block, name, std::forward<Args>(args)...);
+    if constexpr (std::is_same_v<T, bool>) {
+      return GetOrAddBoolean(block, name, std::forward<Args>(args)...);
     } else if constexpr (std::is_same_v<T, Real>) {
       return GetReal(block, name, std::forward<Args>(args)...);
-    } else if constexpr (std::is_same_v<T, bool>) {
-      return GetOrAddBoolean(block, name, std::forward<Args>(args)...);
+    } else if constexpr (std::is_same_v<T, std::string>) {
+      return GetOrAddString(block, name, value, std::forward<Args>(args)...);
+    } else if (std::is_integral_v<T>) {
+      return GetInteger(block, name, std::forward<Args>(args)...);
     } else {
       PARTHENON_THROW("Unknown type\n");
     }
