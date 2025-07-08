2806830a7deb5fd92bebbee43b1cdc0c7f4e55a7
Brandon Barker <bbarker.py@protonmail.ch>
2025-07-08 20:34:34 +0000

    move generated files into BINARY_DIR/generated. rename vars to avoid collisions

--- cmake/Provenance.cmake
+++ cmake/Provenance.cmake
@@ -22,19 +22,19 @@ if (NOT DEFINED pre_configure_dir)
 endif ()
 
 if (NOT DEFINED post_configure_dir)
-    set(post_configure_dir ${CMAKE_SOURCE_DIR}/src/generated)
+    set(post_configure_dir ${CMAKE_BINARY_DIR}/generated)
 endif ()
 
 set(pre_configure_file ${pre_configure_dir}/provenance.cpp.in)
 set(post_configure_file ${post_configure_dir}/provenance.cpp)
 
 function(CheckGitWrite git_hash)
-    file(WRITE ${CMAKE_BINARY_DIR}/git-state.txt ${git_hash})
+    file(WRITE ${CMAKE_BINARY_DIR}/git-state-parthenon.txt ${git_hash})
 endfunction()
 
 function(CheckGitRead git_hash)
-    if (EXISTS ${CMAKE_BINARY_DIR}/git-state.txt)
-        file(STRINGS ${CMAKE_BINARY_DIR}/git-state.txt CONTENT)
+    if (EXISTS ${CMAKE_BINARY_DIR}/git-state-parthenon.txt)
+        file(STRINGS ${CMAKE_BINARY_DIR}/git-state-parthenon.txt CONTENT)
         LIST(GET CONTENT 0 var)
 
         set(${git_hash} ${var} PARENT_SCOPE)
@@ -46,7 +46,7 @@ function(CheckGitVersion)
     execute_process(
         COMMAND git log -1 --format=%h
         WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-        OUTPUT_VARIABLE GIT_HASH
+        OUTPUT_VARIABLE PARTH_GIT_HASH
         OUTPUT_STRIP_TRAILING_WHITESPACE
         )
 
@@ -54,29 +54,34 @@ function(CheckGitVersion)
     execute_process(
         COMMAND git rev-parse --abbrev-ref HEAD
         WORKING_DIRECTORY ${CMAKE_CURRENT_LIST_DIR}
-        OUTPUT_VARIABLE GIT_BRANCH
+        OUTPUT_VARIABLE PARTH_GIT_BRANCH
         OUTPUT_STRIP_TRAILING_WHITESPACE
     )
 
-    CheckGitRead(GIT_HASH_CACHE)
+    CheckGitRead(PARTH_GIT_HASH_CACHE)
     if (NOT EXISTS ${post_configure_dir})
+        message("WHAT1")
         file(MAKE_DIRECTORY ${post_configure_dir})
     endif ()
 
     if (NOT EXISTS ${post_configure_dir}/provenance.hpp)
+        message("WHAT")
         file(COPY ${pre_configure_dir}/provenance.hpp DESTINATION ${post_configure_dir})
     endif()
 
-    if (NOT DEFINED GIT_HASH_CACHE)
-        set(GIT_HASH_CACHE "INVALID")
+    if (NOT DEFINED PARTH_GIT_HASH_CACHE)
+        set(PARTH_GIT_HASH_CACHE "INVALID")
     endif ()
 
     # Only update the provenance.cpp if the hash has changed. This will
     # prevent us from rebuilding the project more than we need to.
-    if (NOT ${GIT_HASH} STREQUAL ${GIT_HASH_CACHE} OR NOT EXISTS ${post_configure_file})
-        # Set che GIT_HASH_CACHE variable the next build won't have
+    if (NOT ${PARTH_GIT_HASH} STREQUAL ${PARTH_GIT_HASH_CACHE} OR NOT EXISTS ${post_configure_file})
+        # Set the PARTH_GIT_HASH_CACHE variable the next build won't have
         # to regenerate the source file.
-        CheckGitWrite(${GIT_HASH})
+        message("should i be here")
+        message("HASH ${PARTH_GIT_HASH}")
+        message("HASH CACHE ${PARTH_GIT_HASH_CACHE}")
+        CheckGitWrite(${PARTH_GIT_HASH})
 
         configure_file(${pre_configure_file} ${post_configure_file} @ONLY)
     endif ()
@@ -89,12 +94,12 @@ function(CheckGitSetup)
         -DRUN_CHECK_GIT_VERSION=1
         -Dpre_configure_dir=${pre_configure_dir}
         -Dpost_configure_file=${post_configure_dir}
-        -DGIT_HASH_CACHE=${GIT_HASH_CACHE}
+        -DPARTH_GIT_HASH_CACHE=${PARTH_GIT_HASH_CACHE}
         -P ${CURRENT_LIST_DIR}/Provenance.cmake
         BYPRODUCTS ${post_configure_file}
         )
 
-        add_library(git_version ${CMAKE_SOURCE_DIR}/src/generated/provenance.cpp)
+    add_library(git_version ${CMAKE_BINARY_DIR}/generated/provenance.cpp)
     target_include_directories(git_version PUBLIC ${CMAKE_BINARY_DIR}/generated)
     add_dependencies(git_version ParthenonAlwaysCheckGit)
 
@@ -115,5 +120,4 @@ set(PARTH_COMPILER "${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}")
 set(PARTH_COMPILER_VERSION ${CMAKE_CXX_COMPILER_VERSION})
 set(PARTH_OPTIMIZATION ${CMAKE_BUILD_TYPE})
 set(PARTH_ARCH ${CMAKE_HOST_SYSTEM_PROCESSOR})
-configure_file(${pre_configure_file} ${post_configure_file} @ONLY)
-
+#configure_file(${pre_configure_file} ${post_configure_file} @ONLY)
