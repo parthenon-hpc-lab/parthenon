41ef1ba475561b0258055ed5397f947389c996e7
Luke Roberts <lfroberts@lanl.gov>
2025-07-09 13:08:37 -0600

    fix prolongation divide by zero

--- src/prolong_restrict/pr_ops.hpp
+++ src/prolong_restrict/pr_ops.hpp
@@ -156,10 +156,10 @@ struct RestrictAverage {
     // symmetry
     const Real tvol = ((vol[0][0][0] + vol[0][1][0]) + (vol[0][0][1] + vol[0][1][1])) +
                       ((vol[1][0][0] + vol[1][1][0]) + (vol[1][0][1] + vol[1][1][1]));
-    coarse(element_idx, l, m, n, ck, cj, ci) =
+    coarse(element_idx, l, m, n, ck, cj, ci) = tvol > 0.0 ?
         (((terms[0][0][0] + terms[0][1][0]) + (terms[0][0][1] + terms[0][1][1])) +
          ((terms[1][0][0] + terms[1][1][0]) + (terms[1][0][1] + terms[1][1][1]))) /
-        tvol;
+        tvol : 0.0;
   }
 };
 
