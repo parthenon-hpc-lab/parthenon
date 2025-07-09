dd0d23c6e9760a70355ee36d0020415a0e469bba
Luke Roberts <lfroberts@lanl.gov>
2025-07-09 13:21:53 -0600

    compile w/ non-cartesian coords

--- example/diffusion/diffusion_equation.hpp
+++ example/diffusion/diffusion_equation.hpp
@@ -269,10 +269,7 @@ class DiffusionEquation {
           const auto &coords = pack.GetCoordinates(b);
           const int gid = pack.GetGID(b);
           const int level = pack.GetLevel(b, 0, 0, 0);
-          const Real dxs[3]{coords.template Dxc<X1DIR>(), coords.template Dxc<X2DIR>(),
-                            coords.template Dxc<X3DIR>()};
           for (int face = 0; face < ndim * 2; ++face) {
-            const Real dx = dxs[dirs[face] - 1];
             const auto &idxer = idxers[face];
             const auto dir = dirs[face];
             const auto te = tes[face];
@@ -288,7 +285,7 @@ class DiffusionEquation {
                     const auto [k, j, i] = idxer(idx);
                     pack.flux(b, dir, var_t(), k, j, i) =
                         sign * pack_mat(b, te, D_t(), k, j, i) *
-                        pack(b, var_t(), k + koff, j + joff, i + ioff) / (0.5 * dx);
+                        pack(b, var_t(), k + koff, j + joff, i + ioff) / (0.5 * coords.Dxc(dir, k, j, i));
                   });
             }
             // Correct for size of neighboring zone at fine-coarse boundary when using
