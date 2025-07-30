dd94417e8e9291d2fd04b965191e6b64f5e6f4f1
Philipp Grete <pgrete@hs.uni-hamburg.de>
2025-07-30 12:02:13 +0200

    Allow writing legacy id fields for particles

--- src/outputs/parthenon_hdf5.cpp
+++ src/outputs/parthenon_hdf5.cpp
@@ -585,9 +585,11 @@ void PHDF5Output::WriteOutputFileImpl(Mesh *pm, ParameterInput *pin, SimTime *tm
                   local_count, global_count, pl_xfer, H5P_DEFAULT);
     }
 
-    // If swarm does not contain the default id object, generate a sequential
-    // one for vis called "id" (to differentiate between the default one)
-    if (swinfo.var_info.count(swarm_position::id::name()) == 0) {
+    // If swarm does not contain the default id object nor a custom one called "id",
+    // generate a sequential one for vis called "id" (to differentiate between the default
+    // one)
+    if (swinfo.var_info.count(swarm_position::id::name()) == 0 &&
+        swinfo.var_info.count("id") == 0) {
       std::vector<int> ids(swinfo.global_count);
       std::iota(std::begin(ids), std::end(ids), swinfo.global_offset);
       local_offset[0] = swinfo.global_offset;
