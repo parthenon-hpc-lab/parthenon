ee94fe1e06ba0f164f2e4bae31e6956696cdf35b
Jonah Miller <jonahm@lanl.gov>
2025-08-28 19:41:30 -0400

    fix allowed values in docstrings

--- src/parameter_input.hpp
+++ src/parameter_input.hpp
@@ -495,6 +495,7 @@ class ParameterInput {
       } else if (allowed_vals.size() > 0) {
         for (const auto &allowed : allowed_vals) {
           record.allowed_values.push_back(std::any(allowed));
+          record.allowed_values_str.push_back(record.ToString(allowed));
         }
       }
       // if two inconsistent docstrings exist, complain
