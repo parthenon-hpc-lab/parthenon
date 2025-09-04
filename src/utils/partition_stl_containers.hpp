3c369ac798d78d8394e19452e5470b7057f86674
Jonah Miller <jonahm@lanl.gov>
2025-09-04 13:09:58 -0400

    Have ToSizeN return sopmething sensible in the case of no meshblocks

--- src/utils/partition_stl_containers.hpp
+++ src/utils/partition_stl_containers.hpp
@@ -47,7 +47,9 @@ auto ToSizeN(Container_t<T, extra...> &container, const int N) {
   using std::to_string;
   using namespace partition_impl;
 
-  PARTHENON_REQUIRE_THROWS(N > 0, "Your partition must be at least size 1");
+  if (N <= 0) {
+    return Partition_t<T>();
+  }
 
   int nelements = container.size();
   int npartitions = IntCeil(nelements, N);
