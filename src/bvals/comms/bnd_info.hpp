853a95673a532b3b7698b286693e9c003ba26032
Jonah Miller <jonahm@lanl.gov>
2025-10-17 11:40:51 -0400

    come on

--- src/bvals/comms/bnd_info.hpp
+++ src/bvals/comms/bnd_info.hpp
@@ -82,8 +82,9 @@ struct BndInfo {
   BndInfo(const BndInfo &) = default;
   BndInfo(MeshBlock *pmb, const NeighborBlock &nb, std::shared_ptr<Variable<Real>> v,
           CommBuffer<buf_pool_t<Real>::owner_t> *combuf, IndexRangeType idx_range_type);
-  KOKKOS_DEFAULTED_FUNCTION
-  ~BndInfo() = default;
+
+  KOKKOS_INLINE_FUNCTION
+  ~BndInfo() {}
 
   // These are are used to generate the BndInfo struct for various
   // kinds of boundary types and operations.
