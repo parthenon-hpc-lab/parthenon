6b103adec9003831f13f4777465e883e742d1a74
par-hermes <par-hermes@automation.bot>
2026-02-26 23:51:58 +0000

    cpp-py-formatter

--- src/mesh/mesh-gmg.cpp
+++ src/mesh/mesh-gmg.cpp
@@ -17,7 +17,6 @@
 //! \file mesh_amr.cpp
 //  \brief implementation of Mesh::AdaptiveMeshRefinement() and related utilities
 
-#include <unistd.h>
 #include <algorithm>
 #include <cstdint>
 #include <iostream>
@@ -28,6 +27,7 @@
 #include <sstream>
 #include <string>
 #include <tuple>
+#include <unistd.h>
 #include <unordered_set>
 #include <vector>
 
